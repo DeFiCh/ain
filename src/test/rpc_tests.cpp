bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove use of CRPCTable::appendCommand in wallet code
This commit does not change behavior.

src/test/rpc_tests.cpp
@@ -26,9 +26,9 @@ UniValue CallRPC(std::string args)
     request.params = RPCConvertValues(strMethod, vArgs);
     request.fHelp = false;
     BOOST_CHECK(tableRPC[strMethod]);
-    rpcfn_type method = tableRPC[strMethod]->actor;
+    const auto& command = *tableRPC[strMethod];
     try {
-        UniValue result = (*method)(request);
+        UniValue result = command.actor(request);
         return result;
     }
     catch (const UniValue& objError) {

src/wallet/rpcwallet.cpp
@@ -3311,8 +3311,8 @@ static const CRPCCommand commands[] =
     { "generating",         "generate",                 &generate,                 true,   {"nblocks","maxtries"} },
 };
 
-void RegisterWalletRPCCommands(CRPCTable &t)
+void RegisterWalletRPCCommands(ipc::Chain& ipc_chain, std::vector<std::unique_ptr<ipc::Handler>>& handlers)
 {
     for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
-        t.appendCommand(commands[vcidx].name, &commands[vcidx]);
+        handlers.emplace_back(ipc_chain.handleRpc(commands[vcidx]));
 }

src/wallet/rpcwallet.h
@@ -5,10 +5,18 @@
 #ifndef BITCOIN_WALLET_RPCWALLET_H
 #define BITCOIN_WALLET_RPCWALLET_H
 
+#include <memory>
+#include <vector>
+
 class CRPCTable;
 class JSONRPCRequest;
 
-void RegisterWalletRPCCommands(CRPCTable &t);
+namespace ipc {
+class Chain;
+class Handler;
+}
+
+void RegisterWalletRPCCommands(ipc::Chain& ipc_chain, std::vector<std::unique_ptr<ipc::Handler>>& handlers);
 
 /**
  * Figures out what wallet, if any, to use for a JSONRPCRequest.

src/wallet/test/wallet_test_fixture.cpp
@@ -22,7 +22,7 @@ WalletTestingSetup::WalletTestingSetup(const std::string& chainName):
     pwalletMain->LoadWallet(fFirstRun);
     pwalletMain->m_ipc_handler = m_ipc_chain->handleNotifications(*pwalletMain);
 
-    RegisterWalletRPCCommands(tableRPC);
+    m_ipc_client->registerRpcs();
 }
 
 WalletTestingSetup::~WalletTestingSetup()

DeFiCh/ain

src/test/rpc_tests.cpp
@@ -31,10 +31,9 @@ UniValue CallRPC(std::string args)
     request.strMethod = strMethod;
     request.params = RPCConvertValues(strMethod, vArgs);
     request.fHelp = false;
-    BOOST_CHECK(tableRPC[strMethod]);
-    rpcfn_type method = tableRPC[strMethod]->actor;
+    if (RPCIsInWarmup(nullptr)) SetRPCWarmupFinished();
     try {
-        UniValue result = (*method)(request);
+        UniValue result = tableRPC.execute(request);
         return result;
     }
     catch (const UniValue& objError) {

src/wallet/rpcwallet.cpp
@@ -4156,8 +4156,8 @@ static const CRPCCommand commands[] =
 };
 // clang-format on
 
-void RegisterWalletRPCCommands(CRPCTable &t)
+void RegisterWalletRPCCommands(interfaces::Chain& chain, std::vector<std::unique_ptr<interfaces::Handler>>& handlers)
 {
     for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
-        t.appendCommand(commands[vcidx].name, &commands[vcidx]);
+        handlers.emplace_back(chain.handleRpc(commands[vcidx]));
 }

src/wallet/rpcwallet.h
@@ -5,7 +5,9 @@
 #ifndef BITCOIN_WALLET_RPCWALLET_H
 #define BITCOIN_WALLET_RPCWALLET_H
 
+#include <memory>
 #include <string>
+#include <vector>
 
 class CRPCTable;
 class CWallet;
@@ -14,7 +16,12 @@ class UniValue;
 struct PartiallySignedTransaction;
 class CTransaction;
 
-void RegisterWalletRPCCommands(CRPCTable &t);
+namespace interfaces {
+class Chain;
+class Handler;
+}
+
+void RegisterWalletRPCCommands(interfaces::Chain& chain, std::vector<std::unique_ptr<interfaces::Handler>>& handlers);
 
 /**
  * Figures out what wallet, if any, to use for a JSONRPCRequest.

src/wallet/test/wallet_test_fixture.cpp
@@ -15,5 +15,5 @@ WalletTestingSetup::WalletTestingSetup(const std::string& chainName):
     m_wallet.LoadWallet(fFirstRun);
     m_wallet.m_chain_notifications_handler = m_chain->handleNotifications(m_wallet);
 
-    RegisterWalletRPCCommands(tableRPC);
+    m_chain_client->registerRpcs();
 }