508f5c40439e00c94517e232efc66b9d6684db12
Ahmed Hilali <ahmed@cakedefi.com>
2019-09-17 18:21:03 +0700

    rm nonce, coinstakeAmount and coinstakePrevout

--- src/test/blockfilter_index_tests.cpp
+++ src/test/blockfilter_index_tests.cpp
@@ -87,7 +87,7 @@ static CBlock CreateBlock(const CBlockIndex* prev,
     unsigned int extraNonce = 0;
     IncrementExtraNonce(&block, prev, extraNonce);
 
-    while (!CheckProofOfWork(block.GetHash(), block.nBits, chainparams.GetConsensus())) ++block.nNonce;
+  //  while (!CheckProofOfWork(block.GetHash(), block.nBits, chainparams.GetConsensus())) ++block.nNonce;
 
     return block;
 }
