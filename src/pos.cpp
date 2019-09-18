8314d35d1da5f998c41219d5bc39657afcd09c1d
IntegralTeam V <repo@sfxdx.ru>
2019-09-18 16:35:23 +0700

    refactor PoS structure. Add height and mintedBlocks params to CBlockHeader

--- src/pos.cpp
+++ src/pos.cpp
@@ -29,35 +29,22 @@ namespace pos {
 
 /// Check PoS signatures (PoS block hashes are signed with coinstake out pubkey)
     bool CheckHeaderSignature(const CBlockHeader& block) {
-        if (!block.IsProofOfStake()) {
-            return true;
-        }
-
-        if (block.proofOfStakeBody->sig.empty()) {
+        if (block.sig.empty()) {
             LogPrintf("CheckBlockSignature: Bad Block - PoS signature is empty\n");
             return false;
         }
 
         CPubKey recoveredPubKey{};
-        if (!recoveredPubKey.RecoverCompact(block.GetHashToSign(), block.proofOfStakeBody->sig)) {
+        if (!recoveredPubKey.RecoverCompact(block.GetHashToSign(), block.sig)) {
             LogPrintf("CheckBlockSignature: Bad Block - malformed signature\n");
             return false;
         }
 
-        if (recoveredPubKey.GetID() != block.proofOfStakeBody->pubKeyHash) {
-            LogPrintf("CheckBlockSignature: Bad Block - wrong signature\n");
-            return false;
-        }
         return true;
     }
 
     bool CheckProofOfStake_headerOnly(const CBlockHeader& block, const Consensus::Params& params) {
-        if (!block.IsProofOfStake()) {
-            return error("CheckProofOfStake_headerOnly(): called on non-PoS %s", block.GetHash().ToString());
-        }
-
         const int64_t coinstakeTime = (int64_t) block.GetBlockTime();
-        const auto& body = *block.proofOfStakeBody; // checked to exist after IsProofOfStake
 
         // checking PoS kernel is faster, so check it first
         if (!CheckKernelHash(block.stakeModifier, block.nBits, coinstakeTime, params).hashOk) {
@@ -68,14 +55,9 @@ namespace pos {
 
     bool CheckProofOfStake(const CBlockIndex* pindexPrev, const CBlock& block, CCoinsViewCache& view,
                            const Consensus::Params& params) {
-        if (block.IsProofOfStake() != block.HasCoinstakeTx()) {
-            return false; // block claimed it's PoS, but doesn't have coinstakeTx
-        }
-        if (!block.IsCompleteProofOfStake()) {
+        if (!block.HasCoinstakeTx()) {
             return error("CheckProofOfStake(): called on a non-PoS block %s", block.GetHash().ToString());
         }
-
-        const auto& body = *block.proofOfStakeBody; // checked to exist after IsProofOfStake
 
         // check staker's pubKeyHash
         {
@@ -86,10 +68,6 @@ namespace pos {
                 return error("CheckProofOfStake(): coinstakeTx scriptPubKey must be P2PKH");
             if (!(typeRet == txnouttype::TX_PUBKEYHASH && addressRet.size() == 1))
                 return error("CheckProofOfStake(): coinstakeTx scriptPubKey must be P2PKH");
-
-            CKeyID keyID(boost::get<PKHash>(addressRet[0]));
-            if (keyID != body.pubKeyHash)
-                return error("CheckProofOfStake(): coinstakeTx scriptPubKey and block pubKeyHash mismatch");
         }
 
         // check staker's coin
