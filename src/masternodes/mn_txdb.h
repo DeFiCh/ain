be5752a2a6a5c54a561a10ed14ee1eb28aac511f
Ahmed Hilali <ahmed@cakedefi.com>
2019-09-23 10:57:12 +0700

    changed copyrights

--- src/masternodes/mn_txdb.h
+++ src/masternodes/mn_txdb.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 IntegralTeam
+// Copyright (c) 2019 DeFi Foundation
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
