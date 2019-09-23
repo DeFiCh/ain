71c2fb2544da2f9001296c3578f3bbde609e7efc
Ahmed Hilali <ahmed@cakedefi.com>
2019-09-23 10:57:12 +0700

    changed copyrights

--- test/functional/rpc_mn_basic.py
+++ test/functional/rpc_mn_basic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) 2014-2019 The Bitcoin Core developers
-# Copyright (c) 2019 The IntegralTeam
+# Copyright (c) DeFi Foundation
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test the masternodes RPC.
