95256aace7d8cfd47a7788024752eefe7d130504
U-Zyn Chua <chua@uzyn.com>
2019-12-19 18:42:46 +0000

    Basic masternode functionality

---
+++ src/masternodes/mn_txdb.h
@@ -0,0 +1,111 @@
+#ifndef MASTERNODES_TXDB_H
+#define MASTERNODES_TXDB_H
+
+#include "dbwrapper.h"
+#include "masternodes.h"
+
+#include <functional>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+
+/** Access to the masternodes database (masternodes/) */
+class CMasternodesViewDB : public CMasternodesView
+{
+private:
+    boost::shared_ptr<CDBWrapper> db;
+    boost::scoped_ptr<CDBBatch> batch;
+
+public:
+    CMasternodesViewDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
+    ~CMasternodesViewDB() override {}
+
+protected:
+    // for test purposes only
+    CMasternodesViewDB();
+
+private:
+    CMasternodesViewDB(CMasternodesViewDB const & other) = delete;
+    CMasternodesViewDB & operator=(CMasternodesViewDB const &) = delete;
+
+    template <typename K, typename V>
+    void BatchWrite(const K& key, const V& value)
+    {
+        if (!batch)
+        {
+            batch.reset(new CDBBatch(*db));
+        }
+        batch->Write<K,V>(key, value);
+    }
+    template <typename K>
+    void BatchErase(const K& key)
+    {
+        if (!batch)
+        {
+            batch.reset(new CDBBatch(*db));
+        }
+        batch->Erase<K>(key);
+    }
+
+    template <typename Container>
+    bool LoadTable(char prefix, Container & data, std::function<void(typename Container::key_type const &, typename Container::mapped_type &)> callback = nullptr)
+    {
+        boost::scoped_ptr<CDBIterator> pcursor(const_cast<CDBWrapper*>(&*db)->NewIterator());
+        pcursor->Seek(prefix);
+
+        while (pcursor->Valid())
+        {
+            boost::this_thread::interruption_point();
+            std::pair<char, typename Container::key_type> key;
+            if (pcursor->GetKey(key) && key.first == prefix)
+            {
+                typename Container::mapped_type value;
+                if (pcursor->GetValue(value))
+                {
+                    data.emplace(key.second, std::move(value));
+                    if (callback)
+                        callback(key.second, data.at(key.second));
+                } else {
+                    return error("MNDB::Load() : unable to read value");
+                }
+            } else {
+                break;
+            }
+            pcursor->Next();
+        }
+        return true;
+    }
+
+protected:
+    void CommitBatch();
+//    void DropBatch();
+
+    bool ReadHeight(int & h);
+    void WriteHeight(int h);
+
+    void WriteMasternode(uint256 const & txid, CMasternode const & node);
+    void EraseMasternode(uint256 const & txid);
+
+//    void WriteDeadIndex(int height, uint256 const & txid, char type);
+//    void EraseDeadIndex(int height, uint256 const & txid);
+
+    void WriteUndo(int height, uint256 const & txid, uint256 const & affectedNode, char undoType);
+    void EraseUndo(int height, uint256 const & txid);
+
+//    void WriteTeam(int blockHeight, CTeam const & team);
+
+public:
+    bool Load() override;
+    bool Flush() override;
+
+private:
+//    bool LoadMasternodes(std::function<void(uint256 &, CMasternode &)> onNode) const;
+//    bool LoadUndo(std::function<void(int, uint256 const &, uint256 const &, char)> onUndo) const;
+//    bool LoadTeams(CTeams & teams) const;
+};
+
+#endif // MASTERNODES_TXDB_H
--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -169,6 +169,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "createwallet", 4, "avoid_reuse"},
     { "getnodeaddresses", 0, "count"},
     { "stop", 0, "wait" },
+    { "mn_create", 0, "inputs" },
+    { "mn_create", 1, "metadata" },
 };
 // clang-format on
 
