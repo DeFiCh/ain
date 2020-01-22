cef8524d0176a82013116f31a44be6bb202712fc
Ahmed Hilali <ahmed@cakedefi.com>
2020-01-22 16:31:06 +0700

    spv: massive rpc improvements and some tests

--- src/spv/spv_rpc.cpp
+++ src/spv/spv_rpc.cpp
@@ -32,18 +32,6 @@ static CWallet* GetWallet(const JSONRPCRequest& request)
     return pwallet;
 }
 
-CAnchor createAnchorMessage(CTxDestination const & rewardDest, uint256 const & forBlock = uint256())
-{
-    CAnchor const anchor = panchorauths->CreateBestAnchor(rewardDest, forBlock);
-
-    /// @todo should it match current team? or leave "as is", trusting auth's signing?
-    auto minQuorum = GetMinAnchorQuorum(panchors->GetCurrentTeam(panchors->GetActiveAnchor()));
-    if (anchor.sigs.size() < minQuorum) {
-        throw JSONRPCError(RPC_VERIFY_ERROR, "Min anchor quorum was not reached (" + std::to_string(anchor.sigs.size()) + ", need "+ std::to_string(minQuorum) + ") ");
-    }
-    return anchor;
-}
-
 UniValue spv_sendrawtx(const JSONRPCRequest& request)
 {
     RPCHelpMan{"spv_sendrawtx",
@@ -134,20 +122,20 @@ UniValue spv_createanchor(const JSONRPCRequest& request)
             HelpRequiringPassphrase(pwallet) + "\n",
         {
                 /// @todo not fully implemented yet! now in test mode!
-//            {"inputs", RPCArg::Type::ARR, RPCArg::Optional::OMITTED_NAMED_ARG, "A json array of json objects",
-//                {
-//                    {"", RPCArg::Type::OBJ, RPCArg::Optional::OMITTED, "",
-//                        {
-//                            {"txid", RPCArg::Type::STR_HEX, RPCArg::Optional::NO, "The transaction id"},
-//                            {"vout", RPCArg::Type::NUM, RPCArg::Optional::NO, "The output number"},
-//                            {"amount", RPCArg::Type::NUM, RPCArg::Optional::NO, "Amount of output"},
-//                            {"privkey", RPCArg::Type::STR, RPCArg::Optional::NO, "WIF private key for signing this output"},
-//                        },
-//                    },
-//                },
-//            },
+            {"inputs", RPCArg::Type::ARR, RPCArg::Optional::OMITTED_NAMED_ARG, "A json array of json objects",
+                {
+                    {"", RPCArg::Type::OBJ, RPCArg::Optional::OMITTED, "",
+                        {
+                            {"txid", RPCArg::Type::STR_HEX, RPCArg::Optional::NO, "The transaction id"},
+                            {"vout", RPCArg::Type::NUM, RPCArg::Optional::NO, "The output number"},
+                            {"amount", RPCArg::Type::NUM, RPCArg::Optional::NO, "Amount of output in satoshis"},
+                            {"privkey", RPCArg::Type::STR, RPCArg::Optional::NO, "WIF private key for signing this output"},
+                        },
+                    },
+                },
+            },
             {"rewardAddress", RPCArg::Type::STR, RPCArg::Optional::NO, "User's P2PKH address (in DeFi chain) for reward"},
-            {"hash", RPCArg::Type::STR, RPCArg::Optional::OMITTED, "ID of block in DF chain to anchor to. Latest possible authorized block if omitted." },
+            {"send", RPCArg::Type::BOOL, RPCArg::Optional::OMITTED, "Send it to btc network (Default = true)"},
         },
         RPCResult{
             "\"txHex\"                  (string) The hex-encoded raw transaction with signature(s)\n"
@@ -169,24 +157,34 @@ UniValue spv_createanchor(const JSONRPCRequest& request)
     }
 
     /// @todo temporary off, tests with fixed values
-//    RPCTypeCheck(request.params, { UniValue::VARR, UniValue::VSTR, UniValue::VSTR }, true);
-//    if (request.params[0].isNull() || request.params[1].isNull())
-//    {
-//        throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameters, arguments 1 and 2 must be non-null");
-//    }
-//    std::string rewardAddress = request.params[1].getValStr();
-//    CTxDestination rewardDest = DecodeDestination(rewardAddress);
-//    if (rewardDest.which() != 1 && rewardDest.which() != 4)
-//    {
-//        throw JSONRPCError(RPC_INVALID_PARAMETER, "rewardAddress (" + rewardAddress + ") does not refer to a P2PKH or P2WPKH address");
-//    }
-//    uint256 const forBlock(ParseHashV(request.params[2].getValStr(), "blockHash"));
+    RPCTypeCheck(request.params, { UniValue::VARR, UniValue::VSTR, UniValue::VBOOL }, true);
+    if (request.params[0].isNull() || request.params[1].isNull())
+    {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameters, arguments 1 and 2 must be non-null");
+    }
 
+    std::vector<spv::TxInputData> inputsData;
+    UniValue inputs(UniValue::VARR);
+    inputs = request.params[0].get_array();
+    for (size_t idx = 0; idx < inputs.size(); ++idx)
+    {
+        UniValue const & input = inputs[idx].get_obj();
+        ParseHashV(input["txid"], "txid");
+        inputsData.push_back({ input["txid"].getValStr(), input["vout"].get_int(), (uint64_t) input["amount"].get_int64(), input["privkey"].getValStr() });
+    }
+
+    std::string rewardAddress = request.params[1].getValStr();
+    CTxDestination rewardDest = DecodeDestination(rewardAddress);
+    if (rewardDest.which() != 1 && rewardDest.which() != 4)
+    {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "rewardAddress (" + rewardAddress + ") does not refer to a P2PKH or P2WPKH address");
+    }
+    bool const send = request.params[2].isNull() ? true : request.params[2].getBool();
 
     auto locked_chain = pwallet->chain().lock();
 
     /// @todo temporary, tests with fixed values
-    CTxDestination rewardDest = DecodeDestination("mmjrUWSKQqnkWzyS98GCuFxA7TXcK3bc3A");
+//    CTxDestination rewardDest = DecodeDestination("mmjrUWSKQqnkWzyS98GCuFxA7TXcK3bc3A");
     CAnchor const anchor = panchorauths->CreateBestAnchor(rewardDest/*, forBlock*/);
     if (anchor.sigs.empty()) {
         throw JSONRPCError(RPC_VERIFY_ERROR, "Min anchor quorum was not reached!");
@@ -195,11 +193,21 @@ UniValue spv_createanchor(const JSONRPCRequest& request)
     CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
     ss << anchor;
 
+
     /// @todo temporary, tests
 //    auto rawtx = spv::CreateAnchorTx("e6f0a5e4db120f6877710bbbb5f9523162b6456bb1d4d89b854e60a794e03b46", 1, 3271995, "cStbpreCo2P4nbehPXZAAM3gXXY1sAphRfEhj7ADaLx8i2BmxvEP", ToByteVector(ss));
-    auto rawtx = spv::CreateAnchorTx("a0d5a294be3cde6a8bddab5815b8c4cb1b2ebf2c2b8a4018205d6f8c576e8963", 3, 2262303, "cStbpreCo2P4nbehPXZAAM3gXXY1sAphRfEhj7ADaLx8i2BmxvEP", ToByteVector(ss));
+//    auto rawtx = spv::CreateAnchorTx("a0d5a294be3cde6a8bddab5815b8c4cb1b2ebf2c2b8a4018205d6f8c576e8963", 3, 2262303, "cStbpreCo2P4nbehPXZAAM3gXXY1sAphRfEhj7ADaLx8i2BmxvEP", ToByteVector(ss));
 
-    bool send = true;
+    uint256 hash;
+    spv::TBytes rawtx;
+    uint64_t cost;
+    try {
+        std::tie(hash, rawtx, cost) = spv::CreateAnchorTx(inputsData, ToByteVector(ss));
+    }
+    catch (std::runtime_error const & e) {
+        throw JSONRPCError(RPC_MISC_ERROR, e.what());
+    }
+
     if (send) {
         if (!spv::pspv)
             throw JSONRPCError(RPC_INVALID_REQUEST, "spv module disabled");
@@ -207,16 +215,12 @@ UniValue spv_createanchor(const JSONRPCRequest& request)
         spv::pspv->SendRawTx(rawtx);
     }
 
-    CMutableTransaction mtx;
-    /// @todo implement separated bitcoin serialize/deserialize
-    DecodeHexTx(mtx, std::string(rawtx.begin(), rawtx.end()), true);
-
     UniValue result(UniValue::VOBJ);
-//    result.pushKV("anchorMsg", HexStr(ss.begin(), ss.end()));
-//    result.pushKV("anchorMsgHash", anchor.GetHash().ToString());
     result.pushKV("txHex", HexStr(rawtx));
-    /// @attention WRONG HASH!!!
-    result.pushKV("txHash", CTransaction(mtx).GetHash().ToString());
+    result.pushKV("txHash", hash.ToString());
+    result.pushKV("defiHash", anchor.blockHash.ToString());
+    result.pushKV("defiHeight", (int) anchor.height);
+    result.pushKV("cost", cost);
 
     return result;
 }
@@ -235,7 +239,6 @@ UniValue spv_createanchortemplate(const JSONRPCRequest& request)
         },
         RPCResult{
             "\"txHex\"                  (string) The hex-encoded raw transaction with signature(s)\n"
-            "\"txHash\"                 (string) The hex-encoded transaction hash\n"
         },
         RPCExamples{
             HelpExampleCli("spv_createanchortemplate", "\"[{\\\"txid\\\":\\\"id\\\",\\\"vout\\\":0,\\\"amount\\\":10000,\\\"privkey\\\":\\\"WIFprivkey\\\"}]\" "
@@ -268,13 +271,58 @@ UniValue spv_createanchortemplate(const JSONRPCRequest& request)
         throw JSONRPCError(RPC_VERIFY_ERROR, "Min anchor quorum was not reached!");
     }
 
-    /// @todo will be implemented only after anchors tests
+    CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
+    ss << anchor;
+    auto metaScripts = spv::EncapsulateMeta(ToByteVector(ss));
 
+    auto consensus = Params().GetConsensus();
+
+    CMutableTransaction mtx;
+    // output[0] - anchor address with creation fee
+    mtx.vout.push_back(CTxOut(consensus.spv.creationFee, GetScriptForDestination(DecodeDestination(consensus.spv.anchors_address))));
+
+    // output[1] - metadata (first part with OP_RETURN)
+    mtx.vout.push_back(CTxOut(0, metaScripts[0]));
+
+    // output[2..n-1] - metadata (rest of the data in p2wsh keys)
+    for (size_t i = 1; i < metaScripts.size(); ++i) {
+        mtx.vout.push_back(CTxOut(spv::P2WSH_DUST, metaScripts[i]));
+    }
 
     UniValue result(UniValue::VOBJ);
-//    result.pushKV("txHex", EncodeHexTx(CTransaction(rawTx)));
-//    result.pushKV("txHash", CTransaction(rawTx).GetHash().ToString());
+    /// @todo implement own btc tx serialization when tokens will join the game
+    result.pushKV("txHex", EncodeHexTx(CTransaction(mtx)));
     return result;
+}
+
+UniValue spv_estimateanchorcost(const JSONRPCRequest& request)
+{
+    CWallet* const pwallet = GetWallet(request);
+
+    RPCHelpMan{"spv_estimateanchorcost",
+        "\nEstimates current anchor cost with default fee, one input and one change output.\n",
+        {
+        },
+        RPCResult{
+            "\"cost\"                  (numeric) Estimated anchor cost (satoshies)\n"
+        },
+        RPCExamples{
+            HelpExampleCli("spv_estimateanchorcost", "")
+            + HelpExampleRpc("spv_estimateanchorcost", "")
+        },
+    }.Check(request);
+
+    auto locked_chain = pwallet->chain().lock();
+
+    // it is unable to create "pure" dummy anchor, cause it needs signing with real key
+    CAnchor const anchor = panchorauths->CreateBestAnchor(CTxDestination(PKHash()));
+    if (anchor.sigs.empty()) {
+        throw JSONRPCError(RPC_VERIFY_ERROR, "No potential anchor, can't estimate!");
+    }
+
+    CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
+    ss << anchor;
+    return UniValue(spv::EstimateAnchorCost(ToByteVector(ss)));
 }
 
 UniValue spv_rescan(const JSONRPCRequest& request)
@@ -358,7 +406,6 @@ UniValue spv_gettxconfirmations(const JSONRPCRequest& request)
     auto locked_chain = pwallet->chain().lock();
 
     return UniValue(panchors->GetAnchorConfirmations(txHash));
-//    return UniValue(spv::pspv->GetTxConfirmations(txHash));
 }
 
 UniValue spv_listanchors(const JSONRPCRequest& request)
@@ -368,7 +415,6 @@ UniValue spv_listanchors(const JSONRPCRequest& request)
     RPCHelpMan{"spv_listanchors",
         "\nList anchors (if any)\n",
         {
-//            {"height", RPCArg::Type::NUM, RPCArg::Optional::NO, "Height in btc chain"},
         },
         RPCResult{
             "\"array\"                  Returns array of anchors\n"
@@ -385,30 +431,28 @@ UniValue spv_listanchors(const JSONRPCRequest& request)
     auto locked_chain = pwallet->chain().lock();
 
     auto const * top = panchors->GetActiveAnchor();
-
+    auto const * cur = top;
     UniValue result(UniValue::VARR);
-//    auto lastHeight = spv::pspv->GetLastBlockHeight();
-    panchors->ForEachAnchorByBtcHeight([&result, &top](const CAnchorIndex::AnchorRec & rec) {
+    panchors->ForEachAnchorByBtcHeight([&result, &top, &cur](const CAnchorIndex::AnchorRec & rec) {
         UniValue anchor(UniValue::VOBJ);
         anchor.pushKV("btcBlockHeight", static_cast<int>(rec.btcHeight));
         anchor.pushKV("btcTxHash", rec.txHash.ToString());
         anchor.pushKV("defiBlockHeight", static_cast<int>(rec.anchor.height));
         anchor.pushKV("defiBlockHash", rec.anchor.blockHash.ToString());
         anchor.pushKV("confirmations", panchors->GetAnchorConfirmations(&rec));
-        if (top && top->txHash == rec.txHash)
-            anchor.pushKV("active", true);
-
+        bool const isActive = cur && cur->txHash == rec.txHash;
+        anchor.pushKV("active", isActive);
+        if (isActive) {
+            cur = panchors->GetAnchorByBtcTx(cur->anchor.previousAnchor);
+        }
         result.push_back(anchor);
     });
-    top ? top->txHash.ToString() : "";
     return result;
 }
 
 
 UniValue spv_setlastheight(const JSONRPCRequest& request)
 {
-    CWallet* const pwallet = GetWallet(request);
-
     RPCHelpMan{"spv_setlastheight",
         "\nSet last processed block height (for test purposes only)...\n",
         {
@@ -428,7 +472,6 @@ UniValue spv_setlastheight(const JSONRPCRequest& request)
     if (!fake_spv)
         throw JSONRPCError(RPC_INVALID_REQUEST, "command disabled");
 
-//    auto locked_chain = pwallet->chain().lock();
     fake_spv->lastBlockHeight = request.params[0].get_int();
     CAnchorIndex::CheckActiveAnchor(true);
     return UniValue();
@@ -439,8 +482,9 @@ static const CRPCCommand commands[] =
 { //  category          name                        actor (function)            params
   //  ----------------- ------------------------    -----------------------     ----------
   { "spv",      "spv_sendrawtx",              &spv_sendrawtx,             { "rawtx" }  },
-  { "spv",      "spv_createanchor",           &spv_createanchor,          { "rewardAddress", "hash" }  },
-  { "spv",      "spv_createanchortemplate",   &spv_createanchortemplate,  { "rewardAddress", "hash" }  },
+  { "spv",      "spv_createanchor",           &spv_createanchor,          { "inputs", "rewardAddress", "send" }  },
+  { "spv",      "spv_createanchortemplate",   &spv_createanchortemplate,  { "rewardAddress" }  },
+  { "spv",      "spv_estimateanchorcost",     &spv_estimateanchorcost,    { }  },
   { "spv",      "spv_rescan",                 &spv_rescan,                { "height" }  },
   { "spv",      "spv_syncstatus",             &spv_syncstatus,            { }  },
   { "spv",      "spv_gettxconfirmations",     &spv_gettxconfirmations,    { "txhash" }  },
