4e7f682cb1e6af8c3b88746dbf29cffe0e640638
IntegralTeam V <repo@sfxdx.ru>
2020-01-24 17:22:18 +0700

    change anchor subsidy

--- src/validation.h
+++ src/validation.h
@@ -261,7 +261,7 @@ bool GetTransaction(const uint256& hash, CTransactionRef& tx, const Consensus::P
  */
 bool ActivateBestChain(CValidationState& state, const CChainParams& chainparams, std::shared_ptr<const CBlock> pblock = std::shared_ptr<const CBlock>());
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams);
-CAmount GetAnchorSubsidy(int nHeight, const Consensus::Params& consensusParams);
+CAmount GetAnchorSubsidy(int anchorHeight, int prevAnchorHeight, const Consensus::Params& consensusParams);
 
 /** Guess verification progress (as a fraction between 0.0=genesis and 1.0=current tip). */
 double GuessVerificationProgress(const ChainTxData& data, const CBlockIndex* pindex);
