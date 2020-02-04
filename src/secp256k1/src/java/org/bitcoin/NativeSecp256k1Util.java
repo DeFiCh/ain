371da167eb7ea1aaa1c801e6a925dae967b36d8b
Integral Team M <repo@sfxdx.ru>
2020-02-04 11:22:41 +0700

    revert failed renaming of 'subtrees'

--- src/secp256k1/src/java/org/bitcoin/NativeSecp256k1Util.java
+++ src/secp256k1/src/java/org/bitcoin/NativeSecp256k1Util.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.defi;
+package org.bitcoin;
 
 public class NativeSecp256k1Util{
 
--- src/univalue/lib/univalue_escapes.h
+++ src/univalue/lib/univalue_escapes.h
@@ -1,6 +1,6 @@
 // Automatically generated file. Do not modify.
-#ifndef DEFI_UNIVALUE_UNIVALUE_ESCAPES_H
-#define DEFI_UNIVALUE_UNIVALUE_ESCAPES_H
+#ifndef BITCOIN_UNIVALUE_UNIVALUE_ESCAPES_H
+#define BITCOIN_UNIVALUE_UNIVALUE_ESCAPES_H
 static const char *escapes[256] = {
 	"\\u0000",
 	"\\u0001",
@@ -259,4 +259,4 @@ static const char *escapes[256] = {
 	NULL,
 	NULL,
 };
-#endif // DEFI_UNIVALUE_UNIVALUE_ESCAPES_H
+#endif // BITCOIN_UNIVALUE_UNIVALUE_ESCAPES_H
