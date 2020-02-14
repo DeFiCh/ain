8785e18a96afc678a37bf07f9e1bf8b7ec73feec
Ahmed Hilali <ahmed@cakedefi.com>
2020-02-14 11:22:14 +0700

    disable criminal punishments

--- src/test/setup_common.cpp
+++ src/test/setup_common.cpp
@@ -77,6 +77,7 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
 
     gArgs.ForceSetArg("-masternode_operator", "mps7BdmwEF2vQ9DREDyNPibqsuSRZ8LuwQ"); // matches with [1] masternode from regtest chainparams (and with testMasternodeKeys.begin())
     gArgs.ForceSetArg("-spv_testnet", "1");
+    fCriminals = true;
 }
 
 BasicTestingSetup::~BasicTestingSetup()
