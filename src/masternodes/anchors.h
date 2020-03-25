007d2e9023879ce3a4240ed2f653eefc10180524
Prasanna Loganathar <pvl@prasannavl.com>
2020-03-25 09:41:30 +0530

    Consistent naming

--- src/masternodes/anchors.h
+++ src/masternodes/anchors.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The DeFi Foundation
+// Copyright (c) 2019 The DeFi Blockchain Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
--- src/spv/spv_wrapper.cpp
+++ src/spv/spv_wrapper.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 DeFi Foundation
+// Copyright (c) 2019 DeFi Blockchain Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
