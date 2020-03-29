da333e313bf291251377dc7f2e77975f1e867e08
Prasanna Loganathar <pvl@prasannavl.com>
2020-03-30 05:25:33 +0530

    Versioning updates: 1.0-alpha1

--- build_msvc/defi_config.h
+++ build_msvc/defi_config.h
@@ -11,13 +11,13 @@
 #define CLIENT_VERSION_IS_RELEASE false
 
 /* Major version */
-#define CLIENT_VERSION_MAJOR 0
+#define CLIENT_VERSION_MAJOR 1
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 18
+#define CLIENT_VERSION_MINOR 0
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 99
+#define CLIENT_VERSION_REVISION 0
 
 /* Copyright holder(s) before %s replacement */
 #define COPYRIGHT_HOLDERS "The %s Developers"
@@ -346,7 +346,7 @@
 #define PACKAGE_NAME "DeFi Blockchain"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "DeFi Blockchain 0.18.99"
+#define PACKAGE_STRING "DeFi Blockchain 1.0.0-alpha1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "defi"
@@ -355,7 +355,7 @@
 #define PACKAGE_URL "https://defichain.io/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.18.99"
+#define PACKAGE_VERSION "1.0.0-alpha1"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
