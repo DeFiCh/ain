e1d15528fdf2029e26b3e6a3108732fbd0a135e8
Prasanna Loganathar <pvl@prasannavl.com>
2020-03-30 06:49:58 +0530

    Change prefix defi-blockchain

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -685,13 +685,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Defi
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Defi
-    // Mac: ~/Library/Application Support/Defi
-    // Unix: ~/.defi
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\DeFi Blockchain
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\DeFi Blockchain
+    // Mac: ~/Library/Application Support/DeFi Blockchain
+    // Unix: ~/.defi-blockchain
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Defi";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "DeFi Blockchain";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -701,10 +701,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Defi";
+    return pathRet / "Library/Application Support/DeFi Blockchain";
 #else
     // Unix
-    return pathRet / ".defi";
+    return pathRet / ".defi-blockchain";
 #endif
 #endif
 }
