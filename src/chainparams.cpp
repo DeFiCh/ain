8814b358ba3fe82be528a733790f5bedb6d15f39
Prasanna Loganathar <pvl@prasannavl.com>
2020-05-04 09:21:03 +0530

    Add dns seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -216,6 +216,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
+        vSeeds.emplace_back("seed.defichain.io");
 //        vSeeds.emplace_back("seed.bitcoin.sipa.be"); // Pieter Wuille, only supports x1, x5, x9, and xd
 //        vSeeds.emplace_back("dnsseed.bluematt.me"); // Matt Corallo, only supports x9
 //        vSeeds.emplace_back("dnsseed.bitcoin.dashjr.org"); // Luke Dashjr
@@ -358,6 +359,7 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
+       vSeeds.emplace_back("testnet-seed.defichain.io");
 //        vSeeds.emplace_back("testnet-seed.bitcoin.jonasschnelli.ch");
 //        vSeeds.emplace_back("seed.tbtc.petertodd.org");
 //        vSeeds.emplace_back("seed.testnet.bitcoin.sprovoost.nl");
