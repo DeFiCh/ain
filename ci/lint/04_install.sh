933e261dd30bb2e4b0e04c90e4a05da547869f5c
DeFiDev M <defidev@cakedefi.com>
2020-07-30 10:18:02 +0700

    lint: shellcheck new origin

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -10,6 +10,6 @@ travis_retry pip3 install codespell==1.15.0
 travis_retry pip3 install flake8==3.5.0
 travis_retry pip3 install vulture==0.29
 
-SHELLCHECK_VERSION=v0.6.0
-curl -s "https://storage.googleapis.com/shellcheck/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
+SHELLCHECK_VERSION=v0.7.1
+curl -L -s "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
 export PATH="/tmp/shellcheck-${SHELLCHECK_VERSION}:${PATH}"
