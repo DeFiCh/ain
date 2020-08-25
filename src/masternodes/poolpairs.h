c6dbd72c6129dc1f16b9591c4774176c17f2cdb7
DeFiDev M <defidev@cakedefi.com>
2020-08-25 09:44:53 +0700

    some poolpair interfaces/template, WIP

--- src/masternodes/poolpairs.h
+++ src/masternodes/poolpairs.h
@@ -8,16 +8,42 @@
 #include <flushablestorage.h>
 
 #include <amount.h>
+#include <arith_uint256.h>
 #include <masternodes/res.h>
 #include <script/script.h>
 #include <serialize.h>
 #include <uint256.h>
 
+struct CPoolPairMessage;
+
 
 class CPoolPair
 {
 public:
+    CAmount reserveA, reserveB, totalLiquidity;
+
+    arith_uint256 priceACumulativeLast, priceBCumulativeLast; // not sure about 'arith', at least sqrt() undefined
+    arith_uint256 kLast;
+    int32_t lastPoolEventHeight;
+
+    CAmount commissionPct;   // comission %% for traders
+    CAmount rewardPct;       // pool yield farming reward %%
+    CScript ownerFeeAddress;
+
+    uint256 creationTx;
+    int32_t creationHeight;
+
+    ResVal<CPoolPair> Create(CPoolPairMessage const & msg);     // or smth else
+    ResVal<CTokenAmount> AddLiquidity(CTokenAmount const & amountA, CTokenAmount amountB, CScript const & shareAddress);
+    // or:
+//    ResVal<CTokenAmount> AddLiquidity(CLiquidityMessage const & msg);
+
+    // ????
+//    ResVal<???> RemoveLiquidity(???);
+//    ResVal<???> Swap(???);
+
     ADD_SERIALIZE_METHODS;
+
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
 
@@ -27,7 +53,21 @@ public:
 class CPoolPairView : public virtual CStorageView
 {
 public:
+    Res CreatePoolPair(CPoolPair const & pool, DCT_ID & poolId);
+    Res DeletePoolPair(DCT_ID const & poolId);
 
+    boost::optional<CPoolPair> GetPoolPair(DCT_ID const & poolId) const;
+//    boost::optional<std::pair<DCT_ID, CPoolPair> > GetPoolPairGuessId(const std::string & str) const; // optional
+    boost::optional<std::pair<DCT_ID, CPoolPair> > GetPoolPair(DCT_ID const & tokenA, DCT_ID const & tokenB) const;
+
+    void ForEachPoolPair(std::function<bool(DCT_ID const & id, CPoolPair const & pool)> callback, DCT_ID const & start = DCT_ID{0});
+    void ForEachShare(std::function<bool(DCT_ID const & id, CScript const & provider)> callback, DCT_ID const & start = DCT_ID{0});
+//    void ForEachShare(std::function<bool(DCT_ID const & id, CScript const & provider, CAmount amount)> callback, DCT_ID const & start = DCT_ID{0}); // optional, with lookup into accounts
+
+    // tags
+    struct ByID { static const unsigned char prefix; }; // lsTokenID -> СPoolPair
+    struct ByPair { static const unsigned char prefix; }; // tokenA+tokenB -> lsTokenID
+    struct ByShare { static const unsigned char prefix; }; // lsTokenID+accountID -> {}
 };
 
 
