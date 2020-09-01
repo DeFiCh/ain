2c124b22694e1f9e4576a1fe8c92a1c6009f65e6
DeFiDev M <defidev@cakedefi.com>
2020-09-01 11:44:26 +0700

    pool rewards, WIP

--- src/masternodes/poolpairs.h
+++ src/masternodes/poolpairs.h
@@ -63,6 +63,7 @@ public:
     uint32_t lastPoolEventHeight;
 
     CAmount rewardPct;       // pool yield farming reward %%
+    bool swapEvent = false;
 
     uint256 creationTx;
     uint32_t creationHeight;
@@ -251,9 +252,63 @@ public:
 //    }
 
     void ForEachShare(std::function<bool(DCT_ID const & id, CScript const & provider)> callback, DCT_ID const & start = DCT_ID{0});
-//    void ForEachShare(std::function<bool(DCT_ID const & id, CScript const & provider, CAmount amount)> callback, DCT_ID const & start = DCT_ID{0}); // optional, with lookup into accounts
+    void ForEachShare(std::function<bool(DCT_ID const & id, CScript const & provider, CAmount amount)> callback, DCT_ID const & start = DCT_ID{0}); // optional, with lookup into accounts
 
 //    Res AddLiquidity(CTokenAmount const & amountA, CTokenAmount amountB, CScript const & shareAddress);
+
+    Res DistributeRewards() {
+
+        CAmount yieldFarming = GetCurrentBlockYieldFarming(); // should return actual "LP_DAILY_DFI_REWARD / 2880" or smth
+        CAmount totalDistributed = 0;
+
+
+        ForEachPoolPair([&] (DCT_ID const & poolId, CPoolPair const & pool) {
+
+            // yield farming counters
+            CAmount poolReward = yieldFarming * pool.rewardPct / COIN; // 'rewardPct' should be defined by 'setgov "LP_SPLITS"', also, it is assumed that it was totally validated and normalized to 100%
+            CAmount distributedFeeA = 0;
+            CAmount distributedFeeB = 0;
+
+            if (!pool.swapEvent && poolReward == 0) {
+                return true; // no events, skip to the next pool
+            }
+
+            ForEachShare([&] (DCT_ID const & currentId, CScript const & provider, CAmount liquidity) {
+                if (currentId != poolId) {
+                    return false; // stop
+                }
+
+                // distribute trading fees
+                if (pool.swapEvent) {
+                    CAmount feeA = pool.blockCommissionA * liquidity / pool.totalLiquidity;
+                    distributedFeeA += feeA;
+                    onTransfer(provider, {pool.idTokenA, feeA});
+
+                    CAmount feeB = pool.blockCommissionB * liquidity / pool.totalLiquidity;
+                    distributedFeeB += feeB;
+                    onTransfer(provider, {pool.idTokenB, feeB});
+                }
+
+                // distribute yield farming
+                if (poolReward) {
+                    CAmount providerReward = poolReward * liquidity / pool.totalLiquidity;
+                    if (providerReward) {
+                        onTransfer(provider, {DCT_ID{0}, providerReward});
+                        totalDistributed += providerReward;
+                    }
+                }
+            }, poolId);
+
+            pool.blockCommissionA -= distributedFeeA;
+            pool.blockCommissionB -= distributedFeeB;
+            pool.swapEvent = false;
+
+            SetPoolPair(poolId, pool);
+        });
+
+/// @todo   return/set somehow  (yieldFarming - totalDistributed)
+        return Res::Ok();
+    }
 
     // tags
     struct ByID { static const unsigned char prefix; }; // lsTokenID -> СPoolPair
