230c995b9788514611f42becbebeb968c88072e6
DeFiDev M <defidev@cakedefi.com>
2020-09-02 17:24:38 +0700

    new community funds view

---
+++ src/masternodes/funding.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 DeFi Blockchain Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DEFI_MASTERNODES_FUNDING_H
+#define DEFI_MASTERNODES_FUNDING_H
+
+#include <flushablestorage.h>
+#include <masternodes/res.h>
+//#include <masternodes/balances.h>
+#include <amount.h>
+//#include <script/script.h>
+
+enum class CommunityAccountType : unsigned char
+{
+    None = 0,
+    IncentiveFunding    = 'I', // or 'yield farming' - source of community rewards for LP (Liquidity Pools)
+    AnchorReward        = 'A',
+};
+
+inline CommunityAccountType CommunityAccountCodeToType (unsigned char ch) {
+    char const types[] = "IA";
+    if (memchr(types, ch, strlen(types)))
+        return static_cast<CommunityAccountType>(ch);
+    else
+        return CommunityAccountType::None;
+}
+
+
+class CFundingView : public virtual CStorageView
+{
+public:
+    CAmount GetCommunityBalance(CommunityAccountType account) const;
+    Res SetCommunityBalance(CommunityAccountType account, CAmount amount);
+
+    void ForEachCommunityBalance(std::function<bool(CommunityAccountType account, CAmount amount)> callback) const;
+
+    Res AddCommunityBalance(CommunityAccountType account, CAmount amount);
+    Res SubCommunityBalance(CommunityAccountType account, CAmount amount);
+
+    // tags
+    struct ById { static const unsigned char prefix; };
+};
+
+#endif //DEFI_MASTERNODES_FUNDING_H
