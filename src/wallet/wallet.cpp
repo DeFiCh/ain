646840cd2033cae708b234d5a5d7eea5f92f3241
ShengguangXiao <shengguangxiao@gmail.com>
2020-09-17 13:45:21 +0800

    Fix compile error

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3006,8 +3006,8 @@ bool CWallet::CreateTransaction(interfaces::Chain::Lock& locked_chain, const std
 
     CMutableTransaction txNew;
 
-    if (!vTokenValues.empty())
-        txNew.nVersion = CTransaction::TOKENS_MIN_VERSION;
+    //if (!vTokenValues.empty())
+    //    txNew.nVersion = CTransaction::TOKENS_MIN_VERSION;
 
     txNew.nLockTime = GetLocktimeForNewTransaction(chain(), locked_chain);
 
