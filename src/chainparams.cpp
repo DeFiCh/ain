be1b5eeafbf88d61ffc2869eb1a9afd1f8f75433
monstrobishi <monstrobishi@gmail.com>
2020-10-20 13:16:34 +0800

    Testnet bayfront height

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -274,7 +274,7 @@ public:
         consensus.BIP65Height = 0; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 0; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
         consensus.AMKHeight = 324500;
-        consensus.BayfrontHeight = 378000;
+        consensus.BayfrontHeight = 388500;
 
         consensus.pos.diffLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 //        consensus.pos.nTargetTimespan = 14 * 24 * 60 * 60; // two weeks
