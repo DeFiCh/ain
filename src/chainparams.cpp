d2ca1b3c6cee0419ec948354ba913e46d7d70eef
monstrobishi <monstrobishi@gmail.com>
2020-10-20 15:27:58 +0800

    Updated testnet height

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -273,8 +273,8 @@ public:
         consensus.BIP34Hash = uint256();
         consensus.BIP65Height = 0; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 0; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
-        consensus.AMKHeight = 324500;
-        consensus.BayfrontHeight = 388500;
+        consensus.AMKHeight = 150;
+        consensus.BayfrontHeight = 3000;
 
         consensus.pos.diffLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 //        consensus.pos.nTargetTimespan = 14 * 24 * 60 * 60; // two weeks
