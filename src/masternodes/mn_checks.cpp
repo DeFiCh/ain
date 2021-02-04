6fe109bd020dabf8e9cb3423e4ad5a5d9d5b5eaf
Anthony Fieroni <bvbfan@abv.bg>
2021-02-04 09:26:43 +0200

    Fix swap max price calculation
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

--- src/masternodes/mn_checks.cpp
+++ src/masternodes/mn_checks.cpp
@@ -1291,7 +1291,7 @@ Res ApplyPoolSwapTx(CCustomCSView &mnview, const CCoinsViewCache &coins, const C
         }
 
         return Res::Ok();
-    }, static_cast<int>(height) >= consensusParams.BayfrontGardensHeight);
+    }, static_cast<int>(height));
 
     if (!res.ok) {
         return Res::Err("%s: %s", __func__, res.msg);
