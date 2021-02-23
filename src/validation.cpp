181f9be7864ad9a205ff1383e01b0bf1d059378b
Anthony Fieroni <bvbfan@abv.bg>
2021-02-23 03:15:01 +0200

    Add ability to interrupt long-standing revert to previous checkpoint  (#233)
    * Respect shutdown request at block disconnect to previous checkpoint
    
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>
    
    * Add new checkpoints
    
    Co-authored-by: Ahmed Hilali <monstrobishi@gmail.com>

--- src/validation.cpp
+++ src/validation.cpp
@@ -2930,6 +2930,8 @@ bool CChainState::ActivateBestChainStep(CValidationState& state, const CChainPar
     DisconnectedBlockTransactions disconnectpool;
     auto disconnectBlocksTo = [&](const CBlockIndex *pindex) -> bool {
         while (m_chain.Tip() && m_chain.Tip() != pindex) {
+            boost::this_thread::interruption_point();
+
             if (!DisconnectTip(state, chainparams, &disconnectpool)) {
                 // This is likely a fatal error, but keep the mempool consistent,
                 // just in case. Only remove from the mempool in this case.
@@ -2941,6 +2943,9 @@ bool CChainState::ActivateBestChainStep(CValidationState& state, const CChainPar
                 return AbortNode(state, "Failed to disconnect block; see debug.log for details");
             }
             fBlocksDisconnected = true;
+
+            if (ShutdownRequested())
+                break;
         }
         return true;
     };
