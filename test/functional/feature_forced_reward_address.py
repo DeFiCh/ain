8b856120460ec8936eac815a71257d0c5de7a968
DefiDev <defidev@cakedefi.com>
2021-03-29 03:33:13 -0700

    linter fix

--- test/functional/feature_forced_reward_address.py
+++ test/functional/feature_forced_reward_address.py
@@ -7,10 +7,7 @@ from test_framework.test_framework import DefiTestFramework
 from test_framework.util import (
     assert_equal,
     assert_raises_rpc_error,
-    connect_nodes_bi,
 )
-
-import time
 
 class TestForcedRewardAddress(DefiTestFramework):
     def set_test_params(self):
