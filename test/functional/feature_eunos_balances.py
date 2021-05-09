6a320cc8a4d568eb25f661b3eabfcd79bcea7de2
Peter Bushnell <bushsolo@gmail.com>
2021-05-09 07:20:04 +0100

    Remove unused variables

--- test/functional/feature_eunos_balances.py
+++ test/functional/feature_eunos_balances.py
@@ -7,7 +7,6 @@
 
 from test_framework.test_framework import DefiTestFramework
 
-from test_framework.authproxy import JSONRPCException
 from test_framework.util import assert_equal
 
 from decimal import Decimal
