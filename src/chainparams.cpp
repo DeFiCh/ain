fdac288aa8db6c342eaf4fe8e411254ccbb71f65
Anthony Fieroni <bvbfan@abv.bg>
2021-05-27 08:48:20 +0300

    Add checkpoint to 875000 as well
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -303,6 +303,7 @@ public:
                 {650000, uint256S("f18d64dd75c53590e833d3068132a65644963d5c5aebb4c73d42cbde8dc28d68")},
                 {757420, uint256S("8d4918be2b2df30175f9e611d9ceb494215b93f2267075ace3f031e784cbccbe")},
                 {850000, uint256S("2d7d58ae18a74f73b9836a8fffd3f65ce409536e654a6c644ce735215238a004")},
+                {875000, uint256S("44d3b3ba8e920cef86b7ec096ab0a2e608d9fedc14a59611a76a5e40aa53145e")},
             }
         };
 
