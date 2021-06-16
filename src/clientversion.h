5e3e3b3fb0531881e72d129b009cc4a5c17ca67d
Peter Bushnell <bushsolo@gmail.com>
2021-06-16 10:26:32 +0100

    Restore previous defining of CLIENT_VERSION

--- src/clientversion.h
+++ src/clientversion.h
@@ -35,6 +35,12 @@
 #include <string>
 #include <vector>
 
+static const int CLIENT_VERSION =
+        1000000 * CLIENT_VERSION_MAJOR
+        +   10000 * CLIENT_VERSION_MINOR
+        +     100 * CLIENT_VERSION_REVISION
+        +       1 * CLIENT_VERSION_BUILD;
+
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
 
