6331671e13d1a86516ed9ee0605a4cfb3c39a597
Peter Bushnell <bushsolo@gmail.com>
2021-09-06 11:34:51 +0100

    Remove boost::chrono and boost::sleep

--- src/wallet/db.cpp
+++ src/wallet/db.cpp
@@ -759,7 +759,7 @@ bool BerkeleyBatch::Rewrite(BerkeleyDatabase& database, const char* pszSkip)
                 return fSuccess;
             }
         }
-        MilliSleep(100);
+        UninterruptibleSleep(std::chrono::milliseconds{100});
     }
 }
 
@@ -891,7 +891,7 @@ bool BerkeleyDatabase::Backup(const std::string& strDest)
                 }
             }
         }
-        MilliSleep(100);
+        UninterruptibleSleep(std::chrono::milliseconds{100});
     }
 }
 
