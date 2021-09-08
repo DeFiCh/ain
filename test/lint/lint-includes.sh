8c28b8761e0221f24dc6b8ba3f32f7351d32d6b1
Peter Bushnell <bushsolo@gmail.com>
2021-09-08 17:31:22 +0100

    Replace Boost shared_ptr and scoped_ptr

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -68,8 +68,6 @@ EXPECTED_BOOST_INCLUDES=(
     boost/preprocessor/cat.hpp
     boost/preprocessor/stringize.hpp
     boost/range/adaptor/transformed.hpp
-    boost/scoped_ptr.hpp
-    boost/shared_ptr.hpp
     boost/signals2/connection.hpp
     boost/signals2/last_value.hpp
     boost/signals2/signal.hpp
