ce613b895725db7c7468011f70ca89b04b82d734
Peter Bushnell <bushsolo@gmail.com>
2021-09-09 12:09:16 +0100

    Remove Boost Chrono and Thread from build system

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -74,8 +74,6 @@ EXPECTED_BOOST_INCLUDES=(
     boost/signals2/last_value.hpp
     boost/signals2/signal.hpp
     boost/test/unit_test.hpp
-    boost/thread.hpp
-    boost/thread/thread.hpp
     boost/variant.hpp
     boost/variant/apply_visitor.hpp
     boost/variant/static_visitor.hpp
