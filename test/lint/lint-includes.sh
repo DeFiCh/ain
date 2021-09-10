45e8ceab9d3e6d0b10aad6534e927a2ad90e0b55
Peter Bushnell <bushsolo@gmail.com>
2021-09-10 10:59:51 +0100

    Remove Boost case_conv

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -47,7 +47,6 @@ fi
 
 EXPECTED_BOOST_INCLUDES=(
     boost/algorithm/string.hpp
-    boost/algorithm/string/case_conv.hpp
     boost/algorithm/string/classification.hpp
     boost/algorithm/string/join.hpp
     boost/algorithm/string/replace.hpp
