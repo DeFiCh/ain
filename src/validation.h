4706c636c255a12289fb7a34afe2d1285557f788
Xiao Shengguang <shengguangxiao@gmail.com>
2021-09-13 19:10:58 +0800

    Refactor code in connectblock to seperate functions (#733)
    * Refactor code in connectblock to seperate functions

--- src/validation.h
+++ src/validation.h
@@ -750,6 +750,10 @@ private:
 
     //! Mark a block as not having block data
     void EraseBlockData(CBlockIndex* index) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+
+    static void ProcessICXEvents(const CBlockIndex* pindex, CCustomCSView& cache, const CChainParams& chainparams);
+
+    static void ProcessLoanEvents(const CBlockIndex* pindex, CCustomCSView& cache, const CChainParams& chainparams);
 };
 
 /** Mark a block as precious and reorganize.
