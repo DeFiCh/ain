6e32c30d498573c79488fa0aa0172cde5aecd3e4
Diego del Corral Tercero <55594560+hidiego@users.noreply.github.com>
2021-10-08 11:05:49 +0200

    Fix leak of FixedInternalPrice validity internal impl details (#773)

--- src/masternodes/oracles.h
+++ src/masternodes/oracles.h
@@ -101,11 +101,13 @@ struct COracle : public CAppointOracleMessage {
 
 struct CFixedIntervalPrice
 {
+private:
+    bool isValidInternal(const int64_t deviationThreshold) const; // 0-1 value for deviation threshold
 public:
     CFixedIntervalPriceId priceFeedId;
     int64_t timestamp;
     std::vector<CAmount> priceRecord{0, 0}; // priceHistory[0] = active price, priceHistory[1] = next price
-    bool isValid(const int64_t deviationThreshold = 3 * COIN / 10) const; // 0-1 value for deviation threshold
+    bool isValid() const;
 
     ADD_SERIALIZE_METHODS;
 
