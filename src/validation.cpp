0df1f86b27eef503b9955164c519b8b3306e897d
Anthony Fieroni <bvbfan@abv.bg>
2021-11-16 16:30:58 +0200

    Over majority threshold
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

--- src/validation.cpp
+++ src/validation.cpp
@@ -3786,7 +3786,7 @@ void CChainState::ProcessProposalEvents(const CBlockIndex* pindex, CCustomCSView
             return true;
         }, CMnVotePerCycle{propId, prop.cycle});
 
-        if (lround(voters * 10000.f / activeMasternodes.size()) < chainparams.GetConsensus().props.minVoting) {
+        if (lround(voters * 10000.f / activeMasternodes.size()) <= chainparams.GetConsensus().props.minVoting) {
             cache.UpdatePropStatus(propId, pindex->nHeight, CPropStatusType::Rejected);
             return true;
         }
@@ -3796,7 +3796,7 @@ void CChainState::ProcessProposalEvents(const CBlockIndex* pindex, CCustomCSView
         case CPropType::CommunityFundRequest:
             majorityThreshold = chainparams.GetConsensus().props.cfp.majorityThreshold;
             break;
-        case CPropType::BlockRewardRellocation:
+        case CPropType::BlockRewardReallocation:
             majorityThreshold = chainparams.GetConsensus().props.brp.majorityThreshold;
             break;
         case CPropType::VoteOfConfidence:
@@ -3804,7 +3804,7 @@ void CChainState::ProcessProposalEvents(const CBlockIndex* pindex, CCustomCSView
             break;
         }
 
-        if (lround(voteYes * 10000.f / voters) < majorityThreshold) {
+        if (lround(voteYes * 10000.f / voters) <= majorityThreshold) {
             cache.UpdatePropStatus(propId, pindex->nHeight, CPropStatusType::Rejected);
             return true;
         }
