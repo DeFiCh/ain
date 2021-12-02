dfe93eaabeeb252c37ce74f89e0d03b1201815da
Jouzo <jdesclercs@gmail.com>
2021-12-02 01:20:31 +0100

    Adds estimatevault RPC (#933)
    * GetAmountInCurrency utility function
    * Adds estimatevault RPC
    * Use operator bool() directly instead of ok
    
    Co-authored-by: Prasanna Loganathar <pvl@prasannavl.com>

--- src/masternodes/masternodes.h
+++ src/masternodes/masternodes.h
@@ -429,6 +429,8 @@ public:
 
     bool CalculateOwnerRewards(CScript const & owner, uint32_t height);
 
+    ResVal<CAmount> GetAmountInCurrency(CAmount amount, CTokenCurrencyPair priceFeedId, bool useNextPrice = false, bool requireLivePrice = true);
+
     ResVal<CCollateralLoans> GetLoanCollaterals(CVaultId const & vaultId, CBalances const & collaterals, uint32_t height, int64_t blockTime, bool useNextPrice = false, bool requireLivePrice = true);
 
     void SetDbVersion(int version);
