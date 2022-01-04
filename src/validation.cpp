b677df77287b575118b1d764b32a8465bcf0deef
Peter Bushnell <bushsolo@gmail.com>
2022-01-04 08:37:26 +0000

    Use correct height field

--- src/validation.cpp
+++ src/validation.cpp
@@ -4578,8 +4578,8 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
     if (block.GetHash() != consensusParams.hashGenesisBlock) {
         TBytes dummy;
         uint8_t hashForks{HasForks::None};
-        hashForks |= block.height >= static_cast<uint32_t>(consensusParams.FortCanningHeight) ? HasForks::FortCanning : HasForks::None;
-        hashForks |= block.height >= static_cast<uint32_t>(consensusParams.GreatWorldHeight) ? HasForks::GreatWorld : HasForks::None;
+        hashForks |= height >= static_cast<uint32_t>(consensusParams.FortCanningHeight) ? HasForks::FortCanning : HasForks::None;
+        hashForks |= height >= static_cast<uint32_t>(consensusParams.GreatWorldHeight) ? HasForks::GreatWorld : HasForks::None;
         for (unsigned int i = 1; i < block.vtx.size(); i++) {
             if (block.vtx[i]->IsCoinBase() &&
                 !IsAnchorRewardTx(*block.vtx[i], dummy, hashForks) &&
