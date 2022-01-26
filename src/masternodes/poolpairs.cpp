ea1a283875c2203e453d2782960e928667630dc4
Prasanna Loganathar <pvl@prasannavl.com>
2022-01-26 19:23:32 +0530

    Add pre FCH guard

--- src/masternodes/poolpairs.cpp
+++ src/masternodes/poolpairs.cpp
@@ -391,6 +391,11 @@ Res CPoolPair::Swap(CTokenAmount in, CAmount dexfeeInPct, PoolPrice const & maxP
     if (in.nTokenId != idTokenA && in.nTokenId != idTokenB)
         return Res::Err("Error, input token ID (" + in.nTokenId.ToString() + ") doesn't match pool tokens (" + idTokenA.ToString() + "," + idTokenB.ToString() + ")");
 
+    if (height < Params().GetConsensus().FortCanningHillHeight) {
+        if (in.nValue <= 0)
+            return Res::Err("Input amount should be positive!");
+    }
+
     if (!status)
         return Res::Err("Pool trading is turned off!");
 
