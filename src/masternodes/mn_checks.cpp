668ec14d6c767a6cc2a4c41e300579600e7b54c2
Diego del Corral <hi@dcorral.com>
2022-02-03 14:18:57 +0100

    Move pairSymbol length check to RPC level

--- src/masternodes/mn_checks.cpp
+++ src/masternodes/mn_checks.cpp
@@ -1120,14 +1120,11 @@ public:
         }
 
         const auto symbolLength = height >= consensus.FortCanningHeight ? CToken::MAX_TOKEN_POOLPAIR_LENGTH : CToken::MAX_TOKEN_SYMBOL_LENGTH;
-        if (pairSymbol.length() > symbolLength) {
-            return Res::Err("pairSymbol is larger than %s", symbolLength);
-        }
         if (pairSymbol.empty()) {
             pairSymbol = trim_ws(tokenA->symbol + "-" + tokenB->symbol).substr(0, symbolLength);
         } else {
             pairSymbol = trim_ws(pairSymbol).substr(0, symbolLength);
-        }         
+        }
 
         CTokenImplementation token;
         token.flags = (uint8_t)CToken::TokenFlags::DAT |
