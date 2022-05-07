18bb272cb2acd8825b2d0f14dfbe00af4605cef6
Peter Bushnell <bushsolo@gmail.com>
2022-05-07 11:03:48 +0100

    Resolve issues after merge

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -177,7 +177,9 @@ bool Consensus::CheckTxInputs(const CTransaction& tx, CValidationState& state, c
 
     if (NotAllowedToFail(txType, nSpendHeight) || (nSpendHeight >= chainparams.GetConsensus().GreatWorldHeight && txType == CustomTxType::UpdateMasternode)) {
         CCustomCSView discardCache(mnview);
-        auto res = ApplyCustomTx(discardCache, inputs, tx, chainparams.GetConsensus(), nSpendHeight, 0, &canSpend);
+        CFutureSwapView futureSwapView(*pfutureSwapView);
+        CUndosView undosView(*pundosView);
+        auto res = ApplyCustomTx(discardCache, futureSwapView, undosView, inputs, tx, chainparams.GetConsensus(), nSpendHeight, 0, &canSpend);
         if (!res.ok && (res.code & CustomTxErrCodes::Fatal) && txType != CustomTxType::UpdateMasternode) {
             return state.Invalid(ValidationInvalidReason::CONSENSUS, false, REJECT_INVALID, "bad-txns-customtx", res.msg);
         }
@@ -226,20 +228,6 @@ bool Consensus::CheckTxInputs(const CTransaction& tx, CValidationState& state, c
     // after fee calc it is guaranteed that both values[0] exists (even if zero)
     if (tx.nVersion < CTransaction::TOKENS_MIN_VERSION && (nValuesIn.size() > 1 || non_minted_values_out.size() > 1)) {
         return state.Invalid(ValidationInvalidReason::CONSENSUS, false, REJECT_INVALID, "bad-txns-tokens-in-old-version-tx");
-    }
-
-    // check for tokens values
-    std::vector<unsigned char> dummy;
-    const auto txType = GuessCustomTxType(tx, dummy);
-
-    if (NotAllowedToFail(txType, nSpendHeight)) {
-        CCustomCSView discardCache(mnview);
-        CFutureSwapView futureSwapView(*pfutureSwapView);
-        CUndosView undosView(*pundosView);
-        auto res = ApplyCustomTx(discardCache, futureSwapView, undosView, inputs, tx, chainparams.GetConsensus(), nSpendHeight);
-        if (!res.ok && (res.code & CustomTxErrCodes::Fatal)) {
-            return state.Invalid(ValidationInvalidReason::CONSENSUS, false, REJECT_INVALID, "bad-txns-customtx", res.msg);
-        }
     }
 
     for (auto const & kv : non_minted_values_out) {
