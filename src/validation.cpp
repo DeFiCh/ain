0bde20ea181f2d84ffccdd537a6b34d5af622fba
Peter Bushnell <bushsolo@gmail.com>
2022-05-23 08:10:53 +0100

    Set rate height

--- src/validation.cpp
+++ src/validation.cpp
@@ -4203,6 +4203,8 @@ static Res VaultSplits(CCustomCSView& view, CFutureSwapView& futureSwapView, ATT
             rate.interestPerBlock = InterestPerBlockCalculationV2(amounts->balances[newTokenId], loanToken->interest, loanSchemeRate);
         }
 
+        rate.height = height;
+
         view.WriteInterestRate(std::make_pair(vaultId, newTokenId), rate, height);
     }
 
