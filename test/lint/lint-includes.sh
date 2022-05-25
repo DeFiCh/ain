fd35eb95db2306ac0eb0c3f9300bd4d18c074840
Prasanna Loganathar <pvl@prasannavl.com>
2022-05-25 21:57:58 +0530

    Fix lint

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -56,9 +56,11 @@ EXPECTED_BOOST_INCLUDES=(
     boost/algorithm/string/replace.hpp
     boost/algorithm/string/split.hpp
     boost/chrono/chrono.hpp
+    boost/circular_buffer.hpp
     boost/date_time/posix_time/posix_time.hpp
     boost/filesystem.hpp
     boost/filesystem/fstream.hpp
+    boost/multiprecision/cpp_int.hpp
     boost/multi_index/hashed_index.hpp
     boost/multi_index/ordered_index.hpp
     boost/multi_index/sequenced_index.hpp
@@ -85,8 +87,7 @@ EXPECTED_BOOST_INCLUDES=(
     boost/variant.hpp
     boost/variant/apply_visitor.hpp
     boost/variant/static_visitor.hpp
-    boost/multiprecision/cpp_int.hpp
-    boost/circular_buffer.hpp
+    boost/asio.hpp
 )
 
 for BOOST_INCLUDE in $(git grep '^#include <boost/' -- "*.cpp" "*.h" | cut -f2 -d: | cut -f2 -d'<' | cut -f1 -d'>' | sort -u); do
