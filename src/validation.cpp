c297accc2157b153c3c8212c9bf3ac540953553c
Prasanna Loganathar <pvl@prasannavl.com>
2022-05-27 15:13:57 +0530

    Avoid duplication

--- src/validation.cpp
+++ src/validation.cpp
@@ -4278,16 +4278,20 @@ static Res VaultSplits(CCustomCSView& view, ATTRIBUTES& attributes, const DCT_ID
         if (LogAcceptCategory(BCLog::TOKEN_SPLIT)) {
             auto s1 = GetInterestPerBlockHighPrecisionString(oldInterestPerBlock);
             auto s2 = GetInterestPerBlockHighPrecisionString(newInterestRatePerBlock);
+            std::string s1Str;
+            std::string s2Str;
             if (s1 && s1) {
-                LogPrint(BCLog::TOKEN_SPLIT, "TokenSplit: V Interest (%s: %s => %s, %s => %s)\n",
-                    vaultId.ToString(), oldRateToHeight.ToString(), newRateToHeight.ToString(),
-                    *s1, *s2);
-            } else {
-                LogPrint(BCLog::TOKEN_SPLIT, "WARNING: TokenSplit GetInterestPerBlockHighPrecisionString failed\n");
-                LogPrint(BCLog::TOKEN_SPLIT, "TokenSplit: V Interest (%s: %s => %s, %s => %s)\n",
-                    vaultId.ToString(), oldRateToHeight.ToString(), newRateToHeight.ToString(),
-                    oldInterestPerBlock.ToString(), newInterestRatePerBlock.ToString());
+                s1Str = *s1;
+                s2Str = *s2;
             }
+            else {
+                s1Str = oldInterestPerBlock.ToString();
+                s2Str = newInterestRatePerBlock.ToString();
+                LogPrint(BCLog::TOKEN_SPLIT, "WARNING: TokenSplit GetInterestPerBlockHighPrecisionString failed\n");
+            }
+            LogPrint(BCLog::TOKEN_SPLIT, "TokenSplit: V Interest (%s: %s => %s, %s => %s)\n",
+                vaultId.ToString(), oldRateToHeight.ToString(), newRateToHeight.ToString(),
+                s1Str, s2Str);
         }
 
         view.WriteInterestRate(std::make_pair(vaultId, newTokenId), rate, rate.height);
