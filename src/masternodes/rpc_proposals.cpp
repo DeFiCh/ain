00c9f85929aac0da4a1278f997b231df5bef0a60
Chanaka Sameera <kchanakasameera@gmail.com>
2022-05-31 15:54:42 +0530

    Added context field into json object returned by getproposal

--- src/masternodes/rpc_proposals.cpp
+++ src/masternodes/rpc_proposals.cpp
@@ -8,6 +8,7 @@ UniValue propToJSON(CPropId const& propId, CPropObject const& prop)
     UniValue ret(UniValue::VOBJ);
     ret.pushKV("proposalId", propId.GetHex());
     ret.pushKV("title", prop.title);
+    ret.pushKV("context", prop.context);
     auto type = static_cast<CPropType>(prop.type);
     ret.pushKV("type", CPropTypeToString(type));
     auto status = static_cast<CPropStatusType>(prop.status);
