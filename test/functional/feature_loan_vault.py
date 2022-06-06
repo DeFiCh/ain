a063c82fb82f8ef80497469db284c88bec11b1da
Prasanna Loganathar <pvl@prasannavl.com>
2022-06-06 12:41:39 +0530

    Revert "Test add missing tests + fix index filtering listauctionhistory (#1215)"
    This reverts commit a47808bc1bad66f0c96355d81fcefc3f685d3eee.

--- test/functional/feature_loan_vault.py
+++ test/functional/feature_loan_vault.py
@@ -289,38 +289,21 @@ class VaultTest (DefiTestFramework):
         list_vault = self.nodes[0].listvaults()
         assert_equal(len(list_vault), 4)
 
-        # Filetering
-        # by owner_address
+        # check listVaults filter by owner_address
         list_vault = self.nodes[0].listvaults({ "ownerAddress": self.owner_addresses[1] })
         assert_equal(len(list_vault), 3)
         for vault in list_vault:
             assert_equal(vault["ownerAddress"], self.owner_addresses[1])
 
-        # by loanSchemeId
+        # check listVaults filter by loanSchemeId
         list_vault = self.nodes[0].listvaults({ "loanSchemeId": "LOAN0003" })
         assert_equal(len(list_vault), 2)
         for vault in list_vault:
             assert_equal(vault["loanSchemeId"], "LOAN0003")
 
-        # Pagination
-        # limit
+        # check listVaults pagination
         list_vault = self.nodes[0].listvaults({}, {"limit": 1})
         assert_equal(len(list_vault), 1)
-
-        # including_include_start
-        list_vault_include_start = self.nodes[0].listvaults({}, {"limit": 1, "including_start": False})
-        assert_equal(len(list_vault_include_start), 1)
-        assert(list_vault[0]['vaultId'] != list_vault_include_start[0]['vaultId'])
-        startId = list_vault[0]['vaultId']
-
-        # start
-        list_vault_start = self.nodes[0].listvaults({}, {"start": startId})
-        assert_equal(len(list_vault_start), 3)
-
-        # start & include_start
-        list_vault_start = self.nodes[0].listvaults({}, {"start": startId, "including_start": True})
-        assert_equal(len(list_vault_start), 4)
-
 
     def test_feeburn(self):
         assert_equal(self.nodes[0].getburninfo()['feeburn'], Decimal('7'))
@@ -557,17 +540,6 @@ class VaultTest (DefiTestFramework):
 
         assert_raises_rpc_error(-26, 'Vault is under liquidation', self.nodes[0].closevault, self.vaults[0], self.owner_addresses[0])
 
-    def listvaults_state_filtering(self):
-        # check listvaults
-        list_vault = self.nodes[0].listvaults()
-        assert_equal(len(list_vault), 4)
-
-        list_vault = self.nodes[0].listvaults({"state": "active"})
-        assert_equal(len(list_vault), 3)
-
-        list_vault = self.nodes[0].listvaults({"state": "inLiquidation"})
-        assert_equal(len(list_vault), 1)
-
     def updatevault_to_scheme_with_lower_collateralization_ratio(self):
         self.nodes[0].deposittovault(self.vaults[1], self.accountDFI, '2.5@DFI')
         self.nodes[0].generate(1)
@@ -602,12 +574,14 @@ class VaultTest (DefiTestFramework):
             error_str = e.error['message']
         assert("Vault <"+self.vaults[1]+"> has loans" in error_str)
         self.nodes[0].generate(1)
+        self.sync_blocks()
 
     def test_closevault(self):
         list_vault = self.nodes[1].listvaults()
         assert_equal(len(list_vault), 4)
         self.nodes[0].closevault(self.vaults[3], self.owner_addresses[1])
         self.nodes[0].generate(1)
+        self.sync_blocks()
         list_vault = self.nodes[1].listvaults()
         assert_equal(len(list_vault), 3)
         try:
@@ -763,77 +737,6 @@ class VaultTest (DefiTestFramework):
         self.nodes[0].generate(1)
         self.nodes[0].closevault(vault_id, address)
         self.nodes[0].generate(1)
-
-    def close_vault_check_burninfo_and_owner_close_fee_payback(self):
-        self.sync_blocks()
-        # Save burninfo and balances before create vault
-        balance1Before = self.nodes[1].getbalance()
-        burninfoBefore = self.nodes[1].getburninfo()
-
-        # Create vault
-        owner1 = self.nodes[1].getnewaddress()
-        vaultId = self.nodes[1].createvault(owner1)
-        self.nodes[1].generate(1)
-
-        # Save burninfo and balances after create vault
-        burninfoAfterCreate = self.nodes[1].getburninfo()
-        balance1AfterCreate = self.nodes[1].getbalance()
-
-        # Close vault
-        self.nodes[1].closevault(vaultId, owner1)
-        self.nodes[1].generate(1)
-
-        # Save burninfo and balances after close vault
-        account1AfterClose = self.nodes[1].getaccount(owner1)[0]
-        burninfoAfterClose = self.nodes[1].getburninfo()
-
-        # checks
-        assert_equal(balance1Before-balance1AfterCreate, Decimal('1.00004640')) # ~1 UTXO charged on createvault
-        assert_equal(burninfoBefore['feeburn']-burninfoAfterCreate['feeburn'], Decimal('-0.50000000')) # 0.5 DFI burned on createvault
-        assert_equal(account1AfterClose, '0.50000000@DFI') # 0.5 DFI returned to owner
-        assert_equal(burninfoAfterClose['feeburn'], burninfoAfterCreate['feeburn']) # No more DFI is burned on close vault
-
-    def close_vault_after_update_owner_check_close_fee_payback(self):
-        self.sync_blocks()
-        owner1 = self.nodes[1].getnewaddress()
-        owner2 = self.nodes[0].getnewaddress()
-
-        # Save burninfo and balances before create vault
-        balance1Before = self.nodes[1].getbalance()
-
-        # Create vault
-        vaultId = self.nodes[1].createvault(owner1)
-        self.nodes[1].generate(1)
-
-        # Save burninfo and balances after create vault
-        balance1AfterCreate = self.nodes[1].getbalance()
-
-        # Update vault to owner2
-        self.nodes[1].updatevault(vaultId, {"ownerAddress": owner2})
-        self.nodes[1].generate(1)
-        self.sync_blocks()
-        vault = self.nodes[0].getvault(vaultId)
-        assert_equal(owner2, vault['ownerAddress'])
-
-        # Close vault without auth
-        try:
-            self.nodes[1].closevault(vaultId, owner1)
-        except JSONRPCException as e:
-            errorString = e.error['message']
-        assert('Incorrect authorization for' in errorString)
-
-        self.nodes[0].closevault(vaultId, owner1)
-        self.nodes[0].generate(1)
-
-        self.sync_blocks()
-        # Save burninfo and balances after close vault
-        account1AfterClose = self.nodes[1].getaccount(owner1)
-        account2AfterClose = self.nodes[1].getaccount(owner2)
-
-        # checks
-        assert_equal(balance1Before-balance1AfterCreate, Decimal('1.00003540')) # ~1 UTXO charged on createvault
-        assert_equal(account1AfterClose, ['0.50000000@DFI']) # 0.5 DFI returned to owner1
-        assert_equal([], account2AfterClose) # close vault called with owner1
 
     def loan_and_collateral_token_to_govvar(self):
         # Move to hard fork
@@ -1072,7 +975,6 @@ class VaultTest (DefiTestFramework):
         self.withdraw_breaking_50pctDFI_rule()
         self.move_interest_from_old_scheme()
         self.vault_enter_liquidation_updating_oracle()
-        self.listvaults_state_filtering()
         self.updatevault_to_scheme_with_lower_collateralization_ratio()
         self.closevault_with_active_loans()
         self.test_closevault()
@@ -1081,8 +983,6 @@ class VaultTest (DefiTestFramework):
         self.test_50pctDFI_fresh_vault_takeloan_withdraw()
         self.test_50pctDFI_rule_after_BTC_price_increase()
         self.overflowed_collateral_value()
-        self.close_vault_check_burninfo_and_owner_close_fee_payback()
-        self.close_vault_after_update_owner_check_close_fee_payback()
         self.loan_and_collateral_token_to_govvar()
 
 if __name__ == '__main__':
