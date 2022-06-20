2dda0f7f164f9d8254e357fd39545cf76023e21d
Peter John Bushnell <bushsolo@gmail.com>
2022-06-20 12:05:37 +0100

    Add FortCanningGardens (#1350)

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -96,6 +96,7 @@ struct Params {
     int FortCanningHillHeight;
     int FortCanningRoadHeight;
     int FortCanningCrunchHeight;
+    int FortCanningGardensHeight;
     int GreatWorldHeight;
 
     /** Foundation share after AMK, normalized to COIN = 100% */
