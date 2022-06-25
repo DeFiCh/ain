53575a44ab264b6e1b10f21f24599810a2e0eebf
Prasanna Loganathar <pvl@prasannavl.com>
2022-06-25 09:20:14 +0530

    Flush startup consolidation, add logaccountbalances (#1333)
    * Add IterateKV for generic iteration, add logaccountbalances
    
    * Add startup consolidation fix
    
    * Minor formatting
    
    * Add support for rpc and log
    
    * Make arg hidden
    
    * Format cleanup
    
    * Fix MacOS compilation
    
    Co-authored-by: Jouzo <15011228+Jouzo@users.noreply.github.com>
    Co-authored-by: jouzo <jdesclercs@gmail.com>

--- src/masternodes/rpc_accounts.cpp
+++ src/masternodes/rpc_accounts.cpp
@@ -2370,30 +2370,86 @@ UniValue getpendingfutureswaps(const JSONRPCRequest& request) {
     return GetRPCResultCache().Set(request, obj);
 }
 
+UniValue logaccountbalances(const JSONRPCRequest& request) {
+    RPCHelpMan{
+        "logaccountbalances",
+        "\nLogs all account balances in accounts for debugging.\n",
+        {
+            {"logfile", RPCArg::Type::BOOL, RPCArg::Optional::OMITTED,
+                "Log file (default = false), if set to true, prints to the log file, otherwise no log output"},
+            {"rpcresult", RPCArg::Type::BOOL, RPCArg::Optional::OMITTED,
+                "RPC Result (default = true), if set to true, returns an RPC result, otherwise no RPC output"},
+        },
+        RPCResult{
+            "{...} (array) Json object with account balances if rpcresult is enabled."
+            "This is for debugging purposes only.\n"},
+        RPCExamples{
+            HelpExampleCli("logaccountbalances", "true true")},
+    }.Check(request);
+
+    auto &p = request.params;
+    auto outToLog = false;
+    auto outToRpc = true;
+
+    if (p.size() > 0) { outToLog = p[0].get_bool(); }
+    if (p.size() > 1) { outToRpc = p[1].get_bool(); }
+
+    std::map<std::string, std::vector<CTokenAmount>> accounts;
+    auto iter = pcustomcsDB->NewIterator();
+    auto n = IterateKV<CAccountsView::ByBalanceKey, BalanceKey, CAmount>([&](BalanceKey key, CAmount val) {
+        auto owner = ScriptToString(key.owner);
+        if (outToLog)
+            LogPrintf("AccountBalance: (%s: %d@%d)\n", owner, val, key.tokenID.v);
+        if (outToRpc)
+            accounts[owner].push_back(CTokenAmount{{key.tokenID.v}, val});
+        return true;
+    }, BalanceKey{}, std::move(iter));
+
+    if (outToLog)
+        LogPrintf("IndexStats: (balances: %d)\n", n);
+
+    if (!outToRpc)
+        return {};
+
+    UniValue result{UniValue::VOBJ};
+    UniValue accountsJson{UniValue::VOBJ};
+    for (auto &[key, v]: accounts) {
+        UniValue b{UniValue::VARR};
+        for (auto &item: v) {
+            b.push_back(item.ToString());
+        }
+        accountsJson.pushKV(key, b);
+    }
+
+    result.pushKV("accounts", accountsJson);
+    result.pushKV("count", static_cast<uint64_t>(n));
+    return result;
+}
 
 static const CRPCCommand commands[] =
 {
-//  category        name                     actor (function)        params
-//  -------------   ------------------------ ----------------------  ----------
-    {"accounts",    "listaccounts",          &listaccounts,          {"pagination", "verbose", "indexed_amounts", "is_mine_only"}},
-    {"accounts",    "getaccount",            &getaccount,            {"owner", "pagination", "indexed_amounts"}},
-    {"accounts",    "gettokenbalances",      &gettokenbalances,      {"pagination", "indexed_amounts", "symbol_lookup"}},
-    {"accounts",    "utxostoaccount",        &utxostoaccount,        {"amounts", "inputs"}},
-    {"accounts",    "sendutxosfrom",         &sendutxosfrom,         {"from", "to", "amount", "change"}},
-    {"accounts",    "accounttoaccount",      &accounttoaccount,      {"from", "to", "inputs"}},
-    {"accounts",    "accounttoutxos",        &accounttoutxos,        {"from", "to", "inputs"}},
-    {"accounts",    "listaccounthistory",    &listaccounthistory,    {"owner", "options"}},
-    {"accounts",    "getaccounthistory",     &getaccounthistory,     {"owner", "blockHeight", "txn"}},
-    {"accounts",    "listburnhistory",       &listburnhistory,       {"options"}},
-    {"accounts",    "accounthistorycount",   &accounthistorycount,   {"owner", "options"}},
-    {"accounts",    "listcommunitybalances", &listcommunitybalances, {}},
-    {"accounts",    "sendtokenstoaddress",   &sendtokenstoaddress,   {"from", "to", "selectionMode"}},
-    {"accounts",    "getburninfo",           &getburninfo,           {}},
-    {"accounts",    "executesmartcontract",  &executesmartcontract,  {"name", "amount", "inputs"}},
-    {"accounts",    "futureswap",            &futureswap,            {"address", "amount", "destination", "inputs"}},
-    {"accounts",    "withdrawfutureswap",    &withdrawfutureswap,    {"address", "amount", "destination", "inputs"}},
-    {"accounts",    "listpendingfutureswaps",    &listpendingfutureswaps,    {}},
-    {"accounts",    "getpendingfutureswaps",     &getpendingfutureswaps,     {"address"}},
+//  category       name                     actor (function)        params
+//  -------------  ------------------------ ----------------------  ----------
+    {"accounts",   "listaccounts",             &listaccounts,              {"pagination", "verbose", "indexed_amounts", "is_mine_only"}},
+    {"accounts",   "getaccount",               &getaccount,                {"owner", "pagination", "indexed_amounts"}},
+    {"accounts",   "gettokenbalances",         &gettokenbalances,          {"pagination", "indexed_amounts", "symbol_lookup"}},
+    {"accounts",   "utxostoaccount",           &utxostoaccount,            {"amounts", "inputs"}},
+    {"accounts",   "sendutxosfrom",            &sendutxosfrom,             {"from", "to", "amount", "change"}},
+    {"accounts",   "accounttoaccount",         &accounttoaccount,          {"from", "to", "inputs"}},
+    {"accounts",   "accounttoutxos",           &accounttoutxos,            {"from", "to", "inputs"}},
+    {"accounts",   "listaccounthistory",       &listaccounthistory,        {"owner", "options"}},
+    {"accounts",   "getaccounthistory",        &getaccounthistory,         {"owner", "blockHeight", "txn"}},
+    {"accounts",   "listburnhistory",          &listburnhistory,           {"options"}},
+    {"accounts",   "accounthistorycount",      &accounthistorycount,       {"owner", "options"}},
+    {"accounts",   "listcommunitybalances",    &listcommunitybalances,     {}},
+    {"accounts",   "sendtokenstoaddress",      &sendtokenstoaddress,       {"from", "to", "selectionMode"}},
+    {"accounts",   "getburninfo",              &getburninfo,               {}},
+    {"accounts",   "executesmartcontract",     &executesmartcontract,      {"name", "amount", "inputs"}},
+    {"accounts",   "futureswap",               &futureswap,                {"address", "amount", "destination", "inputs"}},
+    {"accounts",   "withdrawfutureswap",       &withdrawfutureswap,        {"address", "amount", "destination", "inputs"}},
+    {"accounts",   "listpendingfutureswaps",   &listpendingfutureswaps,    {}},
+    {"accounts",   "getpendingfutureswaps",    &getpendingfutureswaps,     {"address"}},
+    {"hidden",     "logaccountbalances",       &logaccountbalances,        {"logfile", "rpcresult"}},
 };
 
 void RegisterAccountsRPCCommands(CRPCTable& tableRPC) {
