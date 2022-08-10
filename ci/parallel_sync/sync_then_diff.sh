9ec7102c5d1ce873774e279463004a992f5955d5
jouzo <jdesclercs@gmail.com>
2022-08-10 07:56:02 +0100

    Update sync script

--- ci/parallel_sync/sync_then_diff.sh
+++ ci/parallel_sync/sync_then_diff.sh
@@ -5,16 +5,9 @@ set -Eeuo pipefail
 DEFID_BIN=./defid
 DEFI_CLI_BIN=./defi-cli
 
-# Commands
-DEFID_CMD="$DEFID_BIN -daemon -debug=accountchange"
-DEFI_CLI_CMD="$DEFI_CLI_BIN"
-ACCOUNT_BALANCES_CMD="$DEFI_CLI_CMD logaccountbalances"
-LIST_ANCHORS_CMD="$DEFI_CLI_CMD spv_listanchors"
-FETCH=wget
-GREP=grep
-
 # Files and directories
-DEBUG_FILE=".defi/debug.log"
+DATADIR=${DATADIR:-".defi"}
+DEBUG_FILE="$DEBUG_FILE/debug.log"
 TMP_LOG=debug-tmp-$STOP_BLOCK.log
 BASE_PATH=https://storage.googleapis.com
 BUCKET=team-drop
@@ -22,10 +15,20 @@ REF_LOG_DIR=master-log-full
 REF_LOG=debug-$STOP_BLOCK.log
 REF_LOG_PATH=$BASE_PATH/$BUCKET/$REF_LOG_DIR/$REF_LOG
 
+# Commands
+DEFID_CMD="$DEFID_BIN -datadir=$DATADIR -daemon -debug=accountchange"
+DEFI_CLI_CMD="$DEFI_CLI_BIN -datadir=$DATADIR"
+ACCOUNT_BALANCES_CMD="$DEFI_CLI_CMD logaccountbalances"
+LIST_ANCHORS_CMD="$DEFI_CLI_CMD spv_listanchors"
+FETCH=wget
+GREP=grep
+
 # Start defid
 echo "Syncing to block height: $STOP_BLOCK"
 $DEFID_CMD -interrupt-block=$((STOP_BLOCK + 1))
 sleep 30
+
+$DEFI_CLI_CMD clearbanned
 
 BLOCK=0
 # Sync to target block height
@@ -40,6 +43,7 @@ $GREP "AccountChange:" $DEBUG_FILE | cut -d" " -f2- > $TMP_LOG
 $ACCOUNT_BALANCES_CMD >> $TMP_LOG
 $LIST_ANCHORS_CMD >> $TMP_LOG
 
+$DEFI_CLI_CMD stop
 # Download reference log file
 $FETCH $REF_LOG_PATH
 
