f821c128a347acb6e1fea7276494fcfd3c379971
Peter Bushnell <bushsolo@gmail.com>
2022-09-09 12:38:49 +0100

    Test fix: apply scheme check after fork

--- src/masternodes/govvariables/attributes.cpp
+++ src/masternodes/govvariables/attributes.cpp
@@ -1185,20 +1185,22 @@ Res ATTRIBUTES::Apply(CCustomCSView & mnview, const uint32_t height)
                     }
                 }
             } else if (attrV0->key == TokenKeys::LoanCollateralFactor) {
-                std::set<CAmount> ratio;
-                mnview.ForEachLoanScheme([&ratio](const std::string &identifier, const CLoanSchemeData &data) {
-                    ratio.insert(data.ratio);
-                    return true;
-                });
-                if (ratio.empty()) {
-                    return Res::Err("Set loan scheme before setting collateral factor.");
-                }
-                const auto factor = std::get_if<CAmount>(&attribute.second);
-                if (!factor) {
-                    return Res::Err("Unexpected type");
-                }
-                if (*factor >= *ratio.begin() * CENT) {
-                    return Res::Err("Factor cannot be more than or equal to the lowest scheme rate of %d\n", GetDecimaleString(*ratio.begin() * CENT));
+                if (height >= Params().GetConsensus().FortCanningEpilogueHeight) {
+                    std::set<CAmount> ratio;
+                    mnview.ForEachLoanScheme([&ratio](const std::string &identifier, const CLoanSchemeData &data) {
+                        ratio.insert(data.ratio);
+                        return true;
+                    });
+                    if (ratio.empty()) {
+                        return Res::Err("Set loan scheme before setting collateral factor.");
+                    }
+                    const auto factor = std::get_if<CAmount>(&attribute.second);
+                    if (!factor) {
+                        return Res::Err("Unexpected type");
+                    }
+                    if (*factor >= *ratio.begin() * CENT) {
+                        return Res::Err("Factor cannot be more than or equal to the lowest scheme rate of %d\n", GetDecimaleString(*ratio.begin() * CENT));
+                    }
                 }
             }
         } else if (attrV0->type == AttributeTypes::Param) {
