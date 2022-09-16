b5d203db396e0a71cd0038668cdbc00c44446392
Peter John Bushnell <bushsolo@gmail.com>
2022-09-16 15:33:04 +0100

    Allow ICX fix on RegTest (#1479)
    * Fix ICX on RegTest
    
    * Enable TXs on Regtest

--- src/masternodes/mn_checks.cpp
+++ src/masternodes/mn_checks.cpp
@@ -2249,7 +2249,8 @@ public:
         // maker bonus only on fair dBTC/BTC (1:1) trades for now
         DCT_ID BTC = FindTokenByPartialSymbolName(CICXOrder::TOKEN_BTC);
         if (order->idToken == BTC && order->orderPrice == COIN) {
-            if (Params().NetworkIDString() == CBaseChainParams::TESTNET && height >= 1248000) {
+            if ((Params().NetworkIDString() == CBaseChainParams::TESTNET && height >= 1250000) ||
+                 Params().NetworkIDString() == CBaseChainParams::REGTEST) {
                 res = TransferTokenBalance(DCT_ID{0}, offer->takerFee * 50 / 100, CScript(), order->ownerAddress);
             } else {
                 res = TransferTokenBalance(BTC, offer->takerFee * 50 / 100, CScript(), order->ownerAddress);
@@ -3743,7 +3744,10 @@ bool IsDisabledTx(uint32_t height, CustomTxType type, const Consensus::Params& c
     // ICXCloseOffer       = '7',
 
     // disable ICX orders for all networks other than testnet
-    if (Params().NetworkIDString() == CBaseChainParams::TESTNET && static_cast<int>(height) >= 1250000) return false;
+    if (Params().NetworkIDString() == CBaseChainParams::REGTEST ||
+        (Params().NetworkIDString() == CBaseChainParams::TESTNET && static_cast<int>(height) >= 1250000)) {
+        return false;
+    }
 
     // Leaving close orders, as withdrawal of existing should be ok
     switch (type) {
