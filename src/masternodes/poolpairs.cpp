7ed3d45acf884b0ab1b299c0e4966358a706e5dd
Peter Bushnell <bushsolo@gmail.com>
2022-11-08 14:32:58 +0000

    Commission fix from GrandCentral

--- src/masternodes/poolpairs.cpp
+++ src/masternodes/poolpairs.cpp
@@ -227,7 +227,7 @@ auto InitPoolVars(CPoolPairView & view, PoolHeightKey poolKey, uint32_t end) {
     auto it = view.LowerBound<By>(poolKey);
 
     auto height = poolKey.height;
-    static const uint32_t startHeight = Params().GetConsensus().BayfrontHeight;
+    static const uint32_t startHeight = Params().GetConsensus().GrandCentralHeight;
     poolKey.height = std::max(height, startHeight);
 
     while (!MatchPoolId(it, poolId) && poolKey.height < end) {
