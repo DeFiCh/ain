78a53d1731aa4e0de85e29f53c1c3f18e2e4b69e
Peter Bushnell <bushsolo@gmail.com>
2022-11-11 16:16:49 +0000

    Revert "Fix CI deprecation warnings" and other lint changes

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -9,12 +9,10 @@ export LC_ALL=C
 PATH=$PATH:~/.local/bin
 
 curl -fsSL -o- https://bootstrap.pypa.io/pip/3.5/get-pip.py | python3
-pip3 install codespell==2.2.1
-pip3 install flake8==4.0.1
-pip3 install mypy==0.942
-pip3 install pyzmq==22.3.0
-pip3 install vulture==2.3
+pip3 install codespell==1.15.0
+pip3 install flake8==3.5.0
+pip3 install vulture==0.29
 
-SHELLCHECK_VERSION=v0.8.0
+SHELLCHECK_VERSION=v0.7.1
 curl -L -s "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
 export PATH="/tmp/shellcheck-${SHELLCHECK_VERSION}:${PATH}"
--- test/lint/lint-shell-locale.sh
+++ test/lint/lint-shell-locale.sh
@@ -1,67 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 #
-# Copyright (c) 2018-2022 The Bitcoin Core developers
+# Copyright (c) 2018 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+#
+# Make sure all shell scripts:
+# a.) explicitly opt out of locale dependence using
+#     "export LC_ALL=C" or "export LC_ALL=C.UTF-8", or
+# b.) explicitly opt in to locale dependence using the annotation below.
 
-"""
-Make sure all shell scripts are:
-a.) explicitly opt out of locale dependence using
-    "export LC_ALL=C" or "export LC_ALL=C.UTF-8", or
-b.) explicitly opt in to locale dependence using the annotation below.
-"""
+export LC_ALL=C
 
-import subprocess
-import sys
-import re
-
-OPT_IN_LINE = '# This script is intentionally locale dependent by not setting \"export LC_ALL=C\"'
-
-OPT_OUT_LINES = [
-    'export LC_ALL=C',
-    'export LC_ALL=C.UTF-8',
-]
-
-def get_shell_files_list():
-    command = [
-        'git',
-        'ls-files',
-        '--',
-        '*.sh',
-    ]
-    try:
-        return subprocess.check_output(command, stderr = subprocess.STDOUT).decode('utf-8').splitlines()
-    except subprocess.CalledProcessError as e:
-        if e.returncode > 1: # return code is 1 when match is empty
-            print(e.output.decode('utf-8'), end='')
-            sys.exit(1)
-        return []
-
-def main():
-    exit_code = 0
-    shell_files = get_shell_files_list()
-    for file_path in shell_files:
-        if re.search('src/(secp256k1|minisketch)/', file_path):
-            continue
-
-        with open(file_path, 'r', encoding='utf-8') as file_obj:
-            contents = file_obj.read()
-
-        if OPT_IN_LINE in contents:
-            continue
-
-        non_comment_pattern = re.compile(r'^\s*((?!#).+)$', re.MULTILINE)
-        non_comment_lines = re.findall(non_comment_pattern, contents)
-        if not non_comment_lines:
-            continue
-
-        first_non_comment_line = non_comment_lines[0]
-        if first_non_comment_line not in OPT_OUT_LINES:
-            print(f'Missing "export LC_ALL=C" (to avoid locale dependence) as first non-comment non-empty line in {file_path}')
-            exit_code = 1
-
-    return sys.exit(exit_code)
-
-if __name__ == '__main__':
-    main()
-
+EXIT_CODE=0
+for SHELL_SCRIPT in $(git ls-files -- "*.sh" | grep -vE "src/(secp256k1|univalue)/"); do
+    if grep -q "# This script is intentionally locale dependent by not setting \"export LC_ALL=C\"" "${SHELL_SCRIPT}"; then
+        continue
+    fi
+    FIRST_NON_COMMENT_LINE=$(grep -vE '^(#.*)?$' "${SHELL_SCRIPT}" | head -1)
+    if [[ ${FIRST_NON_COMMENT_LINE} != "export LC_ALL=C" && ${FIRST_NON_COMMENT_LINE} != "export LC_ALL=C.UTF-8" ]]; then
+        echo "Missing \"export LC_ALL=C\" (to avoid locale dependence) as first non-comment non-empty line in ${SHELL_SCRIPT}"
+        EXIT_CODE=1
+    fi
+done
+exit ${EXIT_CODE}
