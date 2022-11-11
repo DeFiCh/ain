a20f02a8fd2225d206649af30e2d169ac8bfcc94
Peter Bushnell <bushsolo@gmail.com>
2022-11-11 15:48:23 +0000

    Upstream changes for lint-python.py

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -9,10 +9,12 @@ export LC_ALL=C
 PATH=$PATH:~/.local/bin
 
 curl -fsSL -o- https://bootstrap.pypa.io/pip/3.5/get-pip.py | python3
-pip3 install codespell==1.15.0
+pip3 install codespell==2.2.1
 pip3 install flake8==4.0.1
-pip3 install vulture==0.29
+pip3 install mypy==0.942
+pip3 install pyzmq==22.3.0
+pip3 install vulture==2.3
 
-SHELLCHECK_VERSION=v0.7.1
+SHELLCHECK_VERSION=v0.8.0
 curl -L -s "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
 export PATH="/tmp/shellcheck-${SHELLCHECK_VERSION}:${PATH}"
