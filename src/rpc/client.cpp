c2e2a00db26483c14f7b94ef3cfa3e1002125a9c
Mihailo Milenkovic <mihailo.milenkovic84@gmail.com>
2022-12-12 17:33:49 +0100

    Fix lint

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -356,7 +356,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "creategovvoc", 0, "data" },
     { "creategovvoc", 1, "inputs" },
     { "listgovproposalvotes", 2, "cycle" },
-    { "listgovproposals", 0, "options" },
+    { "listgovproposals", 0, "type" },
 };
 // clang-format on
 
