b5c731c5d0912421a15ccfab15e2fd816eeb7567
Peter John Bushnell <bushsolo@gmail.com>
2023-03-22 09:25:07 +0000

    Set GRPC ports with start up flags (#1838)

--- src/chainparamsbase.h
+++ src/chainparamsbase.h
@@ -23,12 +23,14 @@ public:
 
     const std::string& DataDir() const { return strDataDir; }
     int RPCPort() const { return nRPCPort; }
+    int GRPCPort() const { return nGRPCPort; }
 
     CBaseChainParams() = delete;
-    CBaseChainParams(const std::string& data_dir, int rpc_port) : nRPCPort(rpc_port), strDataDir(data_dir) {}
+    CBaseChainParams(const std::string& data_dir, int rpc_port, int grpc_port) : nRPCPort(rpc_port), nGRPCPort(grpc_port), strDataDir(data_dir) {}
 
 private:
     int nRPCPort;
+    int nGRPCPort;
     std::string strDataDir;
 };
 
