e138aed2100cad269aa99bd2ed74e4966f8b3ab8
Bushstar <bushsolo@gmail.com>
2023-03-28 12:35:10 +0100

    Add create, sign and execute TX

--- src/rust/crates/ain-evm/src/lib.rs
+++ src/rust/crates/ain-evm/src/lib.rs
@@ -1,5 +1,11 @@
+mod transaction;
+
 use ain_evm_runtime::RUNTIME;
 use std::error::Error;
+
+use primitive_types::{H160, H256, U256};
+use transaction::LegacyUnsignedTransaction;
+use ethereum::{TransactionAction, TransactionSignature};
 
 #[cxx::bridge]
 mod server {
@@ -7,6 +13,17 @@ mod server {
         fn evm_add_balance(address: &str, amount: i64) -> Result<()>;
         fn evm_sub_balance(address: &str, amount: i64) -> Result<()>;
         fn evm_send_raw_tx(tx: &str) -> Result<()>;
+
+        fn create_sign_and_execute_evm_tx(
+            chain_id: u64,
+            nonce: [u8; 32],
+            gas_price: [u8; 32],
+            gas_limit: [u8; 32],
+            to: Vec<u8>,
+            value: [u8; 32],
+            input: Vec<u8>,
+            priv_key: [u8; 32],
+        );
     }
 }
 
@@ -20,4 +37,46 @@ fn evm_sub_balance(address: &str, amount: i64) -> Result<(), Box<dyn Error>> {
 
 fn evm_send_raw_tx(_tx: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
+}
+
+fn create_sign_and_execute_evm_tx(
+    chain_id: u64,
+    nonce: [u8; 32],
+    gas_price: [u8; 32],
+    gas_limit: [u8; 32],
+    to: Vec<u8>,
+    value: [u8; 32],
+    input: Vec<u8>,
+    priv_key: [u8; 32],
+) {
+    let to_action : TransactionAction;
+    if to.is_empty() {
+        to_action = TransactionAction::Create;
+    } else {
+        to_action = TransactionAction::Call(H160::from_slice(&to));
+    }
+
+    let nonce_u256 = U256::from(nonce);
+    let gas_price_u256 = U256::from(gas_price);
+    let gas_limit_u256 = U256::from(gas_limit);
+    let value_u256 = U256::from(value);
+
+    // Create
+    let t = LegacyUnsignedTransaction{
+        nonce: nonce_u256,
+        gas_price: gas_price_u256,
+        gas_limit: gas_limit_u256,
+        action: to_action,
+        value: value_u256,
+        input,
+        sig: TransactionSignature::new(0,H256::zero(), H256::zero()).unwrap(),
+    };
+
+    let priv_key_h256 = H256::from(priv_key);
+
+    // Sign
+    t.sign(&priv_key_h256, chain_id);
+
+    // Execute
+    // TODO Jeremy
 }
---
+++ src/rust/crates/ain-evm/src/transaction.rs
@@ -0,0 +1,67 @@
+use primitive_types::{H256, U256};
+use sha3::Digest;
+use rlp::RlpStream;
+use ethereum::{TransactionV2, TransactionAction, TransactionSignature};
+
+pub struct LegacyUnsignedTransaction {
+    pub nonce: U256,
+    pub gas_price: U256,
+    pub gas_limit: U256,
+    pub action: TransactionAction,
+    pub value: U256,
+    pub input: Vec<u8>,
+    pub sig: TransactionSignature,
+}
+
+impl LegacyUnsignedTransaction {
+    fn signing_rlp_append(&self, s: &mut RlpStream, chain_id : u64) {
+        s.begin_list(9);
+        s.append(&self.nonce);
+        s.append(&self.gas_price);
+        s.append(&self.gas_limit);
+        s.append(&self.action);
+        s.append(&self.value);
+        s.append(&self.input);
+        s.append(&chain_id);
+        s.append(&0u8);
+        s.append(&0u8);
+    }
+
+    fn signing_hash(&self, chain_id : u64) -> H256 {
+        let mut stream = RlpStream::new();
+        self.signing_rlp_append(&mut stream, chain_id);
+        let mut output = [0u8; 32];
+        output.copy_from_slice(sha3::Keccak256::digest(&stream.out()).as_slice());
+        H256::from(output)
+    }
+
+    pub fn sign(&self, key: &H256, chain_id: u64) -> TransactionV2 {
+        self.sign_with_chain_id(key, chain_id)
+    }
+
+    pub fn sign_with_chain_id(&self, key: &H256, chain_id: u64) -> TransactionV2 {
+        let hash = self.signing_hash(chain_id);
+        let msg = libsecp256k1::Message::parse(hash.as_fixed_bytes());
+        let s = libsecp256k1::sign(
+            &msg,
+            &libsecp256k1::SecretKey::parse_slice(&key[..]).unwrap(),
+        );
+        let sig = s.0.serialize();
+
+        let sig = TransactionSignature::new(
+            s.1.serialize() as u64 % 2 + chain_id * 2 + 35,
+            H256::from_slice(&sig[0..32]),
+            H256::from_slice(&sig[32..64]),
+        ).unwrap();
+
+        TransactionV2::Legacy(ethereum::LegacyTransaction {
+            nonce: self.nonce,
+            gas_price: self.gas_price,
+            gas_limit: self.gas_limit,
+            action: self.action,
+            value: self.value,
+            input: self.input.clone(),
+            signature: sig,
+        })
+    }
+}
