7354a60839af2922de971746af4c746ac694210a
Jouzo <15011228+Jouzo@users.noreply.github.com>
2023-04-06 08:07:42 +0100

    EVM in/out refinements  (#1868)
    * Return error on sub balance failure.
    
    * Change to expected return type
    
    * Update EVM in/out TX
    
    * Use array instead of rust::Vec
    
    * Pass context to CustomTxVisit
    
    * Add temporary state to TransactionQueue
    
    * Improve readability
    
    * Block integration test
    
    ---------
    
    Co-authored-by: Bushstar <bushsolo@gmail.com>

--- src/rust/crates/ain-evm-state/src/evm.rs
+++ src/rust/crates/ain-evm-state/src/evm.rs
@@ -4,13 +4,12 @@ use ain_evm::{executor::AinExecutor, traits::Executor, transaction::SignedTx};
 use anyhow::anyhow;
 use ethereum::{AccessList, Block, PartialHeader, TransactionV2};
 use evm::backend::MemoryAccount;
-use evm::executor::stack::{MemoryStackState, StackExecutor, StackSubstateMetadata};
 use evm::{
     backend::{MemoryBackend, MemoryVicinity},
-    Config, ExitReason,
+    ExitReason,
 };
 use hex::FromHex;
-use primitive_types::{H160, H256, U256};
+use primitive_types::{H160, U256};
 use std::collections::BTreeMap;
 use std::error::Error;
 use std::fs::File;
@@ -18,8 +17,6 @@ use std::io::{Read, Write};
 use std::path::Path;
 use std::sync::{Arc, RwLock};
 
-pub static CONFIG: Config = Config::london();
-pub static GAS_LIMIT: u64 = u64::MAX;
 pub static EVM_STATE_PATH: &str = "evm_state.bin";
 
 pub type EVMState = BTreeMap<H160, MemoryAccount>;
@@ -87,22 +84,19 @@ impl EVMHandler {
     }
 
     // TODO wrap in EVM transaction and dryrun with evm_call
-    pub fn add_balance(&self, address: &str, value: U256) -> Result<(), Box<dyn Error>> {
-        let to = address.parse()?;
-        let mut state = self.state.write().unwrap();
-        let mut account = state.entry(to).or_default();
-        account.balance = account.balance + value;
-        Ok(())
+    pub fn add_balance(&self, context: u64, address: H160, value: U256) {
+        self.tx_queues.add_balance(context, address, value)
     }
 
-    pub fn sub_balance(&self, address: &str, value: U256) -> Result<(), Box<dyn Error>> {
-        let address = address.parse()?;
-        let mut state = self.state.write().unwrap();
-        let mut account = state.get_mut(&address).unwrap();
-        if account.balance > value {
-            account.balance = account.balance - value;
-        }
-        Ok(())
+    pub fn sub_balance(
+        &self,
+        context: u64,
+        address: H160,
+        value: U256,
+    ) -> Result<(), Box<dyn Error>> {
+        self.tx_queues
+            .sub_balance(context, address, value)
+            .map_err(|e| e.into())
     }
 
     pub fn validate_raw_tx(&self, tx: &str) -> Result<SignedTx, Box<dyn Error>> {
@@ -137,7 +131,8 @@ impl EVMHandler {
     }
 
     pub fn get_context(&self) -> u64 {
-        self.tx_queues.get_context()
+        let state = self.state.read().unwrap().clone();
+        self.tx_queues.get_context(state)
     }
 
     pub fn discard_context(&self, context: u64) {
@@ -158,14 +153,13 @@ impl EVMHandler {
         let mut tx_hashes = Vec::with_capacity(self.tx_queues.len(context));
         let mut failed_tx_hashes = Vec::with_capacity(self.tx_queues.len(context));
         let vicinity = get_vicinity(None, None);
-        let state = self.state.read().unwrap().clone();
+        let state = self.tx_queues.state(context).expect("Wrong context");
         let backend = MemoryBackend::new(&vicinity, state);
         let mut executor = AinExecutor::new(backend);
 
         for signed_tx in self.tx_queues.drain_all(context) {
             let tx_response = executor.exec(&signed_tx);
             println!("tx_response : {:#?}", tx_response);
-
             if tx_response.exit_reason.is_succeed() {
                 // responses.push()
                 tx_hashes.push(signed_tx.transaction);
@@ -173,6 +167,8 @@ impl EVMHandler {
                 failed_tx_hashes.push(signed_tx.transaction)
             }
         }
+
+        self.tx_queues.remove(context);
 
         if update_state {
             let mut state = self.state.write().unwrap();
@@ -215,231 +211,5 @@ fn get_vicinity(origin: Option<H160>, gas_price: Option<U256>) -> MemoryVicinity
         block_gas_limit: U256::MAX,
         chain_id: U256::one(),
         block_base_fee_per_gas: U256::MAX,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use primitive_types::{H256, U256};
-
-    fn create_account(
-        nonce: U256,
-        balance: U256,
-        code: Vec<u8>,
-        storage: BTreeMap<H256, H256>,
-    ) -> MemoryAccount {
-        MemoryAccount {
-            nonce,
-            balance,
-            code,
-            storage,
-        }
-    }
-
-    #[test]
-    fn test_load_non_existent_file() {
-        let state = EVMState::load_from_disk("non_existent_file.bin").unwrap();
-        assert_eq!(state, EVMState::default());
-    }
-
-    #[test]
-    fn test_empty_file() {
-        let state = BTreeMap::new();
-        let path = "empty_test.bin";
-
-        // Save to an empty file
-        state.save_to_disk(path).unwrap();
-
-        let new_state = EVMState::load_from_disk(path).unwrap();
-
-        assert_eq!(state, new_state);
-    }
-
-    #[test]
-    fn test_invalid_file_format() {
-        let invalid_data = b"invalid_data";
-        let path = "invalid_file_format.bin";
-
-        // Write invalid data to a file
-        let mut file = File::create(path).unwrap();
-        file.write_all(invalid_data).unwrap();
-
-        let state = EVMState::load_from_disk(path);
-
-        assert!(state.is_err());
-    }
-
-    #[test]
-    fn test_save_and_load_empty_backend() {
-        let path = "test_empty_backend.bin";
-        let state = BTreeMap::new();
-
-        state.save_to_disk(path).unwrap();
-
-        let loaded_backend = EVMState::load_from_disk(path).unwrap();
-
-        assert_eq!(state, loaded_backend);
-    }
-
-    #[test]
-    fn test_save_and_load_single_account() {
-        let path = "test_single_account.bin";
-        let mut state = BTreeMap::new();
-
-        let account = create_account(
-            U256::from(1),
-            U256::from(1000),
-            vec![1, 2, 3],
-            BTreeMap::new(),
-        );
-        let address = H160::from_low_u64_be(1);
-        state.insert(address, account);
-
-        state.save_to_disk(path).unwrap();
-
-        let loaded_backend = EVMState::load_from_disk(path).unwrap();
-
-        assert_eq!(state, loaded_backend);
-    }
-
-    #[test]
-    fn test_save_and_load_multiple_accounts() {
-        let path = "test_multiple_accounts.bin";
-        let mut state = BTreeMap::new();
-
-        let account1 = create_account(
-            U256::from(1),
-            U256::from(1000),
-            vec![1, 2, 3],
-            BTreeMap::new(),
-        );
-        let address1 = H160::from_low_u64_be(1);
-        state.insert(address1, account1);
-
-        let account2 = create_account(
-            U256::from(2),
-            U256::from(2000),
-            vec![4, 5, 6],
-            BTreeMap::new(),
-        );
-        let address2 = H160::from_low_u64_be(2);
-        state.insert(address2, account2);
-
-        state.save_to_disk(path).unwrap();
-
-        let loaded_backend = EVMState::load_from_disk(path).unwrap();
-
-        assert_eq!(state, loaded_backend);
-    }
-}
-
-mod tests {
-    use ain_evm::transaction::SignedTx;
-    use evm::backend::MemoryAccount;
-    use primitive_types::{H160, H256, U256};
-
-    use super::EVMHandler;
-    use rlp::Encodable;
-    #[test]
-    fn test_finalize_block_and_update_test() {
-        let handler = EVMHandler::new();
-        handler
-            .add_balance(
-                "0x6745f998a96050bb9b0449e6bd4358138a519679",
-                U256::from_str_radix("100000000000000000000", 10).unwrap(),
-            )
-            .unwrap();
-        let context = handler.get_context();
-
-        let tx1: SignedTx = "f86b02830186a0830186a094a8f7c4c78c36e54c3950ad58dad24ca5e0191b2989056bc75e2d631000008025a0b0842b0c78dd7fc33584ec9a81ab5104fe70169878de188ba6c11fe7605e298aa0735dc483f625f17d68d1e1fae779b7160612628e6dde9eecf087892fe60bba4e".try_into().unwrap();
-        handler.tx_queues.add_signed_tx(context, tx1.clone());
-
-        handler
-            .add_balance(
-                "0xc0cd829081485e70348975d325fe5275140277bd",
-                U256::from_str_radix("100000000000000000000", 10).unwrap(),
-            )
-            .unwrap();
-        let tx2: SignedTx = "f86b02830186a0830186a094a8f7c4c78c36e54c3950ad58dad24ca5e0191b2989056bc75e2d631000008025a01465e2d999c34b22bf4b8b5c9439918e46341f4f0da1b00a6b0479c541161d4aa074abe79c51bf57086e1e84b57ee483cbb2ecf30e8222bc0472436fabfc57dda8".try_into().unwrap();
-        handler.tx_queues.add_signed_tx(context, tx2.clone());
-
-        let tx3: SignedTx = "f86b02830186a0830186a094a8f7c4c78c36e54c3950ad58dad24ca5e0191b2989056bc75e2d631000008025a070b21a24cec13c0569099ee2f8221268103fd609646b73f7c9e85efeb7af5c8ea03d5de75bc12ce28a80f7c0401df6021cc82a334cb1c802c8b9d46223c5c8eb40".try_into().unwrap();
-        handler.tx_queues.add_signed_tx(context, tx3.clone());
-
-        assert_eq!(handler.tx_queues.len(context), 3);
-        assert_eq!(handler.tx_queues.len(handler.get_context()), 0);
-
-        let (block, failed_txs) = handler.finalize_block(context, true).unwrap();
-        assert_eq!(
-            block.transactions,
-            vec![tx1, tx2]
-                .into_iter()
-                .map(|t| t.transaction)
-                .collect::<Vec<_>>()
-        );
-        assert_eq!(
-            failed_txs,
-            vec![tx3]
-                .into_iter()
-                .map(|t| t.transaction)
-                .collect::<Vec<_>>()
-        );
-
-        let state = handler.state.read().unwrap();
-        assert_eq!(
-            state
-                .get(
-                    &"0xa8f7c4c78c36e54c3950ad58dad24ca5e0191b29"
-                        .parse()
-                        .unwrap()
-                )
-                .unwrap()
-                .balance,
-            U256::from_str_radix("200000000000000000000", 10).unwrap()
-        );
-        assert_eq!(
-            state
-                .get(
-                    &"0x6745f998a96050bb9b0449e6bd4358138a519679"
-                        .parse()
-                        .unwrap()
-                )
-                .unwrap()
-                .balance,
-            U256::from_str_radix("0", 10).unwrap()
-        );
-        assert_eq!(
-            state
-                .get(
-                    &"0xc0cd829081485e70348975d325fe5275140277bd"
-                        .parse()
-                        .unwrap()
-                )
-                .unwrap()
-                .balance,
-            U256::from_str_radix("0", 10).unwrap()
-        );
-    }
-
-    #[test]
-    fn test_finalize_block_and_do_not_update_test() {
-        let handler = EVMHandler::new();
-        handler
-            .add_balance(
-                "0x4a1080c5533cb89edc4b65013f08f78868e382de",
-                U256::from_str_radix("100000000000000000000", 10).unwrap(),
-            )
-            .unwrap();
-        let context = handler.get_context();
-
-        let tx1: SignedTx = "f86b02830186a0830186a094a8f7c4c78c36e54c3950ad58dad24ca5e0191b2989056bc75e2d631000008025a0b0842b0c78dd7fc33584ec9a81ab5104fe70169878de188ba6c11fe7605e298aa0735dc483f625f17d68d1e1fae779b7160612628e6dde9eecf087892fe60bba4e".try_into().unwrap();
-        handler.tx_queues.add_signed_tx(context, tx1.clone());
-
-        let old_state = handler.state.read().unwrap();
-        let _ = handler.finalize_block(context, false).unwrap();
-
-        let new_state = handler.state.read().unwrap();
-        assert_eq!(*new_state, *old_state);
     }
 }
--- src/rust/crates/ain-evm-state/src/tx_queue.rs
+++ src/rust/crates/ain-evm-state/src/tx_queue.rs
@@ -1,3 +1,4 @@
+use primitive_types::{H160, U256};
 use rand::Rng;
 use std::{
     collections::HashMap,
@@ -5,6 +6,8 @@ use std::{
 };
 
 use ain_evm::transaction::SignedTx;
+
+use crate::evm::EVMState;
 
 #[derive(Debug)]
 pub struct TransactionQueueMap {
@@ -18,64 +21,97 @@ impl TransactionQueueMap {
         }
     }
 
-    pub fn get_context(&self) -> u64 {
+    pub fn get_context(&self, state: EVMState) -> u64 {
         let mut rng = rand::thread_rng();
         loop {
             let context = rng.gen();
             let mut write_guard = self.queues.write().unwrap();
 
             if !write_guard.contains_key(&context) {
-                write_guard.insert(context, TransactionQueue::new());
+                write_guard.insert(context, TransactionQueue::new(state));
                 return context;
             }
         }
     }
 
-    pub fn clear(&self, index: u64) {
-        if let Some(queue) = self.queues.read().unwrap().get(&index) {
+    pub fn remove(&self, context_id: u64) -> Option<TransactionQueue> {
+        self.queues.write().unwrap().remove(&context_id)
+    }
+
+    pub fn clear(&self, context_id: u64) {
+        if let Some(queue) = self.queues.read().unwrap().get(&context_id) {
             queue.clear()
         }
     }
 
-    pub fn add_signed_tx(&self, index: u64, signed_tx: SignedTx) {
-        if let Some(queue) = self.queues.read().unwrap().get(&index) {
+    pub fn add_signed_tx(&self, context_id: u64, signed_tx: SignedTx) {
+        if let Some(queue) = self.queues.read().unwrap().get(&context_id) {
             queue.add_signed_tx(signed_tx)
         }
     }
 
-    pub fn drain_all(&self, index: u64) -> Vec<SignedTx> {
-        match self.queues.read().unwrap().get(&index) {
+    pub fn drain_all(&self, context_id: u64) -> Vec<SignedTx> {
+        match self.queues.read().unwrap().get(&context_id) {
             Some(queue) => queue.drain_all(),
             None => Vec::new(),
         }
     }
 
-    pub fn len(&self, index: u64) -> usize {
-        match self.queues.read().unwrap().get(&index) {
+    pub fn len(&self, context_id: u64) -> usize {
+        match self.queues.read().unwrap().get(&context_id) {
             Some(queue) => queue.len(),
             None => 0,
+        }
+    }
+
+    pub fn add_balance(&self, context_id: u64, address: H160, value: U256) {
+        if let Some(queue) = self.queues.read().unwrap().get(&context_id) {
+            queue.add_balance(address, value)
+        }
+    }
+
+    pub fn sub_balance(
+        &self,
+        context_id: u64,
+        address: H160,
+        value: U256,
+    ) -> Result<(), QueueError> {
+        if let Some(queue) = self.queues.read().unwrap().get(&context_id) {
+            queue.sub_balance(address, value)
+        } else {
+            Err(QueueError::NoSuchContext)
+        }
+    }
+
+    pub fn state(&self, context_id: u64) -> Option<EVMState> {
+        if let Some(queue) = self.queues.read().unwrap().get(&context_id) {
+            Some(queue.state())
+        } else {
+            None
         }
     }
 }
 
 #[derive(Debug)]
 pub struct TransactionQueue {
-    txs: Mutex<Vec<SignedTx>>,
+    transactions: Mutex<Vec<SignedTx>>,
+    state: RwLock<EVMState>,
 }
 
 impl TransactionQueue {
-    pub fn new() -> Self {
+    pub fn new(state: EVMState) -> Self {
         Self {
-            txs: Mutex::new(Vec::new()),
+            transactions: Mutex::new(Vec::new()),
+            state: RwLock::new(state),
         }
     }
 
     pub fn clear(&self) {
-        self.txs.lock().unwrap().clear()
+        self.transactions.lock().unwrap().clear()
     }
 
     pub fn drain_all(&self) -> Vec<SignedTx> {
-        self.txs
+        self.transactions
             .lock()
             .unwrap()
             .drain(..)
@@ -83,10 +119,48 @@ impl TransactionQueue {
     }
 
     pub fn add_signed_tx(&self, signed_tx: SignedTx) {
-        self.txs.lock().unwrap().push(signed_tx)
+        self.transactions.lock().unwrap().push(signed_tx)
     }
 
     pub fn len(&self) -> usize {
-        self.txs.lock().unwrap().len()
+        self.transactions.lock().unwrap().len()
+    }
+
+    pub fn state(&self) -> EVMState {
+        self.state.read().unwrap().clone()
+    }
+
+    pub fn add_balance(&self, address: H160, value: U256) {
+        let mut state = self.state.write().unwrap();
+        let mut account = state.entry(address).or_default();
+        account.balance = account.balance + value;
+    }
+
+    pub fn sub_balance(&self, address: H160, value: U256) -> Result<(), QueueError> {
+        let mut state = self.state.write().unwrap();
+        let mut account = state.get_mut(&address).unwrap();
+        if account.balance > value {
+            account.balance = account.balance - value;
+            Ok(())
+        } else {
+            Err(QueueError::InsufficientBalance)
+        }
     }
 }
+
+#[derive(Debug)]
+pub enum QueueError {
+    NoSuchContext,
+    InsufficientBalance,
+}
+
+impl std::fmt::Display for QueueError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            QueueError::NoSuchContext => write!(f, "No transaction queue for this context"),
+            QueueError::InsufficientBalance => write!(f, "Insufficient balance"),
+        }
+    }
+}
+
+impl std::error::Error for QueueError {}
