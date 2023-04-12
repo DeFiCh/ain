9a7131b362826cd2f24a94e1aad0da8f04efef4e
Shoham Chakraborty <shhmchk@gmail.com>
2023-04-12 12:30:24 +0800

    Use defi-config.h instead of hardcoded values

--- src/rust/crates/ain-evm-cpp-ffi/build.rs
+++ src/rust/crates/ain-evm-cpp-ffi/build.rs
@@ -1,16 +1,10 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
-        .define("CLIENT_VERSION_MAJOR", Some("3"))
-        .define("CLIENT_VERSION_MINOR", Some("2"))
-        .define("CLIENT_VERSION_REVISION", Some("8"))
-        .define("CLIENT_VERSION_BUILD", Some("0"))
-        .define("CLIENT_VERSION_RC", Some("0"))
-        .define("CLIENT_VERSION_IS_RELEASE", Some("true"))
-        .define("COPYRIGHT_YEAR", Some("2021"))
         .include("../../../../depends/x86_64-pc-linux-gnu/include/")
         .include("../../../")
         .include("../../../leveldb/include/")
         .include("../../../univalue/include/")
+        .include("../../../config/defi-config.h")
         .flag_if_supported("-std=c++17")
         .flag_if_supported("-Wno-unused-parameter")
         .compile("ain-evm-cpp-ffi");
