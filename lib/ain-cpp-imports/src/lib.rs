c6a6d834b636ad1ae5c29303482eb161945a5eb5
Shoham Chakraborty <shhmchk@gmail.com>
2023-04-14 13:52:18 +0800

    Enable C++ FFI

--- lib/ain-cpp-imports/src/lib.rs
+++ lib/ain-cpp-imports/src/lib.rs
@@ -1,21 +1,21 @@
 use std::error::Error;
 
-// #[cxx::bridge]
-// mod ffi {
-//     unsafe extern "C++" {
-//         include!("masternodes/evm_ffi.h");
+#[cxx::bridge]
+mod ffi {
+    unsafe extern "C++" {
+        include!("masternodes/evm_ffi.h");
 
-//         fn getChainId() -> u64;
-//         fn isMining() -> bool;
-//     }
-// }
+        fn getChainId() -> u64;
+        fn isMining() -> bool;
+    }
+}
 
 pub fn get_chain_id() -> Result<u64, Box<dyn Error>> {
-    // let chain_id = ffi::getChainId();
-    Ok(0)
+    let chain_id = ffi::getChainId();
+    Ok(chain_id)
 }
 
 pub fn is_mining() -> Result<bool, Box<dyn Error>> {
-    // let is_mining = ffi::isMining();
-    Ok(true)
+    let is_mining = ffi::isMining();
+    Ok(is_mining)
 }
