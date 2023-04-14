6d7f2a4588547adf37cc6ef4f6a3beb10c9ccd7e
Prasanna Loganathar <pvl@prasannavl.com>
2023-04-14 19:52:22 +0800

    Update pkg_name with correct one

--- lib/ain-cpp-imports/build.rs
+++ lib/ain-cpp-imports/build.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let pkg_name = env::var("CARGO_PKG_NAME").unwrap();
     let manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let lib_path = &manifest_path
         .parent()
@@ -14,7 +15,7 @@ fn main() {
         .include(lib_path)
         .flag_if_supported("-std=c++17")
         .flag_if_supported("-Wno-unused-parameter")
-        .compile("ain-evm-cpp-ffi");
+        .compile(pkg_name.as_str());
 
     println!(
         "cargo:rerun-if-changed={}/lib.rs",
