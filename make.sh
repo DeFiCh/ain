3ad2ff7f66e858285414604127f1e11e5d827e13
Niven <sieniven@gmail.com>
2023-04-18 23:14:31 +0800

    Install protobuf-compiler pkg dep

--- make.sh
+++ make.sh
@@ -403,7 +403,7 @@ pkg_install_deps() {
         libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev \
         libminiupnpc-dev libzmq3-dev libqrencode-dev wget \
         libdb-dev libdb++-dev libdb5.3 libdb5.3-dev libdb5.3++ libdb5.3++-dev \
-        curl cmake unzip
+        curl cmake unzip protobuf-compiler
 
     _fold_end
 }
