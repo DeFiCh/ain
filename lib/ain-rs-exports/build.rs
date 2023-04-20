9c936558e9c6595c9409854140ec7533dde20412
Prasanna Loganathar <pvl@prasannavl.com>
2023-04-20 15:27:35 +0800

    Clean up rust double builds, respect debug builds

--- lib/ain-rs-exports/build.rs
+++ lib/ain-rs-exports/build.rs
@@ -5,52 +5,48 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::PathBuf;
 
-fn main() {
-    let pkg_name = env::var("CARGO_PKG_NAME").unwrap();
-    let manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-
-    // TODO: Use root path to force re-run during development
-    if std::path::Path::new(".git/HEAD").exists() {
-        println!("cargo:rerun-if-changed=.git/HEAD");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pkg_name = env::var("CARGO_PKG_NAME")?;
+    let manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+    // Using a direct path for now
+    let git_head_path = manifest_path.join("../../.git/HEAD");
+    if git_head_path.exists() {
+        println!("cargo:rerun-if-changed={}", git_head_path.to_string_lossy());
     }
 
-    let target_dir = if let Ok(v) = env::var("BUILD_DIR") {
-        PathBuf::from(v)
-    } else {
-        manifest_path.clone()
-    };
-    println!("BUILD_DIR: {:?}", target_dir);
-    std::fs::create_dir_all(&target_dir).unwrap();
+    // If TARGET_DIR is set, which we do from Makefile, uses that instead of OUT_DIR. 
+    // Otherwise, use the path for OUT_DIR that cargo sets, as usual. 
+    // Reason: Currently setting --out-dir is nightly only, so there's no way to get OUT_DIR
+    // out of cargo reliably for pointing the C++ link targets to this determinisitcally.
+    let target_dir: PathBuf = PathBuf::from(env::var("TARGET_DIR").or(env::var("OUT_DIR"))?);
+    let res = ["src", "include", "lib"].map(|x| target_dir.join(x));
+    for x in &res {
+        std::fs::create_dir_all(x)?;
+    }
+    let [out_src_dir, out_include_dir, _out_lib_dir] = res;
 
     let lib_path = &manifest_path.join("src").join("lib.rs");
     println!("cargo:rerun-if-changed={}", lib_path.as_path().display());
 
     let mut content = String::new();
-    File::open(lib_path)
-        .unwrap()
-        .read_to_string(&mut content)
-        .unwrap();
+    File::open(lib_path)?.read_to_string(&mut content)?;
 
     let pkg_name_underscored = pkg_name.replace('-', "_");
     let header_file_path = pkg_name_underscored.clone() + ".h";
     let source_file_path = pkg_name_underscored + ".cpp";
 
-    let tt: TokenStream = content.parse().unwrap();
+    let tt: TokenStream = content.parse()?;
     let mut opt = cxx_gen::Opt::default();
     opt.include.push(cxx_gen::Include {
         path: header_file_path.clone(),
         kind: cxx_gen::IncludeKind::Bracketed,
     });
 
-    let codegen = cxx_gen::generate_header_and_cc(tt, &opt).unwrap();
-    let cpp_stuff = String::from_utf8(codegen.implementation).unwrap();
+    let codegen = cxx_gen::generate_header_and_cc(tt, &opt)?;
+    let cpp_stuff = String::from_utf8(codegen.implementation)?;
 
-    File::create(target_dir.join(header_file_path))
-        .unwrap()
-        .write_all(&codegen.header)
-        .unwrap();
-    File::create(target_dir.join(source_file_path))
-        .unwrap()
-        .write_all(cpp_stuff.as_bytes())
-        .unwrap();
+    File::create(out_include_dir.join(header_file_path))?.write_all(&codegen.header)?;
+    File::create(out_src_dir.join(source_file_path))?.write_all(cpp_stuff.as_bytes())?;
+
+    Ok(())
 }
