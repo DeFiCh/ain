4784d2968db956f1a980eb4a30ad5d3bbd9bbbe4
Shoham Chakraborty <shhmchk@gmail.com>
2023-05-12 11:39:30 +0700

    Add package hashes

--- depends/packages/protobuf.mk
+++ depends/packages/protobuf.mk
@@ -14,15 +14,15 @@ ifeq ($(build_os)-$(build_arch),linux-x86_64)
 endif
 ifeq ($(build_os)-$(build_arch),linux-aarch64)
   $(package)_file_name=protoc-$($(package)_version)-linux-aarch_64.zip
-  $(package)_sha256_hash=
+  $(package)_sha256_hash=aa2efbb2d481b7ad3c2428e0aa4dd6d813e4538e6c0a1cd4d921ac998187e07e
 endif
 ifeq ($(build_os)-$(build_arch),darwin-x86_64)
   $(package)_file_name=protoc-$($(package)_version)-osx-x86_64.zip
-  $(package)_sha256_hash=
+  $(package)_sha256_hash=8bb75680c376190d960ef1d073618c1103960f70dc4fafa7bde872029562aec1
 endif
 ifeq ($(build_os)-$(build_arch),darwin-arm)
   $(package)_file_name=protoc-$($(package)_version)-osx-aarch_64.zip
-  $(package)_sha256_hash=
+  $(package)_sha256_hash=a196fd39acd312688b58e81266fd88e9f7799967c5439738c10345a29049261d
 endif
 
 ifeq ($($(package)_file_name),)
