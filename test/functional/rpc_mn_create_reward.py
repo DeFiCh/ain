d19caa1ac7ac0bbe0e2c0c4c08dca7fa678832cd
DrPing <drping974@proton.me>
2023-05-16 17:26:52 +0700

    Fix lint error rpc_mn_create_reward

--- test/functional/rpc_mn_create_reward.py
+++ test/functional/rpc_mn_create_reward.py
@@ -9,7 +9,6 @@ from test_framework.test_framework import DefiTestFramework
 from test_framework.authproxy import JSONRPCException
 from test_framework.util import (
     assert_equal,
-    connect_nodes_bi,
     assert_raises_rpc_error,
 )
 
