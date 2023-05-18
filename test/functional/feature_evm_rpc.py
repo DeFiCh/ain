18fb0d77c4172436a132b70e6a2eae0804d2d092
canonbrother <w.canonbrother@gmail.com>
2023-05-18 08:29:37 +0800

    add eth_sendRawTransaction test

--- test/functional/feature_evm_rpc.py
+++ test/functional/feature_evm_rpc.py
@@ -8,7 +8,8 @@
 from test_framework.test_framework import DefiTestFramework
 from test_framework.util import (
     assert_equal,
-    assert_raises_rpc_error
+    assert_raises_rpc_error,
+    assert_is_hex_string
 )
 
 # Utility function to assert value of U256 output
@@ -148,6 +149,54 @@ class EVMTest(DefiTestFramework):
         assert_equal(latest_full_block['transactions'][0]['value'], '0xde0b6b3a7640000')
         assert_equal(latest_full_block['transactions'][0]['v'], '0x25')
         assert_equal(latest_full_block['transactions'][0]['r'], '0x37f41c543402c9b02b35b45ef43ac31a63dcbeba0c622249810ecdec00aee376')
+        
+    def test_send_raw_transaction(self):
+        # LEGACY_TX = {
+        #     value: 0,
+        #     data: contractBytecode,
+        #     gasLimit: 72_000,
+        #     gasPrice: 8,
+        # }
+        # TODO(): debugging signing_hash err causes connection refused
+        # rawtx = '0xf9014b8008830119408080b8fe608060405234801561001057600080fd5b5060df8061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063165c4a1614602d575b600080fd5b603c6038366004605f565b604e565b60405190815260200160405180910390f35b600060588284607f565b9392505050565b600080604083850312156070578182fd5b50508035926020909101359150565b600081600019048311821515161560a457634e487b7160e01b81526011600452602481fd5b50029056fea2646970667358221220223df7833fd08eb1cd3ce363a9c4cb4619c1068a5f5517ea8bb862ed45d994f764736f6c634300080200331ba0f531cca7810ea999647ff5da417365083cce67ff4c985ba7ff8c9e34f34eaf58a04233cfa0f0afb10a0d617ca15fdae36df7bfae34a29ac2135843c526460cd93c'
+        # hash = self.nodes[0].eth_sendRawTransaction(rawtx)
+        # print('hash:', hash)
+        
+        # EIP2930_TX = {
+        #     value: 0,
+        #     data: contractBytecode,
+        #     gasLimit: 500_000,
+        #     gasPrice: 150_000_000_000,
+        #     accessList: [
+        #     {
+        #         address: 0x9b8a4af42140d8a4c153a822f02571a1dd037e89,
+        #         storageKeys: [
+        #             "0x0000000000000000000000000000000000000000000000000000000000000000"
+        #         ]
+        #     },
+        #     ],
+        #     type: 1
+        # }
+        rawtx = '0x01f9018b80808522ecb25c008307a1208080b8fe608060405234801561001057600080fd5b5060df8061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063165c4a1614602d575b600080fd5b603c6038366004605f565b604e565b60405190815260200160405180910390f35b600060588284607f565b9392505050565b600080604083850312156070578182fd5b50508035926020909101359150565b600081600019048311821515161560a457634e487b7160e01b81526011600452602481fd5b50029056fea2646970667358221220223df7833fd08eb1cd3ce363a9c4cb4619c1068a5f5517ea8bb862ed45d994f764736f6c63430008020033f838f7949b8a4af42140d8a4c153a822f02571a1dd037e89e1a0000000000000000000000000000000000000000000000000000000000000000001a088a2f79b7966749c71aa7081d49c835e2e6d19e1f4973055983029a6783e14fea04aed191ed7e8550a1bbf0e0ce4f3c803ad70e2aa1e5ae245f99454ce7ba55c00'
+        hash = self.nodes[0].eth_sendRawTransaction(rawtx)
+        self.nodes[0].generate(1)
+        receipt = self.nodes[0].eth_getTransactionReceipt(hash)
+        assert_is_hex_string(receipt['contractAddress'])
+       
+        # EIP1559_TX = {
+        #     value: 0,
+        #     data: contractBytecode,
+        #     gasLimit: 102_000,
+        #     maxPriorityFeePerGas: 152_000_000_000, # 152 gwei
+        #     maxFeePerGas: 150_000_000_000, # 150 gwei
+        #     type: 2,
+        # }
+        rawtx = '0x02f901588080852363e7f0008522ecb25c0083018e708080b8fe608060405234801561001057600080fd5b5060df8061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063165c4a1614602d575b600080fd5b603c6038366004605f565b604e565b60405190815260200160405180910390f35b600060588284607f565b9392505050565b600080604083850312156070578182fd5b50508035926020909101359150565b600081600019048311821515161560a457634e487b7160e01b81526011600452602481fd5b50029056fea2646970667358221220223df7833fd08eb1cd3ce363a9c4cb4619c1068a5f5517ea8bb862ed45d994f764736f6c63430008020033c080a03e29322f77e70d98b925414c50574ab1981d066151cb793b5db86a74e6a3f5c8a005445f12244e15edffd96c46a8abebd4a48d72ef4d69d7fede08668b7fa0df26'
+        hash = self.nodes[0].eth_sendRawTransaction(rawtx)
+        self.nodes[0].generate(1)
+        receipt = self.nodes[0].eth_getTransactionReceipt(hash)
+        print('receipt:', receipt)
+        assert_is_hex_string(receipt['contractAddress'])
 
     def run_test(self):
         self.setup()
