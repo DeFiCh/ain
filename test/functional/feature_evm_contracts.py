bb3d2c0565b6550da9b69aeb13507c659dc2039d
Shoham Chakraborty <shhmchk@gmail.com>
2023-06-05 14:15:00 +0800

    Clean up test

--- test/functional/feature_evm_contracts.py
+++ test/functional/feature_evm_contracts.py
@@ -3,13 +3,9 @@
 # Copyright (c) DeFi Blockchain Developers
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
-"""Test EVM behaviour"""
+"""Test EVM contract"""
 
 from test_framework.test_framework import DefiTestFramework
-from test_framework.util import (
-    assert_equal,
-    assert_raises_rpc_error
-)
 from contracts import EVMContract
 
 class EVMTest(DefiTestFramework):
@@ -22,18 +18,9 @@ class EVMTest(DefiTestFramework):
 
     def setup(self):
         self.address = self.nodes[0].get_genesis_keys().ownerAuthAddress
-        self.ethAddress = '0x9b8a4af42140d8a4c153a822f02571a1dd037e89'
-        # self.toAddress = '0x6c34cbb9219d8caa428835d2073e8ec88ba0a110'
-        self.nodes[0].importprivkey('af990cc3ba17e776f7f57fcc59942a82846d75833fa17d2ba59ce6858d886e23') # ethAddress
-        # self.nodes[0].importprivkey('17b8cb134958b3d8422b6c43b0732fcdb8c713b524df2d45de12f0c7e214ba35') # toAddress
 
         # Generate chain
-        self.nodes[0].generate(101)
-
-        # assert_raises_rpc_error(-32600, "called before NextNetworkUpgrade height", self.nodes[0].evmtx, self.ethAddress, 0, 21, 21000, self.toAddress, 0.1)
-
-        # Move to fork height
-        self.nodes[0].generate(4)
+        self.nodes[0].generate(105)
 
         self.nodes[0].getbalance()
         self.nodes[0].utxostoaccount({self.address: "201@DFI"})
@@ -43,9 +30,9 @@ class EVMTest(DefiTestFramework):
     def run_test(self):
         self.setup()
         evmcontract = EVMContract("SimpleStorage.sol", "Test", self.nodes[0].get_evm_rpc(), self.nodes[0].generate)
-
         address = evmcontract.get_address()
-        self.nodes[0].transferdomain(1,{self.address:["50@DFI"]}, {self.ethAddress:["50@DFI"]})
+
+        self.nodes[0].transferdomain(1,{self.address:["50@DFI"]}, {address:["50@DFI"]})
         self.nodes[0].generate(1)
 
         contract = evmcontract.deploy_contract(None)
