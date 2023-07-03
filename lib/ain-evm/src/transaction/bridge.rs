1d5d2fd847f57590027157c6103a175a450c62f6
Peter John Bushnell <bushsolo@gmail.com>
2023-07-03 13:58:31 +0100

    Allow Eth TXs with higher fees to replace lower fees TXs with the same nonce (#2136)
    * Add evm_remove_txs_by_sender FFI method
    
    * WIP Replace all TXs by sender
    
    * Remove logging. Resolve remaing bugs.
    
    * lint: remove unused var
    
    ---------
    
    Co-authored-by: jouzo <jdesclercs@gmail.com>
    Co-authored-by: Prasanna Loganathar <pvl@prasannavl.com>

--- lib/ain-evm/src/transaction/bridge.rs
+++ lib/ain-evm/src/transaction/bridge.rs
@@ -11,3 +11,12 @@ pub enum BridgeTx {
     EvmIn(BalanceUpdate),
     EvmOut(BalanceUpdate),
 }
+
+impl BridgeTx {
+    pub fn sender(&self) -> H160 {
+        match self {
+            BridgeTx::EvmIn(tx) => tx.address,
+            BridgeTx::EvmOut(tx) => tx.address,
+        }
+    }
+}
