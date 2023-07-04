307574033894dd4c889c0b39e7ac5f3833182145
Niven <sieniven@gmail.com>
2023-07-04 09:22:49 +0800

    Fix PoolPair value overflow undefined behavior bug (#2077)
    * Fixes to check for max PoolPair value to prevent value overflow
    
    * Revert validation and legacy const expr
    
    * Fixes to poolprice max and compatibility with legacy max poolprice
    
    * Use setMaxPoolPrice in customtx rpc
    
    * Fix max poolpair value check
    
    * Refactor for more clarity
    
    * Fix new unit test value
    
    * Refactor items
    
    * Switch to getMaxValid, cleanup headers
    
    * Simplfy actions checkout
    
    * Remove unused code after re-abstractions
    
    ---------
    
    Co-authored-by: Prasanna Loganathar <pvl@prasannavl.com>

--- src/masternodes/mn_checks.cpp
+++ src/masternodes/mn_checks.cpp
@@ -4615,7 +4615,7 @@ Res CPoolSwap::ExecuteSwap(CCustomCSView &view, std::vector<DCT_ID> poolIDs, con
     }
 
     // Single swap if no pool IDs provided
-    auto poolPrice = POOLPRICE_MAX;
+    auto poolPrice = PoolPrice::getMaxValid();
     std::optional<std::pair<DCT_ID, CPoolPair> > poolPair;
     if (poolIDs.empty()) {
         poolPair = view.GetPoolPair(obj.idTokenFrom, obj.idTokenTo);
@@ -4788,7 +4788,7 @@ Res CPoolSwap::ExecuteSwap(CCustomCSView &view, std::vector<DCT_ID> poolIDs, con
     }
 
     // Reject if price paid post-swap above max price provided
-    if (height >= static_cast<uint32_t>(consensus.FortCanningHeight) && obj.maxPrice != POOLPRICE_MAX) {
+    if (height >= static_cast<uint32_t>(consensus.FortCanningHeight) && !obj.maxPrice.isAboveValid()) {
         if (swapAmountResult.nValue != 0) {
             const auto userMaxPrice = arith_uint256(obj.maxPrice.integer) * COIN + obj.maxPrice.fraction;
             if (arith_uint256(obj.amountFrom) * COIN / swapAmountResult.nValue > userMaxPrice) {
@@ -4822,7 +4822,7 @@ Res SwapToDFIorDUSD(CCustomCSView &mnview,
     obj.idTokenFrom = tokenId;
     obj.idTokenTo   = DCT_ID{0};
     obj.amountFrom  = amount;
-    obj.maxPrice    = POOLPRICE_MAX;
+    obj.maxPrice    = PoolPrice::getMaxValid();
 
     auto poolSwap = CPoolSwap(obj, height);
     auto token    = mnview.GetToken(tokenId);
