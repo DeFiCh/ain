349b958600edfe1dab6f25ca191837bd56ce0551
Prasanna Loganathar <pvl@prasannavl.com>
2023-07-06 12:25:13 +0800

    Fix boost test lints

--- src/test/empty_template_tests.cpp
+++ src/test/empty_template_tests.cpp
@@ -5,10 +5,10 @@
 #include <test/setup_common.h>
 #include <boost/test/unit_test.hpp>
 
-BOOST_FIXTURE_TEST_SUITE(empty_tests, BasicTestingSetup, *boost::unit_test::disabled())
+BOOST_FIXTURE_TEST_SUITE(empty_template_tests, BasicTestingSetup, *boost::unit_test::disabled())
 
 
-BOOST_AUTO_TEST_CASE(empty_test_case_1)
+BOOST_AUTO_TEST_CASE(empty_template_test_case_1)
 {
     BOOST_TEST_MESSAGE("Hello world!");
 }
