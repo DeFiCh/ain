b6513bf6b02c624db1607d3122cfb324837c51e1
Shoham Chakraborty <shhmchk@gmail.com>
2023-07-20 14:38:32 +0800

    Add DST20 to test_runner.py

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -291,6 +291,7 @@ BASE_SCRIPTS = [
     'feature_loan_priceupdate.py',
     'feature_loan_vaultstate.py',
     'feature_loan.py',
+    'feature_dst20.py',
     'feature_evm_contracts.py',
     'feature_evm_fee.py',
     'feature_evm_genesis.py',
