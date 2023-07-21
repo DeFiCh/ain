f00748ed0452ea597d329bff7ac57d5d62a0dbf0
Niven <sieniven@gmail.com>
2023-07-21 11:29:45 +0800

    Fix lint

--- lib/ain-evm/src/amount.rs
+++ lib/ain-evm/src/amount.rs
@@ -7,7 +7,6 @@ pub struct Satoshi(pub U256);
 pub const WEI_TO_GWEI: U256 = U256([1_000_000_000, 0, 0, 0]);
 pub const GWEI_TO_SATS: U256 = U256([10, 0, 0, 0]);
 
-
 impl Wei {
     pub fn to_gwei(&self) -> GWei {
         GWei(self.0 / WEI_TO_GWEI)
