26ae5b4d0fa4fac94e45387f2df94df5cd71c4d6
Bushstar <bushsolo@gmail.com>
2023-07-27 11:45:27 +0100

    Resolve lint error

--- test/functional/feature_evm.py
+++ test/functional/feature_evm.py
@@ -50,7 +50,6 @@ class EVMTest(DefiTestFramework):
         address = self.nodes[0].get_genesis_keys().ownerAuthAddress
         eth_address = '0x9b8a4af42140d8a4c153a822f02571a1dd037e89'
         eth_address_privkey = 'af990cc3ba17e776f7f57fcc59942a82846d75833fa17d2ba59ce6858d886e23'
-        eth_address_bech32 = 'bcrt1qta8meuczw0mhqupzjl5wplz47xajz0dn0wxxr8'
         to_address = '0x6c34cbb9219d8caa428835d2073e8ec88ba0a110'
         to_address_privkey = '17b8cb134958b3d8422b6c43b0732fcdb8c713b524df2d45de12f0c7e214ba35'
 
