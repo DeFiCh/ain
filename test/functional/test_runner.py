8fd66e7a44a6c5928be11a6524c4a15c5e727907
Prasanna Loganathar <pvl@prasannavl.com>
2023-07-28 00:11:56 +0800

    Revert "Revert "evm: Update state root on every block (#2213)""
    This reverts commit 6d0a0f5999544b8e0dd1a6a57dc723dc4405055a.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -301,6 +301,7 @@ BASE_SCRIPTS = [
     'feature_evm_smart_contract.py',
     'feature_evm_transferdomain.py',
     'feature_evm.py',
+    'feature_evm_state_root_change.py',
     'feature_loan_low_interest.py',
     'feature_loan_estimatecollateral.py',
     'feature_vault_pct_check_factor.py',
