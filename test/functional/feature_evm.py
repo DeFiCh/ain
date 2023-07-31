92cc487b8b888355c90c29e8ef3823b90521a7f7
Niven <sieniven@gmail.com>
2023-07-31 15:08:51 +0800

    revert feature_evm

--- test/functional/feature_evm.py
+++ test/functional/feature_evm.py
@@ -396,7 +396,7 @@ class EVMTest(DefiTestFramework):
         for i in range(63):
             self.nodes[0].evmtx(eth_address, i, 21, 21001, to_address, 1)
 
-        # Test error at the 64th EVM TX
+        # Test error at the 65th EVM TX
         assert_raises_rpc_error(-26, "too-many-eth-txs-by-sender", self.nodes[0].evmtx, eth_address, 64, 21, 21001, to_address, 1)
 
         # Mint a block
