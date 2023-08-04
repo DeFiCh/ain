b83adfe2201c2bc7b4437bfa5a4d36218e477464
Prasanna Loganathar <pvl@prasannavl.com>
2023-08-04 15:17:50 +0800

    Rename to use try convention

--- lib/ain-rs-exports/src/lib.rs
+++ lib/ain-rs-exports/src/lib.rs
@@ -132,7 +132,7 @@ pub mod ffi {
 
         fn evm_try_get_block_count(result: &mut CrossBoundaryResult) -> u64;
 
-        fn evm_create_dst20(
+        fn evm_try_create_dst20(
             result: &mut CrossBoundaryResult,
             context: u64,
             native_hash: [u8; 32],
@@ -141,7 +141,7 @@ pub mod ffi {
             token_id: &str,
         );
 
-        fn evm_bridge_dst20(
+        fn evm_try_bridge_dst20(
             result: &mut CrossBoundaryResult,
             context: u64,
             address: &str,
