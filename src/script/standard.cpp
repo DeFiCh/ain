7d2a3ea82e419fdde0d403e3a126bede9b361a79
Peter John Bushnell <bushsolo@gmail.com>
2023-08-04 08:15:03 +0100

    Add OP_RETURN for core, DVM and EVM. Check sizes on consensus. (#2268)
    * Add OP_RETURN max size and reject consensus side
    
    * Add OP_RETURN sizes for EVM and DVM
    
    * Update var names. Add tests.

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -12,7 +12,6 @@
 typedef std::vector<unsigned char> valtype;
 
 bool fAcceptDatacarrier = DEFAULT_ACCEPT_DATACARRIER;
-unsigned nMaxDatacarrierBytes = MAX_OP_RETURN_RELAY;
 
 CScriptID::CScriptID(const CScript& in) : uint160(Hash160(in.begin(), in.end())) {}
 
