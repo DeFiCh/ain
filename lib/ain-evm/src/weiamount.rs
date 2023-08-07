a219661c3e9b307a369d07868d1b65584ab67b74
Niven <sieniven@gmail.com>
2023-08-08 01:48:53 +0800

    Add pre- validation check for max money

--- lib/ain-evm/src/weiamount.rs
+++ lib/ain-evm/src/weiamount.rs
@@ -8,6 +8,7 @@ pub struct WeiAmount(pub U256);
 pub const WEI_TO_GWEI: U256 = U256([1_000_000_000, 0, 0, 0]);
 pub const WEI_TO_SATS: U256 = U256([10_000_000_000, 0, 0, 0]);
 pub const GWEI_TO_SATS: U256 = U256([10, 0, 0, 0]);
+pub const MAX_MONEY_SATS: U256 = U256([120_000_000_000_000_000, 0, 0, 0]);
 
 impl WeiAmount {
     pub fn to_gwei(&self) -> U256 {
@@ -16,6 +17,12 @@ impl WeiAmount {
 
     pub fn to_satoshi(&self) -> U256 {
         self.0 / WEI_TO_SATS
+    }
+
+    pub fn wei_range(&self) -> bool {
+        let max_money_sats = MAX_MONEY_SATS;
+        let max_money_wei = max_money_sats.saturating_mul(WEI_TO_SATS);
+        self.0 >= U256::zero() && self.0 <= max_money_wei
     }
 }
 
