9a438ed8528fc7797f6a977f60b75b6c2ba44a55
canonbrother <w.canonbrother@gmail.com>
2023-08-16 16:34:55 +0800

    refine test

--- test/functional/feature_evm_contract_env_vars.py
+++ test/functional/feature_evm_contract_env_vars.py
@@ -100,87 +100,189 @@ class EVMTest(DefiTestFramework):
             address=receipt["contractAddress"], abi=abi
         )
 
-    def run_test(self):
-        self.setup()
-
-        self.should_create_contract()
-
-        node = self.nodes[0]
-
-        tx = {
-            "from": self.evm_key_pair.address,
-            "value": "0x0",
-            # "data": self.contract.encodeABI('mul', [2,3]),
-            "chainId": node.w3.eth.chain_id,
-            "nonce": node.w3.eth.get_transaction_count(self.evm_key_pair.address),
-            # "gasPrice": 10_000_000_000,
-            "gas": 1_000_000,
-            "maxFeePerGas": 15_000_000_000,
-            "maxPriorityFeePerGas": 9_000_000_000,
-            "type": "0x2",
-        }
-
+    def test_blockhash(self):
         b1hash = self.contract.functions.blockHash(1).call().hex()
-        b1 = node.eth_getBlockByNumber(1, False)
+        b1 = self.nodes[0].eth_getBlockByNumber(1, False)
         assert_equal(f"0x{b1hash}", b1["hash"])
 
+    def test_block_basefee(self):
         base_fee = self.contract.functions.baseFee().call()
         assert_equal(base_fee, 10_000_000_000)
 
+    def test_block_chainid(self):
         chain_id = self.contract.functions.chainId().call()
         assert_equal(chain_id, 1133)
 
+    def test_coinbase(self):
         block = self.nodes[0].getblock(self.nodes[0].getblockhash(self.nodes[0].getblockcount()))
         raw_tx = self.nodes[0].getrawtransaction(block['tx'][0], 1)
         opreturn_miner_keyid = raw_tx['vout'][1]['scriptPubKey']['hex'][120:]
         miner_eth_address = self.nodes[0].addressmap(self.nodes[0].get_genesis_keys().operatorAuthAddress, 1)
         miner_eth_keyid = self.nodes[0].getaddressinfo(miner_eth_address['format']['erc55'])['witness_program']
-        # assert_equal(opreturn_miner_keyid, miner_eth_keyid)
 
         coinbase = self.contract.functions.coinbase().call()
         assert_equal(coinbase, self.nodes[0].w3.to_checksum_address(f"0x{opreturn_miner_keyid}"))
 
+    def test_difficulty(self):
         difficulty = self.contract.functions.difficulty().call()
         assert_equal(difficulty, 0)
 
+    def test_block_gaslimit(self):
         gas = self.contract.functions.gasLimit().call()
         assert_equal(gas, 30_000_000)
 
+    def test_block_number(self):
         n = self.contract.functions.blockNumber().call()
-        bn = node.eth_blockNumber()
+        bn = self.nodes[0].eth_blockNumber()
         assert_equal(f"0x{n}", bn)
 
+    def test_timestamp(self):
+        block = self.nodes[0].getblock(self.nodes[0].getblockhash(self.nodes[0].getblockcount()))
         ts = self.contract.functions.timestamp().call()
         assert_equal(ts, block["time"])
 
+    def test_gasleft(self):
+        tx = {
+            "from": self.evm_key_pair.address,
+            "value": "0x0",
+            "chainId": self.nodes[0].w3.eth.chain_id,
+            "nonce": self.nodes[0].w3.eth.get_transaction_count(self.evm_key_pair.address),
+            "gas": 1_000_000,
+            "maxFeePerGas": 15_000_000_000,
+            "maxPriorityFeePerGas": 9_000_000_000,
+            "type": "0x2",
+        }
         gas_left = self.contract.functions.gasLeft().call(tx) # 978_708
         assert tx["gas"] > gas_left
 
+    def test_msg_sender(self):
         sender = self.contract.functions.getSender().call()
         assert_equal(sender, self.evm_key_pair.address)
 
+    def test_msg_value(self):
+        tx = {
+            "from": self.evm_key_pair.address,
+            "value": "0x1a",
+            "chainId": self.nodes[0].w3.eth.chain_id,
+            "nonce": self.nodes[0].w3.eth.get_transaction_count(self.evm_key_pair.address),
+            "gas": 1_000_000,
+            "maxFeePerGas": 15_000_000_000,
+            "maxPriorityFeePerGas": 9_000_000_000,
+            "type": "0x2",
+        }
+        value = self.contract.functions.getValue().call(tx)
+        assert_equal(value, int(tx["value"], base=16))
+
+    def test_msg_data(self):
+        tx = {
+            "from": self.evm_key_pair.address,
+            "value": "0x0",
+            "chainId": self.nodes[0].w3.eth.chain_id,
+            "nonce": self.nodes[0].w3.eth.get_transaction_count(self.evm_key_pair.address),
+            "gas": 1_000_000,
+            "maxFeePerGas": 15_000_000_000,
+            "maxPriorityFeePerGas": 9_000_000_000,
+            "type": "0x2",
+        }
         data = self.contract.encodeABI('getData', [])
         d = self.contract.functions.getData().call(tx)
         assert_equal(data, f"0x{d.hex()}")
 
+    def test_msg_sig(self):
+        tx = {
+            "from": self.evm_key_pair.address,
+            "value": "0x0",
+            "chainId": self.nodes[0].w3.eth.chain_id,
+            "nonce": self.nodes[0].w3.eth.get_transaction_count(self.evm_key_pair.address),
+            "gas": 1_000_000,
+            "maxFeePerGas": 15_000_000_000,
+            "maxPriorityFeePerGas": 9_000_000_000,
+            "type": "0x2",
+        }
         data = self.contract.encodeABI('getSig', [])
         sig = self.contract.functions.getSig().call(tx)
         assert_equal(data, f"0x{sig.hex()}")
 
-        # tx_gas_price = self.contract.functions.getTxGasPrice().call(tx)
-        # assert_equal(tx_gas_price, tx["gasPrice"])
+    def test_tx_gasprice(self):
+        tx = {
+            "from": self.evm_key_pair.address,
+            "value": "0x0",
+            "chainId": self.nodes[0].w3.eth.chain_id,
+            "nonce": self.nodes[0].w3.eth.get_transaction_count(self.evm_key_pair.address),
+            "gasPrice": 10_000_000_000,
+            "gas": 1_000_000,
+        }
+        tx_gas_price = self.contract.functions.getTxGasPrice().call(tx)
+        assert_equal(tx_gas_price, tx["gasPrice"])
+
+        tx = {
+            "from": self.evm_key_pair.address,
+            "value": "0x0",
+            "chainId": self.nodes[0].w3.eth.chain_id,
+            "nonce": self.nodes[0].w3.eth.get_transaction_count(self.evm_key_pair.address),
+            "gas": 1_000_000,
+            "maxFeePerGas": 15_000_000_000,
+            "maxPriorityFeePerGas": 9_000_000_000,
+            "type": "0x2",
+        }
         tx_gas_price = self.contract.functions.getTxGasPrice().call(tx)
         assert_equal(tx_gas_price, tx["maxFeePerGas"])
 
+    def test_tx_origin(self):
+        tx = {
+            "from": self.evm_key_pair.address,
+            "value": "0x0",
+            "chainId": self.nodes[0].w3.eth.chain_id,
+            "nonce": self.nodes[0].w3.eth.get_transaction_count(self.evm_key_pair.address),
+            "gas": 1_000_000,
+            "maxFeePerGas": 15_000_000_000,
+            "maxPriorityFeePerGas": 9_000_000_000,
+            "type": "0x2",
+        }
         tx_origin = self.contract.functions.getTxOrigin().call(tx)
         assert_equal(tx_origin, self.evm_key_pair.address)
 
-        tx["value"] = "0x1a"
-        value = self.contract.functions.getValue().call(tx)
-        assert_equal(value, int(tx["value"], base=16))
-
+    def test_call_contract_property(self):
         count = self.contract.functions.count().call()
         assert_equal(count, 45)
+
+
+    def run_test(self):
+        self.setup()
+
+        self.should_create_contract()
+
+        self.test_blockhash() # blockhash(number)
+
+        self.test_block_basefee() # block.basefee
+
+        self.test_block_chainid() # block.chainid
+
+        self.test_coinbase() # block.coinbase
+
+        self.test_difficulty() # block.difficulty
+
+        self.test_block_gaslimit() # block.gaslimit
+
+        self.test_block_number() # block.number
+
+        self.test_timestamp() # block.timestamp
+
+        self.test_gasleft() # gasleft()
+
+        self.test_msg_sender() # msg.sender
+
+        self.test_msg_value() # msg.value
+
+        self.test_msg_data() # msg.data
+
+        self.test_msg_sig() # msg.sig
+
+        self.test_tx_gasprice() # tx.gasprice
+
+        self.test_tx_origin() # tx.origin
+
+        self.test_call_contract_property()
 
 if __name__ == "__main__":
     EVMTest().main()
