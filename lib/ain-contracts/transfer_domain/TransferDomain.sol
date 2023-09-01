5d9659f78d32d32361b99f52c8da7cabfea9e9f9
jouzo <jdesclercs@gmail.com>
2023-09-01 17:11:21 +0100

    Spoof ERC20

--- lib/ain-contracts/transfer_domain/TransferDomain.sol
+++ lib/ain-contracts/transfer_domain/TransferDomain.sol
@@ -23,7 +23,39 @@ contract TransferDomain {
             to.transfer(amount);
         }
 
-        emit Transfer(from, to, amount / 1 ether);
+        emit Transfer(from, to, amount);
         emit NativeAddress(nativeAddress);
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view virtual returns (string memory) {
+        return "DFI";
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view virtual returns (string memory) {
+        return "DFI";
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the default value returned by this function, unless
+     * it's overridden.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual returns (uint8) {
+        return 18;
     }
 }
