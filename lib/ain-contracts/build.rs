4fd73b6a888c7f9315c3e18bd37b10e2e85a1616
Prasanna Loganathar <pvl@prasannavl.com>
2023-09-05 11:21:05 +0800

    Cleanup ain contract build script

--- lib/ain-contracts/build.rs
+++ lib/ain-contracts/build.rs
@@ -3,28 +3,31 @@ use std::fs;
 use std::path::PathBuf;
 
 use anyhow::format_err;
+use anyhow::{Result};
 use ethers_solc::{Project, ProjectPathsConfig, Solc};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // compile solidity project
-    // configure `root` as our project root
+fn main() -> Result<()> {
+    let out_dir: PathBuf = PathBuf::from(env::var("OUT_DIR")?);
+    let solc_path_str = env::var("SOLC_PATH")?;
+
+    // Solidity project root and contract names relative to our project
     let contracts = vec![
         ("dfi_intrinsics", "DFIIntrinsics"),
         ("dst20", "DST20"),
         ("system_reserved", "SystemReservedContract"),
     ];
 
-    for (file_path, contract_name) in contracts {
-        let solc = Solc::new(env::var("SOLC_PATH")?);
-        let output_path = env::var("CARGO_TARGET_DIR")?;
-        let root = PathBuf::from(file_path);
-        if !root.exists() {
-            return Err("Project root {root:?} does not exists!".into());
+    for (sol_project_name, contract_name) in contracts {
+        let solc = Solc::new(solc_path_str.clone());
+
+        let sol_project_root = PathBuf::from(sol_project_name);
+        if !sol_project_root.exists() {
+            return Err(format_err!("Solidity project missing: {sol_project_root:?}"));
         }
 
         let paths = ProjectPathsConfig::builder()
-            .root(&root)
-            .sources(&root)
+            .root(&sol_project_root)
+            .sources(&sol_project_root)
             .build()?;
 
         let project = Project::builder()
@@ -36,21 +39,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let output = project.compile().unwrap();
         let artifacts = output.into_artifacts();
 
+        let sol_project_outdir = out_dir.join(sol_project_name);
+
         for (id, artifact) in artifacts {
             if id.name == contract_name {
                 let abi = artifact.abi.ok_or_else(|| format_err!("ABI not found"))?;
-                let bytecode = artifact.deployed_bytecode.expect("No bytecode found");
+                let bytecode = artifact
+                    .deployed_bytecode
+                    .ok_or_else(|| format_err!("Bytecode not found"))?;
+                let bytecode_out_path = sol_project_outdir.clone().join("bytecode.json");
+                let abi_out_path = sol_project_outdir.clone().join("abi.json");
 
-                fs::create_dir_all(format!("{output_path}/ain_contracts/{file_path}"))?;
+                fs::create_dir_all(sol_project_outdir.clone())?;
                 fs::write(
-                    PathBuf::from(format!(
-                        "{output_path}/ain_contracts/{file_path}/bytecode.json"
-                    )),
-                    serde_json::to_string(&bytecode).unwrap().as_bytes(),
+                    bytecode_out_path,
+                    serde_json::to_string(&bytecode)?.as_bytes(),
                 )?;
                 fs::write(
-                    PathBuf::from(format!("{output_path}/ain_contracts/{file_path}/abi.json")),
-                    serde_json::to_string(&abi).unwrap().as_bytes(),
+                    abi_out_path,
+                    serde_json::to_string(&abi)?.as_bytes(),
                 )?;
             }
         }
