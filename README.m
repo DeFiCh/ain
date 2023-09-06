fda3a5f8af9e5f0ef6f2ae2411b85804806b504a
Bunyuh// <aryanfrank8@gmail.com>
2023-09-07 06:33:01 +0800

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,23 @@
+(https://github.com/DeFiCh/ain/actions/workflows/lint.yaml/badge.svg)](https://github.com/DeFiCh/ain/actions/workflows/lint.yaml)
+(https://github.com/DeFiCh/ain/actions/workflows/tests-e2e.yaml/badge.svg)](https://github.com/DeFiCh/ain/actions/workflows/tests-e2e.yaml)
+(https://github.com/DeFiCh/ain/actions/workflows/build-dev.yaml/badge.svg)](https://github.com/DeFiCh/ain/actions/workflows/build-dev.yaml)
+
+(https://defichain.com)?
+
+(https://defichain.com)
+
+(https://defichain.com/white-paper/)
+
+(https://github.com/DeFiCh/ain/releases) page.
+
+[https://github.com/DeFiCh/ain/releases] or compile from the release tags.
+
+[https://github.com/bitcoin/bitcoin] 
+
+[https://github.com/bitcoin/bitcoin/commit/7d6f63cc2c2b9c4f07a43619eef0b7314474fffd]– which is slightly
+
+( ports: `666', testnet ports: `666`, changi ports: `666`, devnet ports: `666`, regnet ports: `666`. https://opensource.org/licenses//
+
+ (https://github.com/DeFiCh/ain/tags) are created regularly to indicate new official, stable release
+
+(mailto:engineering@defichain.com) for any questions or collaborations.
