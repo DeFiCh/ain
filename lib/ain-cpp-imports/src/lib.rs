b35281497691f857a1c332ee740a252bb1c25948
Peter John Bushnell <bushsolo@gmail.com>
2023-09-15 16:39:47 +0100

    Include Transfer Domain TXs in getPoolTransactions (#2454)
    * Include Transfer Domain TXs in getPoolTransactions
    
    * Remove EVM check
    
    * Add type and direction to getPoolTransactions

--- lib/ain-cpp-imports/src/lib.rs
+++ lib/ain-cpp-imports/src/lib.rs
@@ -21,6 +21,12 @@ mod ffi {
         pub symbol: String,
     }
 
+    pub struct TransactionData {
+        pub tx_type: u8,
+        pub data: String,
+        pub direction: u8,
+    }
+
     const UNIMPL_MSG: &str = "This cannot be used on a test path";
     pub fn getChainId() -> u64 {
         unimplemented!("{}", UNIMPL_MSG)
@@ -49,7 +55,7 @@ mod ffi {
     pub fn getChainWork(_block_hash: [u8; 32]) -> [u8; 32] {
         unimplemented!("{}", UNIMPL_MSG)
     }
-    pub fn getPoolTransactions() -> Vec<String> {
+    pub fn getPoolTransactions() -> Vec<TransactionData> {
         unimplemented!("{}", UNIMPL_MSG)
     }
     pub fn getNativeTxSize(_data: Vec<u8>) -> u64 {
@@ -128,7 +134,7 @@ pub fn get_chainwork(block_hash: [u8; 32]) -> Result<[u8; 32], Box<dyn Error>> {
     Ok(chainwork)
 }
 
-pub fn get_pool_transactions() -> Result<Vec<String>, Box<dyn Error>> {
+pub fn get_pool_transactions() -> Result<Vec<ffi::TransactionData>, Box<dyn Error>> {
     let transactions = ffi::getPoolTransactions();
     Ok(transactions)
 }
