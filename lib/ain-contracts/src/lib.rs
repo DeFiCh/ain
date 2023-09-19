f72ad5fb8d2de9f9be36f315b17a740a0a8b941d
Shoham Chakraborty <shhmchk@gmail.com>
2023-09-19 10:49:35 +0800

    Fix Rust lint (#2467)

--- lib/ain-contracts/src/lib.rs
+++ lib/ain-contracts/src/lib.rs
@@ -203,17 +203,50 @@ mod test {
     #[test]
     fn test_addr_gen() {
         let items: Vec<(Result<H160>, &str)> = vec![
-            (generate_intrinsic_addr(DST20_ADDR_PREFIX_BYTE, 0x0), "ff00000000000000000000000000000000000000"),
-            (generate_intrinsic_addr(DST20_ADDR_PREFIX_BYTE, 0x1), "ff00000000000000000000000000000000000001"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x0), "df00000000000000000000000000000000000000"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x1), "df00000000000000000000000000000000000001"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x2), "df00000000000000000000000000000000000002"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xff), "df000000000000000000000000000000000000ff"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xfe), "df000000000000000000000000000000000000fe"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x1fffe), "df0000000000000000000000000000000001fffe"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xffff), "df0000000000000000000000000000000000ffff"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xffff_ffff_ffff_ffff), "df0000000000000000000000ffffffffffffffff"),
-            (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xefff_ffff_ffff_ffff), "df0000000000000000000000efffffffffffffff"),
+            (
+                generate_intrinsic_addr(DST20_ADDR_PREFIX_BYTE, 0x0),
+                "ff00000000000000000000000000000000000000",
+            ),
+            (
+                generate_intrinsic_addr(DST20_ADDR_PREFIX_BYTE, 0x1),
+                "ff00000000000000000000000000000000000001",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x0),
+                "df00000000000000000000000000000000000000",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x1),
+                "df00000000000000000000000000000000000001",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x2),
+                "df00000000000000000000000000000000000002",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xff),
+                "df000000000000000000000000000000000000ff",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xfe),
+                "df000000000000000000000000000000000000fe",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x1fffe),
+                "df0000000000000000000000000000000001fffe",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xffff),
+                "df0000000000000000000000000000000000ffff",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xffff_ffff_ffff_ffff),
+                "df0000000000000000000000ffffffffffffffff",
+            ),
+            (
+                generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0xefff_ffff_ffff_ffff),
+                "df0000000000000000000000efffffffffffffff",
+            ),
             // Should not compile
             // (generate_intrinsic_addr(INTRINSICS_ADDR_PREFIX_BYTE, 0x1ffff_ffff_ffff_ffff), "df0000000000000000000001ffffffffffffffff"),
         ];
