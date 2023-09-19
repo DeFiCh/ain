d0b42ca3eab37d80725afbc237fff58a6fed541f
Bushstar <bushsolo@gmail.com>
2023-09-19 08:36:50 +0100

    Add missing Dakota fork to tests

--- test/functional/feature_evm_fee.py
+++ test/functional/feature_evm_fee.py
@@ -22,6 +22,7 @@ class EVMFeeTest(DefiTestFramework):
                 "-txnotokens=0",
                 "-amkheight=50",
                 "-bayfrontheight=51",
+                "-dakotaheight=51",
                 "-eunosheight=80",
                 "-fortcanningheight=82",
                 "-fortcanninghillheight=84",
--- test/functional/feature_evm_smart_contract.py
+++ test/functional/feature_evm_smart_contract.py
@@ -21,6 +21,7 @@ class EVMTest(DefiTestFramework):
                 "-txnotokens=0",
                 "-amkheight=50",
                 "-bayfrontheight=51",
+                "-dakotaheight=51",
                 "-eunosheight=80",
                 "-fortcanningheight=82",
                 "-fortcanninghillheight=84",
