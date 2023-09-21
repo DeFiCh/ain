653fda9c166dc119375d16ab7e70f72b8d6586fa
Shoham Chakraborty <shhmchk@gmail.com>
2023-09-21 18:33:33 +0800

    Add old asserts

--- test/functional/feature_evm_dst20.py
+++ test/functional/feature_evm_dst20.py
@@ -188,6 +188,9 @@ class DST20(DefiTestFramework):
             self.bytecode,
         )
 
+        assert_equal(tx1["input"], tx2["input"])
+        assert_equal(tx2["input"], tx3["input"])
+
         self.rollback_to(block_height)
 
     def test_unused_dst20(self):
