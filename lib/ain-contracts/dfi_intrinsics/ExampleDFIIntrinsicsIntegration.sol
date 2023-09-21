124c86576b9573c96e2babec19dfc7342e78d2ca
Cuong  Truong <cuong@cakedefi.com>
2023-09-21 22:25:49 +0800

    change the example to using DFIRegistry

--- lib/ain-contracts/dfi_intrinsics/ExampleDFIIntrinsicsIntegration.sol
+++ lib/ain-contracts/dfi_intrinsics/ExampleDFIIntrinsicsIntegration.sol
@@ -2,20 +2,25 @@
 pragma solidity ^0.8.0;
 
 import "./IDFIIntrinsicsV1.sol";
+import "../dfi_intrinsics_registry/IDFIIntrinsicsRegistry.sol";
 
 contract ExampleDFIIntrinsicsV1Integration {
 
-    address _DFIIntrinsicsV1Address;
+    address _DFIIntrinsicsRegistry;
+
+    function _getDFIIntrinsicsV1() internal view returns(address) {
+        return IDFIIntrinsicsRegistry(_DFIIntrinsicsRegistry).getAddressForVersion(1);
+    }
     
     function getVersion() external view returns (uint256) {
-        return IDFIIntrinsicsV1(_DFIIntrinsicsV1Address).version();
+        return IDFIIntrinsicsV1(_getDFIIntrinsicsV1()).version();
     }
 
     function getEvmBlockCount() external view returns (uint256) {
-        return IDFIIntrinsicsV1(_DFIIntrinsicsV1Address).evmBlockCount();
+        return IDFIIntrinsicsV1(_getDFIIntrinsicsV1()).evmBlockCount();
     }
 
     function getDvmBlockCount() external view returns (uint256) {
-        return IDFIIntrinsicsV1(_DFIIntrinsicsV1Address).dvmBlockCount();
+        return IDFIIntrinsicsV1(_getDFIIntrinsicsV1()).dvmBlockCount();
     }
 }
--- lib/ain-contracts/dfi_intrinsics_registry/DFIIntrinsicsRegistry.sol
+++ lib/ain-contracts/dfi_intrinsics_registry/DFIIntrinsicsRegistry.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
+import "./IDFIIntrinsicsRegistry.sol";
+
 pragma solidity ^0.8.0;
 
-contract DFIIntrinsicsRegistry {
+contract DFIIntrinsicsRegistry is IDFIIntrinsicsRegistry {
     mapping(uint256 => address) private _versionToAddress;
 
     function getAddressForVersion(uint256 _version) public view returns (address) {
