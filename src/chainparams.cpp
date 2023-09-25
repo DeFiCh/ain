4beb9254e64748d425dc632e5a2eac8b8531e8a5
Prasanna Loganathar <pvl@prasannavl.com>
2023-09-25 12:51:31 +0800

    [testing] Ocean pre-checks

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -417,7 +417,7 @@ public:
         consensus.FortCanningEpilogueHeight = 1244000;
         consensus.GrandCentralHeight = 1366000;
         consensus.GrandCentralEpilogueHeight = 1438200;
-        consensus.NextNetworkUpgradeHeight = std::numeric_limits<int>::max();;
+        consensus.NextNetworkUpgradeHeight = 1913900;
 
         consensus.pos.diffLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 //        consensus.pos.nTargetTimespan = 14 * 24 * 60 * 60; // two weeks
