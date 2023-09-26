deef7d71ac31aabe151337b7040f17c63b0fae0d
jouzo <jdesclercs@gmail.com>
2023-09-26 11:44:29 +0100

    prepay gas fee cleanup

--- lib/ain-evm/src/fee.rs
+++ lib/ain-evm/src/fee.rs
@@ -1,17 +1,14 @@
 use anyhow::format_err;
-use ethereum::TransactionV2;
 use ethereum_types::U256;
 
 use crate::{transaction::SignedTx, Result};
 
 pub fn calculate_prepay_gas_fee(signed_tx: &SignedTx, base_fee: U256) -> Result<U256> {
-    let effective_gas_price = signed_tx.effective_gas_price(base_fee);
-    match &signed_tx.transaction {
-        TransactionV2::Legacy(tx) => tx.gas_limit.checked_mul(effective_gas_price),
-        TransactionV2::EIP2930(tx) => tx.gas_limit.checked_mul(effective_gas_price),
-        TransactionV2::EIP1559(tx) => tx.gas_limit.checked_mul(effective_gas_price),
-    }
-    .ok_or_else(|| format_err!("calculate prepay gas failed from overflow").into())
+    let gas_limit = signed_tx.gas_limit();
+
+    gas_limit
+        .checked_mul(signed_tx.effective_gas_price(base_fee))
+        .ok_or_else(|| format_err!("calculate prepay gas failed from overflow").into())
 }
 
 // Gas prices are denoted in wei
