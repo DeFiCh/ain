a08320dad0ee5c953afabe346a4209f121998a8f
Prasanna Loganathar <pvl@prasannavl.com>
2023-10-09 10:12:32 +0800

    Cache tx hash on SignedTx

--- lib/ain-evm/src/transaction/mod.rs
+++ lib/ain-evm/src/transaction/mod.rs
@@ -100,6 +100,7 @@ impl From<&LegacyTransaction> for LegacyUnsignedTransaction {
 pub struct SignedTx {
     pub transaction: TransactionV2,
     pub sender: H160,
+    hash_cache: Cell<Option<H256>>,
 }
 
 impl fmt::Debug for SignedTx {
@@ -179,6 +180,7 @@ impl TryFrom<TransactionV2> for SignedTx {
         Ok(SignedTx {
             transaction: src,
             sender: public_key_to_address(&pubkey),
+            hash_cache: Cell::new(None),
         })
     }
 }
@@ -317,11 +319,16 @@ impl SignedTx {
     }
 
     pub fn hash(&self) -> H256 {
-        match &self.transaction {
-            TransactionV2::Legacy(tx) => tx.hash(),
-            TransactionV2::EIP2930(tx) => tx.hash(),
-            TransactionV2::EIP1559(tx) => tx.hash(),
+        let h = &self.hash_cache;
+        if h.get().is_none() {
+            let val = match &self.transaction {
+                TransactionV2::Legacy(tx) => tx.hash(),
+                TransactionV2::EIP2930(tx) => tx.hash(),
+                TransactionV2::EIP1559(tx) => tx.hash(),
+            };
+            h.set(Some(val));
         }
+        h.get().unwrap()
     }
 
     pub fn get_tx_type(&self) -> U256 {
@@ -332,7 +339,7 @@ impl SignedTx {
 use std::{
     cmp::min,
     convert::{TryFrom, TryInto},
-    fmt,
+    fmt, cell::Cell,
 };
 
 #[derive(Debug)]
