52154bcdcd7199aae82e8b2c2ea996c0e4366c30
Prasanna Loganathar <pvl@prasannavl.com>
2023-11-02 14:26:26 +0800

    Bail ECC pre-cache tail tasks early if TX validation is done

--- src/validation.cpp
+++ src/validation.cpp
@@ -3273,6 +3273,12 @@ bool CChainState::ConnectBlock(const CBlock &block,
         UpdateCoins(tx, view, i == 0 ? undoDummy : blockundo.vtxundo.back(), pindex->nHeight);
     }
 
+
+    // If it's not completed by now, we don't need it anymore. 
+    // Bail here so that other concurrent tasks won't be awaiting on these
+    // unnecessarily. 
+    evmEccPreCacheTaskPool.MarkCancelled();
+
     int64_t nTime3 = GetTimeMicros();
     nTimeConnect += nTime3 - nTime2;
     LogPrint(BCLog::BENCH,
