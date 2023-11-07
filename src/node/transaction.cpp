81ec357fa6ffc1bc0ae9f44c909fa7cab053926e
Bushstar <bushsolo@gmail.com>
2023-11-07 12:41:44 +0000

    lint: circular deps

--- src/node/transaction.cpp
+++ src/node/transaction.cpp
@@ -4,7 +4,6 @@
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
 #include <consensus/validation.h>
-#include <dfi/masternodes.h>
 #include <net.h>
 #include <net_processing.h>
 #include <txmempool.h>
