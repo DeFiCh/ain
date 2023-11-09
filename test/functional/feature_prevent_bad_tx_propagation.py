365a3ea1806e8d167d3a34e53d8a8869822ae0a5
Bushstar <bushsolo@gmail.com>
2023-11-09 10:32:02 +0000

    Remove unused import

--- test/functional/feature_prevent_bad_tx_propagation.py
+++ test/functional/feature_prevent_bad_tx_propagation.py
@@ -5,7 +5,6 @@
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 """Test account mining behaviour"""
 
-from test_framework.authproxy import JSONRPCException
 from test_framework.test_framework import DefiTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error
 
