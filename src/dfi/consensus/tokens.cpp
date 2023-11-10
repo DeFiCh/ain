1c7b4d3d62beef06d650b866b5d99661a27580be
Niven <sieniven@gmail.com>
2023-11-10 14:53:41 +0800

    Remove import

--- src/dfi/consensus/tokens.cpp
+++ src/dfi/consensus/tokens.cpp
@@ -2,7 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-#include <ain_rs_exports.h>
 #include <coins.h>
 #include <consensus/params.h>
 #include <dfi/consensus/tokens.h>
