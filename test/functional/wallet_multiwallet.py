93af12276510f9ce3a0a9a763d7fd4965b5c7acd
Bushstar <bushsolo@gmail.com>
2023-11-21 16:27:06 +0000

    Revert "Build: Upgrade boost dependency to version 1.83 (#2696)"
    This reverts commit d7c7b834b81a766d6bff192e9c580f00c4746f5d.

--- test/functional/wallet_multiwallet.py
+++ test/functional/wallet_multiwallet.py
@@ -104,7 +104,7 @@ class MultiWalletTest(DefiTestFramework):
             assert_equal(os.path.isfile(wallet_file(wallet_name)), True)
 
         # should not initialize if wallet path can't be created
-        exp_stderr = "boost::filesystem::create_directories:"
+        exp_stderr = "boost::filesystem::create_directory:"
         self.nodes[0].assert_start_raises_init_error(
             ["-wallet=wallet.dat/bad"], exp_stderr, match=ErrorMatch.PARTIAL_REGEX
         )
