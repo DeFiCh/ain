1d5b4640b5cf94c7f60b90fca3aaa339a92a0b3f
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-11-22 23:46:12 +0800

    added server

--- lib/ain-ocean/src/server.rs
+++ lib/ain-ocean/src/server.rs
@@ -5,15 +5,4 @@ pub struct OceanRPCServer {
     pub config: rpc_config,
 }
 
-impl OceanRPCServer {
-    fn new(config: rpc_config) -> Self {
-        Self { config }
-    }
-
-    async fn start_server(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let addr = self.config.listen.to_string(); // Handle the error as needed
-        let server = self.config.listen.to_string();
-
-        Ok(())
-    }
-}
+impl OceanRPCServer {}
