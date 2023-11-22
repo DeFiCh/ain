49df161a07941671a61617e9f4e6d1c6f3135247
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-11-22 23:46:12 +0800

    added server

--- lib/ain-ocean/src/config.rs
+++ lib/ain-ocean/src/config.rs
@@ -3,12 +3,3 @@ pub struct Config {
     pub enable: bool,
     pub listen: String,
 }
-
-impl Config {
-    fn default() -> Config {
-        Config {
-            enable: false,
-            listen: "3000".to_string(),
-        }
-    }
-}
