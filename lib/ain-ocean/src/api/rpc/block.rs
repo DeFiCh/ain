b2293265a73e1f71c41506e7cc177d05ed837e0c
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-11-26 20:46:07 +0800

    added default value to config

--- lib/ain-ocean/src/api/rpc/block.rs
+++ lib/ain-ocean/src/api/rpc/block.rs
@@ -2,12 +2,12 @@ use anyhow::Result;
 use jsonrpsee::RpcModule;
 
 pub fn register_block_methods(module: &mut RpcModule<()>) -> Result<()> {
-    module.register_method("block_getBlock", |params, _| {
+    module.register_method("block_getBlock", |_params, _| {
         // Implement your method logic here
         todo!()
     })?;
 
-    module.register_method("block_submitBlock", |params, _| {
+    module.register_method("block_submitBlock", |_params, _| {
         // Implement your method logic here
         todo!()
     })?;
