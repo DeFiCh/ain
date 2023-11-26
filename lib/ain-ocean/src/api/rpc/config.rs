be00a55d29849c6f8d1a6558a4e940f27543a081
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-11-26 20:37:55 +0800

    added rpc server and block modules

--- lib/ain-ocean/src/api/rpc/config.rs
+++ lib/ain-ocean/src/api/rpc/config.rs
@@ -3,11 +3,12 @@ pub struct Config {
     pub enable: bool,
     pub listen: String,
 }
-impl Default for Config {
-    fn default() -> Self {
+
+impl Config {
+    fn default() -> Config {
         Config {
-            enable: true,
-            listen: "127.0.0.1:3000".to_string(),
+            enable: false,
+            listen: "3000".to_string(),
         }
     }
 }
--- lib/ain-ocean/src/api/rpc/server.rs
+++ lib/ain-ocean/src/api/rpc/server.rs
@@ -13,12 +13,13 @@ impl OceanRPCServer {
     pub async fn start_rpc_server(&self) -> anyhow::Result<()> {
         let listen = self.rpc_config.listen.clone();
         let server = ServerBuilder::default().build(listen).await?;
+        // Create and configure the RPC modules
         let mut block_module = RpcModule::new(());
         register_block_methods(&mut block_module)?;
         let server_handle = server.start(block_module);
         // Wait for a Ctrl+C signal before shutting down
         signal::ctrl_c().await.expect("failed to listen for event");
-        let _ = server_handle.stop();
+        server_handle.stop();
 
         Ok(())
     }
