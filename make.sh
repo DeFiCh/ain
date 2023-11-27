ab6cb01d38d47ac3a6a2cc5d3e7d1e7d9b11fba5
Niven <sieniven@gmail.com>
2023-11-27 09:02:47 +0800

    Make.sh fixes

--- make.sh
+++ make.sh
@@ -745,14 +745,13 @@ pkg_install_deps_osx_tools() {
 
 pkg_install_gh_cli() {
     _fold_start "pkg-install-gh_cli"
-    type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | \
-        sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && \
-        sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+        dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && \
+        chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) \
         signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] \
         https://cli.github.com/packages stable main" | \
-        sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+        tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     apt-get update
     apt-get install -y gh
 
