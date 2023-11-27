2a853fc8132141fffa83961f31eddc1464ccf891
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-11-27 11:29:02 +0800

    added rocks db test cases

--- lib/ain-ocean/src/database/db_test.rs
+++ lib/ain-ocean/src/database/db_test.rs
@@ -2,7 +2,6 @@
 
 extern crate rocksdb;
 extern crate tempdir;
-use crate::database::db_manger::RocksDB;
 use bitcoin::blockdata::block::Header;
 use bitcoin::blockdata::block::Version;
 use bitcoin::blockdata::script::ScriptBuf;
@@ -11,7 +10,6 @@ use bitcoin::blockdata::transaction::Transaction;
 use bitcoin::blockdata::transaction::TxIn;
 use bitcoin::blockdata::transaction::TxOut;
 use bitcoin::blockdata::witness::Witness;
-use bitcoin::consensus::encode::{deserialize, serialize};
 use bitcoin::hash_types::TxMerkleNode;
 use bitcoin::hash_types::Txid;
 use bitcoin::pow::CompactTarget;
