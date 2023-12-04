30a52ac1972cf61041727090f2d3a7f7a0093629
canonbrother <w.canonbrother@gmail.com>
2023-12-04 17:37:56 +0800

    add ApiPageResponse + impl json on list_blocks

--- lib/ain-ocean/src/api/block.rs
+++ lib/ain-ocean/src/api/block.rs
@@ -1,5 +1,7 @@
-use axum::{extract::Path, routing::get, Router};
-use serde::Deserialize;
+use axum::{debug_handler, extract::Path, Json, routing::get, Router};
+use serde::{Deserialize, Serialize};
+
+use crate::api_paged_response::ApiPagedResponse;
 
 #[derive(Deserialize)]
 struct BlockId {
@@ -11,8 +13,29 @@ struct BlockHash {
     hash: String,
 }
 
-async fn list_blocks() -> String {
-    "List of blocks".to_string()
+#[derive(Deserialize)]
+struct ListBlocksRequest {
+    size: usize,
+    next: Option<String>
+}
+
+#[debug_handler]
+async fn list_blocks(Json(req): Json<ListBlocksRequest>) -> Json<ApiPagedResponse<Block>> {
+    // TODO(): query from db
+    // db::block::list(req).await...
+    let blocks = vec![
+        Block { id: "0".into() },
+        Block { id: "1".into() },
+        Block { id: "2".into() },
+    ];
+    
+    Json(ApiPagedResponse
+        ::of(
+            blocks,
+            req.size, 
+            |block| block.clone().id 
+        )
+    )
 }
 
 async fn get_block(Path(BlockId { id }): Path<BlockId>) -> String {
@@ -29,3 +52,35 @@ pub fn router() -> Router {
         .route("/:id", get(get_block))
         .route("/:hash/transactions", get(get_transactions))
 }
+
+#[derive(Clone, Debug, Serialize)]
+#[serde(default)]
+pub struct Block {
+    id: String,
+    // TODO(): type mapping
+    // hash: H256,
+    // previous_hash: H256,
+    
+    // height: u64,
+    // version: u64,
+    // time: u64, // ---------------| block time in seconds since epoch
+    // median_time: u64, // --------| meidan time of the past 11 block timestamps
+    
+    // transaction_count: u64,
+
+    // difficulty: u64,
+    
+    // masternode: H256,
+    // minter: H256,
+    // minter_block_count: u64,
+    // reward: f64
+    
+    // state_modifier: H256,
+    // merkle_root: H256,
+    
+    // size: u64,
+    // size_stripped: u64,
+    // weight: u64,
+}
+
+
