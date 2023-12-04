6c013afbf562028fd3e7a7ffddf52f97d2b3cdf2
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-12-04 23:06:10 +0800

    added method for oracle,price,order_history

--- lib/ain-ocean/src/data_acces/oracle_price_aggregated_interval.rs
+++ lib/ain-ocean/src/data_acces/oracle_price_aggregated_interval.rs
@@ -1,9 +1,12 @@
+use crate::database::db_manger::ColumnFamilyOperations;
 use crate::database::db_manger::RocksDB;
 use crate::model::oracle_price_aggregated_interval::OraclePriceAggregatedInterval;
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 
-pub struct OraclePriceAggregatedIntervalDb {}
+pub struct OraclePriceAggregatedIntervalDb {
+    pub db: RocksDB,
+}
 
 impl OraclePriceAggregatedIntervalDb {
     pub async fn query(
@@ -15,9 +18,26 @@ impl OraclePriceAggregatedIntervalDb {
         todo!()
     }
     pub async fn put(&self, oracle: OraclePriceAggregatedInterval) -> Result<()> {
-        todo!()
+        match serde_json::to_string(&oracle) {
+            Ok(value) => {
+                let key = oracle.id.clone();
+                self.db.put(
+                    "oracle_price_aggregated_interval",
+                    key.as_bytes(),
+                    value.as_bytes(),
+                )?;
+                Ok(())
+            }
+            Err(e) => Err(anyhow!(e)),
+        }
     }
     pub async fn delete(&self, id: String) -> Result<()> {
-        todo!()
+        match self
+            .db
+            .delete("oracle_price_aggregated_interval", id.as_bytes())
+        {
+            Ok(_) => Ok(()),
+            Err(e) => Err(anyhow!(e)),
+        }
     }
 }
--- lib/ain-ocean/src/data_acces/oracle_token_currency.rs
+++ lib/ain-ocean/src/data_acces/oracle_token_currency.rs
@@ -1,18 +1,32 @@
+use crate::database::db_manger::ColumnFamilyOperations;
 use crate::database::db_manger::RocksDB;
 use crate::model::oracle_token_currency::OracleTokenCurrency;
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 
-pub struct OracleTokenCurrencyDb {}
+pub struct OracleTokenCurrencyDb {
+    pub db: RocksDB,
+}
 
 impl OracleTokenCurrencyDb {
     pub async fn query(&self, limit: i32, lt: String) -> Result<Vec<OracleTokenCurrency>> {
         todo!()
     }
     pub async fn put(&self, oracle_token: OracleTokenCurrency) -> Result<()> {
-        todo!()
+        match serde_json::to_string(&oracle_token) {
+            Ok(value) => {
+                let key = oracle_token.id.clone();
+                self.db
+                    .put("oracle_token_currency", key.as_bytes(), value.as_bytes())?;
+                Ok(())
+            }
+            Err(e) => Err(anyhow!(e)),
+        }
     }
     pub async fn delete(&self, id: String) -> Result<()> {
-        todo!()
+        match self.db.delete("oracle_token_currency", id.as_bytes()) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(anyhow!(e)),
+        }
     }
 }
