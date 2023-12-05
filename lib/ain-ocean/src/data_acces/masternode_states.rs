0df6dc39f49ca0f576ee62de353658e8f67049eb
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-12-05 15:49:40 +0800

    update data_acess module

--- lib/ain-ocean/src/data_acces/masternode_states.rs
+++ lib/ain-ocean/src/data_acces/masternode_states.rs
@@ -1,25 +1,64 @@
+use crate::database::db_manger::ColumnFamilyOperations;
 use crate::database::db_manger::RocksDB;
 use crate::model::masternode_stats::MasternodeStats;
 use anyhow::{anyhow, Result};
+use bitcoin::absolute::Height;
+use rocksdb::{ColumnFamilyDescriptor, IteratorMode, DB};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-pub struct MasterStatsDb {}
+#[derive(Debug)]
+pub struct MasterStatsDb {
+    pub db: RocksDB,
+}
 impl MasterStatsDb {
     pub async fn get_latest(&self) -> Result<MasternodeStats> {
+        // let mut latest_stats: Option<MasternodeStats> = None;
+        // let mut highest_height = -1;
+
+        // let iter = self.db.iterator("masternode_stats", IteratorMode::End); // Start from the end of the DB
+
+        // for (key, value) in iter {
+        //     let stats: MasternodeStats = serde_json::from_slice(&value)?;
+        //     if stats.block.height > highest_height {
+        //         highest_height = stats.block.height;
+        //         latest_stats = Some(stats);
+        //     }
+        // }
+
+        // Ok(latest_stats);
         todo!()
     }
     pub async fn query(&self, limit: i32, lt: i32) -> Result<Vec<MasternodeStats>> {
         todo!()
     }
-    pub async fn get(&self) -> Result<MasternodeStats> {
-        todo!()
+    pub async fn get(&self, height: i32) -> Result<Option<MasternodeStats>> {
+        let bytes: &[u8] = &height.to_be_bytes();
+        match self.db.get("masternode_stats", bytes) {
+            Ok(Some(value)) => {
+                let master_states: MasternodeStats =
+                    serde_json::from_slice(&value).map_err(|e| anyhow!(e))?;
+                Ok(Some(master_states))
+            }
+            Ok(None) => Ok(None),
+            Err(e) => Err(anyhow!(e)),
+        }
     }
     pub async fn store(&self, stats: MasternodeStats) -> Result<()> {
-        todo!()
+        match serde_json::to_string(&stats) {
+            Ok(value) => {
+                let key = stats.block.height.clone();
+                let bytes: &[u8] = &key.to_be_bytes();
+                self.db.put("masternode_stats", bytes, value.as_bytes())?;
+                Ok(())
+            }
+            Err(e) => Err(anyhow!(e)),
+        }
     }
     pub async fn delete(&self, height: i32) -> Result<()> {
-        todo!()
+        let bytes: &[u8] = &height.to_be_bytes();
+        match self.db.delete("masternode_stats", bytes) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(anyhow!(e)),
+        }
     }
 }
--- lib/ain-ocean/src/data_acces/transaction_vout.rs
+++ lib/ain-ocean/src/data_acces/transaction_vout.rs
@@ -1,21 +1,42 @@
+use crate::database::db_manger::ColumnFamilyOperations;
 use crate::database::db_manger::RocksDB;
 use crate::model::transaction_vout::TransactionVout;
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Default)]
-#[serde(rename_all = "camelCase")]
-pub struct TransactionVinDb {}
+#[derive(Debug)]
+pub struct TransactionVinDb {
+    pub db: RocksDB,
+}
 
 impl TransactionVinDb {
-    pub async fn get(&self) -> Result<TransactionVout> {
-        todo!()
+    pub async fn get(&self, txid: String, n: i64) -> Result<Option<TransactionVout>> {
+        match self.db.get("transaction_vout", txid.as_bytes()) {
+            Ok(Some(value)) => {
+                let master_node: TransactionVout =
+                    serde_json::from_slice(&value).map_err(|e| anyhow!(e))?;
+                Ok(Some(master_node))
+            }
+            Ok(None) => Ok(None),
+            Err(e) => Err(anyhow!(e)),
+        }
     }
-    pub async fn store(&self, stats: TransactionVout) -> Result<TransactionVout> {
-        todo!()
+    pub async fn store(&self, trx_out: TransactionVout) -> Result<()> {
+        match serde_json::to_string(&trx_out) {
+            Ok(value) => {
+                let key = trx_out.id.clone();
+                self.db
+                    .put("transaction_vout", key.as_bytes(), value.as_bytes())?;
+                Ok(())
+            }
+            Err(e) => Err(anyhow!(e)),
+        }
     }
-    pub async fn delete(&self, id: String) -> Result<TransactionVout> {
-        todo!()
+    pub async fn delete(&self, id: String) -> Result<()> {
+        match self.db.delete("transaction_vout", id.as_bytes()) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(anyhow!(e)),
+        }
     }
     pub async fn query(&self, txid: String, limit: i32, lt: i32) -> Result<TransactionVout> {
         todo!()
