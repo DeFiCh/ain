6bd15ea55256b554a6252980bd75a2a799de5416
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-12-05 11:59:56 +0800

    merged feature/ain-ocean

--- lib/ain-ocean/src/api/mod.rs
+++ lib/ain-ocean/src/api/mod.rs
@@ -30,39 +30,3 @@ pub fn ocean_router() -> Router {
         .nest("/transactions", transactions::router())
         .nest("/blocks", block::router())
 }
-pub mod rpc;
-use std::net::SocketAddr;
-
-use axum::{Router, Server};
-use axum::Router;
-
-mod address;
-mod block;
-mod fee;
-mod governance;
-mod loan;
-mod masternode;
-mod oracle;
-mod poolpairs;
-mod prices;
-mod rawtx;
-mod stats;
-mod tokens;
-mod transactions;
-
-pub fn ocean_router() -> Router {
-    Router::new()
-        .nest("/address", address::router())
-        .nest("/governance", governance::router())
-        .nest("/loans", loan::router())
-        .nest("/fee", fee::router())
-        .nest("/masternodes", masternode::router())
-        .nest("/oracles", oracle::router())
-        .nest("/poolpairs", poolpairs::router())
-        .nest("/prices", prices::router())
-        .nest("/rawtx", rawtx::router())
-        .nest("/stats", stats::router())
-        .nest("/tokens", tokens::router())
-        .nest("/transactions", transactions::router())
-        .nest("/blocks", block::router())
-}
