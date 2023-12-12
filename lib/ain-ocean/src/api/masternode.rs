65beb08c5f8b8ed2634209f7e9f852b7156b1624
canonbrother <w.canonbrother@gmail.com>
2023-12-12 16:16:23 +0800

    masternode typing

--- lib/ain-ocean/src/api/masternode.rs
+++ lib/ain-ocean/src/api/masternode.rs
@@ -1,11 +1,56 @@
-use axum::{extract::Path, routing::get, Router};
+use axum::{extract::{Path, Query}, routing::get, Router, Json};
 
-async fn list_masternodes() -> String {
-    "List of masternodes".to_string()
+use crate::{
+    api_paged_response::ApiPagedResponse,
+    api_query::PaginationQuery,
+    error::OceanResult,
+    model::masternode::{MasternodeData, MasternodeState, MasternodeOwner, MasternodeOperator},
+};
+
+
+async fn list_masternodes(Query(query): Query<PaginationQuery>) -> OceanResult<Json<ApiPagedResponse<Masternode>>> {
+    let masternodes = vec![
+        MasternodeData {
+            id: "e86c027861cc0af423313f4152a44a83296a388eb51bf1a6dde9bd75bed55fb4".into(),
+            sort: "00000000e86c027861cc0af423313f4152a44a83296a388eb51bf1a6dde9bd75bed55fb4".into(),
+            state: MasternodeState::Enabled,
+            minted_blocks: 2,
+            owner: MasternodeOwner {
+                address: "mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU".into(),
+            },
+            operator: MasternodeOperator {
+                address: "mswsMVsyGMj1FzDMbbxw2QW3KvQAv2FKiy".into(),
+            },
+            creation: MasternodeCreation {
+                height: 0
+            },
+            resign: None,
+            timelock: 0
+        }
+    ];
+    Ok(Json(ApiPagedResponse::of(masternodes, query.size, |masternode| {
+        masternode.clone().id
+    })))
 }
 
-async fn get_masternode(Path(masternode_id): Path<String>) -> String {
-    format!("Details of masternode with id {}", masternode_id)
+async fn get_masternode(Path(masternode_id): Path<String>) -> OceanResult<Json<Masternode>> {
+    Ok(Json(Masternode {
+        id: "e86c027861cc0af423313f4152a44a83296a388eb51bf1a6dde9bd75bed55fb4".into(),
+        sort: "00000000e86c027861cc0af423313f4152a44a83296a388eb51bf1a6dde9bd75bed55fb4".into(),
+        state: MasternodeState::Enabled,
+        minted_blocks: 2,
+        owner: MasternodeOwner {
+            address: "mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU".into(),
+        },
+        operator: MasternodeOperator {
+            address: "mswsMVsyGMj1FzDMbbxw2QW3KvQAv2FKiy".into(),
+        },
+        creation: MasternodeCreation {
+            height: 0
+        },
+        resign: None,
+        timelock: 0
+    }))
 }
 
 pub fn router() -> Router {
--- lib/ain-ocean/src/model/masternode.rs
+++ lib/ain-ocean/src/model/masternode.rs
@@ -25,6 +25,65 @@ pub struct MasternodeBlock {
     pub median_time: u64,
 }
 
+enum MasternodeState {
+    PreEnabled,
+    Enabled,
+    PreResigned,
+    Resigned,
+    PreBanned,
+    Banned,
+    Unknown,
+}
+
+impl std::fmt::Display for MasternodeState {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+       match self {
+        MasternodeState::PreEnabled => write!(f, "PRE_ENABLED"),
+        MasternodeState::Enabled => write!(f, "ENABLED"),
+        MasternodeState::PreResigned => write!(f, "PRE_RESIGNED"),
+        MasternodeState::Resigned => write!(f, "RESIGNED"),
+        MasternodeState::PreBanned => write!(f, "PRE_BANNED"),
+        MasternodeState::Banned => write!(f, "BANNED"),
+        MasternodeState::Unknown => write!(f, "UNKNOWN"),
+       } 
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct MasternodeOwner {
+    pub address: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct MasternodeOperator {
+    pub address: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct MasternodeCreation {
+    pub height: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct MasternodeResign {
+    pub tx: String,
+    pub height: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct MasternodeData {
+    pub id: String,
+    pub sort: String,
+    pub state: MasternodeState,
+    pub minted_blocks: i32,
+    pub owner: MasternodeOwner,
+    pub operator: MasternodeOperator,
+    pub creation: MasternodeCreation,
+    pub resign: Option<MasternodeCreationResign>,
+    pub timelock: i32,
+}
+
 #[derive(Serialize, Deserialize, Debug, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct HistoryItem {
