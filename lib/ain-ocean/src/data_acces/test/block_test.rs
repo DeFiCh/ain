ad474a864692637ea6453f51009cddb5d946800f
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-12-12 13:46:27 +0800

    added block indexing and query method

--- lib/ain-ocean/src/data_acces/test/block_test.rs
+++ lib/ain-ocean/src/data_acces/test/block_test.rs
@@ -111,57 +111,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_query_by_height() {
-        let temp_dir = TempDir::new("test_rocksdb").expect("Failed to create temp directory");
-        let rocksdb = RocksDB::new(temp_dir.path().to_str().unwrap())
-            .expect("Failed to create RocksDB instance");
-        let block_db = BlockDb { db: rocksdb };
-
-        // Create dummy blocks for testing
-        let block1 = create_dummy_block(1);
-        let block2 = create_dummy_block(2);
-        let block3 = create_dummy_block(3);
-
-        // Put the blocks
-        block_db.put_block(block1.clone()).await.unwrap();
-        block_db.put_block(block2.clone()).await.unwrap();
-        block_db.put_block(block3.clone()).await.unwrap();
-
-        // Test query_by_height with limit and lt conditions
-        let result = block_db.query_by_height(2, 3).await.unwrap();
-
-        // Assert the result contains the correct blocks in the correct order
-        assert_eq!(result, vec![block2, block1]);
-    }
-
-    #[tokio::test]
-    async fn test_query_by_height_with_limit_and_lt() {
-        // Create a temporary RocksDB instance for testing
-        let temp_dir = TempDir::new("test_rocksdb").expect("Failed to create temp directory");
-        let rocksdb = RocksDB::new(temp_dir.path().to_str().unwrap())
-            .expect("Failed to create RocksDB instance");
-        let block_db = BlockDb { db: rocksdb };
-
-        // Insert dummy blocks into the "blocks" column family
-        for height in 1..=10 {
-            let dummy_block = create_dummy_block(height);
-            block_db.put_block(dummy_block.clone()).await.unwrap();
-        }
-
-        // Test the query_by_height method
-        let result = block_db.query_by_height(5, 8).await.unwrap();
-
-        // Check if the result has the expected length
-        assert_eq!(result.len(), 5);
-
-        // Check if the blocks are sorted in descending order by height
-        for i in 0..result.len() - 1 {
-            assert!(result[i].height >= result[i + 1].height);
-        }
-    }
-
-    #[tokio::test]
-    async fn test_query_by_height_new_method() {
+    async fn test_query_by_height_descending() {
         // Create a temporary RocksDB instance for testing
         let temp_dir = TempDir::new("test_rocksdb").expect("Failed to create temp directory");
         let rocksdb = RocksDB::new(temp_dir.path().to_str().unwrap())
@@ -169,17 +119,98 @@ mod tests {
         let block_db = BlockDb { db: rocksdb };
 
         // Create dummy blocks with heights 1 to 5 and insert them into the database
-        for height in 1..=5 {
+        for height in 1..=10 {
             let dummy_block = create_dummy_block(height);
             block_db.put_block(dummy_block.clone()).await.unwrap();
         }
 
         // Test the query_by_height method
-        let result_blocks = block_db.query_by_height(3, 4).await.unwrap();
-
+        let result_blocks = block_db
+            .query_by_height(3, 4, crate::data_acces::block::SortOrder::Descending)
+            .await
+            .unwrap();
         // Verify that the result contains the expected blocks
         assert_eq!(result_blocks.len(), 3);
-        assert_eq!(result_blocks[0].height, 4);
-        assert_eq!(result_blocks[1].height, 3);
+        assert_eq!(result_blocks[0].height, 3);
+        assert_eq!(result_blocks[1].height, 2);
+    }
+
+    #[tokio::test]
+    async fn test_query_by_height_ascending() {
+        // Create a temporary RocksDB instance for testing
+        let temp_dir = TempDir::new("test_rocksdb").expect("Failed to create temp directory");
+        let rocksdb = RocksDB::new(temp_dir.path().to_str().unwrap())
+            .expect("Failed to create RocksDB instance");
+        let block_db = BlockDb { db: rocksdb };
+
+        // Create dummy blocks with heights 1 to 5 and insert them into the database
+        for height in 1..=10 {
+            let dummy_block = create_dummy_block(height);
+            block_db.put_block(dummy_block.clone()).await.unwrap();
+        }
+
+        // Test the query_by_height method
+        let result_blocks = block_db
+            .query_by_height(5, 6, crate::data_acces::block::SortOrder::Ascending)
+            .await
+            .unwrap();
+        // Verify that the result contains the expected blocks
+        assert_eq!(result_blocks.len(), 5);
+        assert_eq!(result_blocks[0].height, 1);
+        assert_eq!(result_blocks[1].height, 2);
+    }
+
+    #[tokio::test]
+    async fn test_put_block_updates_latest_height() {
+        // Setup test environment
+        let temp_dir = TempDir::new("test_rocksdb").expect("Failed to create temp directory");
+        let rocksdb = RocksDB::new(temp_dir.path().to_str().unwrap())
+            .expect("Failed to create RocksDB instance");
+        let block_db = BlockDb { db: rocksdb };
+
+        // Create and insert sample blocks
+        for height in 1..=10 {
+            let dummy_block = create_dummy_block(height);
+            block_db.put_block(dummy_block).await.unwrap();
+        }
+
+        // Retrieve latest block height
+        let latest_height_bytes = block_db
+            .db
+            .get("latest_block_height", b"latest_block_height")
+            .expect("Failed to retrieve latest block height")
+            .expect("Latest block height not found");
+        let latest_height = i32::from_be_bytes(
+            latest_height_bytes
+                .as_slice()
+                .try_into()
+                .expect("Byte length mismatch"),
+        );
+
+        // Assert correctness
+        assert_eq!(latest_height, 10); // Assert that latest height is 10
+    }
+
+    #[tokio::test]
+    async fn test_get_highest_retrieves_correct_block() -> Result<(), Box<dyn std::error::Error>> {
+        // Setup test environment
+        let temp_dir = TempDir::new("test_rocksdb").expect("Failed to create temp directory");
+        let rocksdb = RocksDB::new(temp_dir.path().to_str().unwrap())
+            .expect("Failed to create RocksDB instance");
+        let block_db = BlockDb { db: rocksdb };
+
+        // Insert sample blocks
+        for height in 1..=5 {
+            let dummy_block = create_dummy_block(height);
+            block_db.put_block(dummy_block).await.unwrap();
+        }
+
+        // Call get_highest
+        let highest_block = block_db.get_highest().await.unwrap().unwrap();
+
+        // Assert that the highest block is the one with height 5
+        assert_eq!(highest_block.height, 5);
+
+        Ok(())
     }
 }
