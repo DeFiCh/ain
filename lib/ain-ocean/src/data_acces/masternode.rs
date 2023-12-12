4fd25a7e981c0792a031fd4da1885e112ae0cc0d
nagarajm22 <nagaraj.manjunath@cakedefi.com>
2023-12-12 21:59:18 +0800

    added query methods and extra table

--- lib/ain-ocean/src/data_acces/masternode.rs
+++ lib/ain-ocean/src/data_acces/masternode.rs
@@ -1,5 +1,8 @@
+use crate::database::db_manger::ColumnFamilyOperations;
+use crate::database::db_manger::{RocksDB, SortOrder};
+use crate::model::masternode::Masternode;
 use anyhow::{anyhow, Result};
-use serde::{Deserialize, Serialize};
+use rocksdb::IteratorMode;
 use serde_json;
 
 use crate::{
@@ -12,8 +15,39 @@ pub struct MasterNodeDB {
 }
 
 impl MasterNodeDB {
-    pub async fn query(&self, limit: i32, lt: i32) -> Result<Vec<Masternode>> {
-        todo!()
+    pub async fn query(
+        &self,
+        limit: i32,
+        lt: i32,
+        sort_order: SortOrder,
+    ) -> Result<Vec<Masternode>> {
+        let iterator = self.db.iterator("masternode", IteratorMode::End)?;
+        let mut master_node: Vec<Masternode> = Vec::new();
+        let collected_blocks: Vec<_> = iterator.collect();
+
+        for result in collected_blocks.into_iter().rev() {
+            let (key, value) = match result {
+                Ok((key, value)) => (key, value),
+                Err(err) => return Err(anyhow!("Error during iteration: {}", err)),
+            };
+
+            let master_stats: Masternode = serde_json::from_slice(&value)?;
+            master_node.push(master_stats);
+
+            if master_node.len() == limit as usize {
+                break;
+            }
+        }
+
+        // Sort blocks based on the specified sort order
+        match sort_order {
+            SortOrder::Ascending => master_node.sort_by(|a, b| a.block.height.cmp(&b.block.height)),
+            SortOrder::Descending => {
+                master_node.sort_by(|a, b| b.block.height.cmp(&a.block.height))
+            }
+        }
+
+        Ok(master_node)
     }
     pub async fn get(&self, id: String) -> Result<Option<Masternode>> {
         match self.db.get("masternode", id.as_bytes()) {
--- lib/ain-ocean/src/data_acces/oracle_price_aggregated_interval.rs
+++ lib/ain-ocean/src/data_acces/oracle_price_aggregated_interval.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use rocksdb::{ColumnFamilyDescriptor, IteratorMode, DB};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -17,7 +18,31 @@ impl OraclePriceAggregatedIntervalDb {
         limit: i32,
         lt: String,
     ) -> Result<Vec<OraclePriceAggregatedInterval>> {
-        todo!()
+        let iterator = self
+            .db
+            .iterator("oracle_price_aggregated_interval", IteratorMode::End)?;
+        let mut oracle_prices: Vec<OraclePriceAggregatedInterval> = Vec::new();
+        let collected_items: Vec<_> = iterator.collect();
+
+        for result in collected_items.into_iter().rev() {
+            let (key, value) = match result {
+                Ok((key, value)) => (key, value),
+                Err(err) => return Err(anyhow!("Error during iteration: {}", err)),
+            };
+
+            let oracle_price: OraclePriceAggregatedInterval = serde_json::from_slice(&value)?;
+
+            // Check if the id is less than 'lt'
+            if String::from_utf8(key.to_vec())? < lt {
+                oracle_prices.push(oracle_price);
+
+                if oracle_prices.len() == limit as usize {
+                    break;
+                }
+            }
+        }
+
+        Ok(oracle_prices)
     }
     pub async fn put(&self, oracle: OraclePriceAggregatedInterval) -> Result<()> {
         match serde_json::to_string(&oracle) {
