d4e841d71a4a7a80e67b282938b872f327578b3e
Bushstar <bushsolo@gmail.com>
2023-12-13 10:01:15 +0000

    lint: set correct include names

--- src/util/getuniquepath.h
+++ src/util/getuniquepath.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_UTIL_GETUNIQUEPATH_H
-#define BITCOIN_UTIL_GETUNIQUEPATH_H
+#ifndef DEFI_UTIL_GETUNIQUEPATH_H
+#define DEFI_UTIL_GETUNIQUEPATH_H
 
 #include <fs.h>
 
@@ -16,5 +16,5 @@
  */
 fs::path GetUniquePath(const fs::path& base);
 
-#endif // BITCOIN_UTIL_GETUNIQUEPATH_H
+#endif // DEFI_UTIL_GETUNIQUEPATH_H
  
