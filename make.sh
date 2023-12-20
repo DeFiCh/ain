0cbeb08b9293d7c7ce8aae00b079301d06c8ee3c
Niven <sieniven@gmail.com>
2023-12-20 13:27:18 +0800

    Remove setting cargo bin dir

--- make.sh
+++ make.sh
@@ -1165,7 +1165,6 @@ ci_export_vars() {
     if [[ -n "${GITHUB_ACTIONS-}" ]]; then
         # GitHub Actions
         echo "BUILD_VERSION=${IMAGE_VERSION}" >> "$GITHUB_ENV"
-        echo "PATH=$HOME/.cargo/bin:$PATH" >> "$GITHUB_ENV"
         echo "CARGO_INCREMENTAL=0" >> "$GITHUB_ENV"
 
         if [[ "${MAKE_DEBUG}" == "1" ]]; then
