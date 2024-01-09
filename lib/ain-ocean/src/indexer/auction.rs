70e9e7a4d1810ee9aa84d49e4acf0d87d1ee452d
Jouzo <15011228+Jouzo@users.noreply.github.com>
2024-01-09 10:00:01 +0100

    Index auction (#2771)

--- lib/ain-ocean/src/indexer/auction.rs
+++ lib/ain-ocean/src/indexer/auction.rs
@@ -1,14 +1,53 @@
 use dftx_rs::{vault::PlaceAuctionBid, Transaction};
+use log::debug;
 
 use super::BlockContext;
-use crate::indexer::{Index, Result};
+use crate::{
+    indexer::{Index, Result},
+    model::{VaultAuctionBatchHistory, VaultAuctionBatchHistoryBlock},
+    repository::RepositoryOps,
+    SERVICES,
+};
 
 impl Index for PlaceAuctionBid {
     fn index(&self, ctx: &BlockContext, tx: Transaction, idx: usize) -> Result<()> {
-        todo!()
+        debug!("[PlaceAuctionBid] Indexing...");
+
+        let auction = VaultAuctionBatchHistory {
+            id: format!("{}-{}-{}", self.vault_id, self.index, tx.txid()),
+            key: format!("{}-{}", self.vault_id, self.index),
+            sort: format!("{}-{}", ctx.height, idx),
+            vault_id: self.vault_id,
+            index: idx,
+            from: self.from.clone(),
+            amount: self.token_amount.amount,
+            token_id: self.token_amount.token.0,
+            block: VaultAuctionBatchHistoryBlock {
+                hash: ctx.hash,
+                height: ctx.height,
+                time: ctx.time,
+                median_time: ctx.median_time,
+            },
+        };
+        debug!("auction : {:?}", auction);
+
+        let key = (self.vault_id, self.index, tx.txid());
+        SERVICES.auction.by_id.put(&key, &auction)?;
+        SERVICES
+            .auction
+            .by_height
+            .put(&(self.vault_id, self.index, ctx.height, idx), &key)
     }
 
-    fn invalidate(&self, context: &BlockContext, tx: Transaction, idx: usize) -> Result<()> {
-        todo!()
+    fn invalidate(&self, ctx: &BlockContext, tx: Transaction, idx: usize) -> Result<()> {
+        debug!("[PlaceAuctionBid] Invalidating...");
+        SERVICES
+            .auction
+            .by_id
+            .delete(&(self.vault_id, self.index, tx.txid()))?;
+        SERVICES
+            .auction
+            .by_height
+            .delete(&(self.vault_id, self.index, ctx.height, idx))
     }
 }
