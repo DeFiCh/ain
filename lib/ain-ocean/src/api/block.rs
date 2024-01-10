0c0d93bb7b882ec2e60ed1766af9b62f763996e9
canonbrother <w.canonbrother@gmail.com>
2024-01-11 06:25:45 +0800

    update ocean block apis

--- lib/ain-ocean/src/api/block.rs
+++ lib/ain-ocean/src/api/block.rs
@@ -3,40 +3,61 @@ use axum::{
     routing::get,
     Json, Router,
 };
+use bitcoin::BlockHash;
+use log::debug;
 use serde::{Deserialize, Serialize};
 
-use crate::{api_paged_response::ApiPagedResponse, api_query::PaginationQuery, error::OceanResult};
+use crate::{
+    api_paged_response::ApiPagedResponse,
+    api_query::PaginationQuery,
+    error::OceanResult,
+    SERVICES,
+    repository::RepositoryOps,
+    model::Block,
+};
 
 #[derive(Deserialize)]
 struct BlockId {
     id: String,
 }
 
-#[derive(Deserialize)]
-struct BlockHash {
-    hash: String,
-}
-
 async fn list_blocks(
     Query(query): Query<PaginationQuery>,
 ) -> OceanResult<Json<ApiPagedResponse<Block>>> {
-    // TODO(): query from lvldb.. or maybe pull from index
-    let blocks = vec![
-        Block { id: "0".into() },
-        Block { id: "1".into() },
-        Block { id: "2".into() },
-    ];
+    let blocks = SERVICES
+        .block
+        .by_height
+        .list(None)?
+        .take(query.size)
+        .map(|item| {
+            let (_, id) = item?;
+            let b = SERVICES
+                .block
+                .by_id
+                .get(&id)?
+                .ok_or("Missing block index")?;
 
-    Ok(Json(ApiPagedResponse::of(blocks, query.size, |block| {
-        block.clone().id
-    })))
+            Ok(b)
+        })
+        .collect::<OceanResult<Vec<_>>>()?;
+
+    Ok(Json(ApiPagedResponse::of(
+        blocks,
+        query.size,
+        |block| block.clone().id,
+    )))
 }
 
-async fn get_block(Path(BlockId { id }): Path<BlockId>) -> OceanResult<Json<Block>> {
-    Ok(Json(Block { id }))
+async fn get_block(Path(id): Path<BlockHash>) -> OceanResult<Json<Option<Block>>> {
+    let block = SERVICES
+        .block
+        .by_id
+        .get(&id)?;
+
+    Ok(Json(block))
 }
 
-async fn get_transactions(Path(BlockHash { hash }): Path<BlockHash>) -> String {
+async fn get_transactions(Path(hash): Path<BlockHash>) -> String {
     format!("Transactions for block with hash {}", hash)
 }
 
@@ -45,34 +66,4 @@ pub fn router() -> Router {
         .route("/", get(list_blocks))
         .route("/:id", get(get_block))
         .route("/:hash/transactions", get(get_transactions))
-}
-
-#[derive(Clone, Debug, Serialize)]
-#[serde(default)]
-pub struct Block {
-    id: String,
-    // TODO(): type mapping
-    // hash: H256,
-    // previous_hash: H256,
-
-    // height: u64,
-    // version: u64,
-    // time: u64, // ---------------| block time in seconds since epoch
-    // median_time: u64, // --------| meidan time of the past 11 block timestamps
-
-    // transaction_count: u64,
-
-    // difficulty: u64,
-
-    // masternode: H256,
-    // minter: H256,
-    // minter_block_count: u64,
-    // reward: f64
-
-    // state_modifier: H256,
-    // merkle_root: H256,
-
-    // size: u64,
-    // size_stripped: u64,
-    // weight: u64,
 }
