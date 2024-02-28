3c3bf7563625974eb905603e20a2be0d62daa11a
canonbrother <w.canonbrother@gmail.com>
2024-02-28 11:10:40 +0800

    enable api poolpairs list

--- lib/ain-ocean/src/api/mod.rs
+++ lib/ain-ocean/src/api/mod.rs
@@ -9,8 +9,8 @@ mod governance;
 mod loan;
 mod masternode;
 mod oracle;
-// mod poolpairs;
 pub mod prices;
+mod poolpairs;
 // mod rawtx;
 mod cache;
 pub mod common;
@@ -75,7 +75,7 @@ pub async fn ocean_router(
             .nest("/fee", fee::router(Arc::clone(&context)))
             .nest("/masternodes", masternode::router(Arc::clone(&context)))
             .nest("/oracles", oracle::router(Arc::clone(&context)))
-            // .nest("/poolpairs", poolpairs::router(Arc::clone(&context)))
+            .nest("/poolpairs", poolpairs::router(Arc::clone(&context)))
             // .nest("/prices", prices::router(Arc::clone(&context)))
             // .nest("/rawtx", rawtx::router(Arc::clone(&context)))
             .nest("/stats", stats::router(Arc::clone(&context)))
