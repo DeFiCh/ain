a76ad2467137ff47e8629e8bfeb0768ba446310f
Niven <sieniven@gmail.com>
2024-03-04 18:29:20 +0800

    Fix naming

--- test/functional/feature_evm_rpc_tracer.py
+++ test/functional/feature_evm_rpc_tracer.py
@@ -3,7 +3,7 @@
 # Copyright (c) DeFi Blockchain Developers
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
-"""Test eth_createAccessList RPC behaviour"""
+"""Test eth tracer RPC behaviour"""
 
 from test_framework.test_framework import DefiTestFramework
 from test_framework.evm_contract import EVMContract
