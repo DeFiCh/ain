ac4ab68495053283d32cb94dcebb1da8ae0ca2f7
Bushstar <bushsolo@gmail.com>
2024-03-07 07:23:06 +0000

    Remove unused interface

--- lib/ain-contracts/dfi_intrinsics_v2/DFIIntrinsicsV2.sol
+++ lib/ain-contracts/dfi_intrinsics_v2/DFIIntrinsicsV2.sol
@@ -7,14 +7,6 @@ interface IDFIIntrinsicsRegistry {
     function get(uint256 _version) external view returns (address);
 }
 
-interface IERC20 {
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-}
-
 contract DFIIntrinsicsV2 is IDFIIntrinsicsV2 {
     uint256 private _version;
     address private registryAddress;
@@ -41,7 +33,6 @@ contract DFIIntrinsicsV2 is IDFIIntrinsicsV2 {
     }
 
     function depositAndSplitTokens(address tokenContract, uint256 amount) public {
-        // require(IERC20(tokenContract).transferFrom(msg.sender, address(this), amount), "Transfer failed");
         splitTokens(msg.sender, tokenContract, amount);
     }
 
