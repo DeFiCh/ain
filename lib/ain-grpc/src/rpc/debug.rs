bf6428674a9b17e6e928edf95ba204aab10537c9
Niven <sieniven@gmail.com>
2024-04-04 17:55:55 +0800

    Fix merge

--- lib/ain-grpc/src/rpc/debug.rs
+++ lib/ain-grpc/src/rpc/debug.rs
@@ -162,14 +162,10 @@ impl MetachainDebugRPCServer for MetachainDebugRPCModule {
 
         // Handle trace params
         let params = handle_trace_params(trace_params)?;
-        match params.1 {
-            TraceType::Raw { .. } => (),
-            TraceType::CallList => (),
-        }
+        let trace_block = self.get_block(Some(block_number))?;
         let raw_max_memory_usage =
             usize::try_from(ain_cpp_imports::get_tracing_raw_max_memory_usage_bytes())
                 .map_err(|_| to_custom_err("failed to convert response size limit to usize"))?;
-        let trace_block = self.get_block(Some(block_number))?;
 
         Ok(self
             .handler
@@ -187,21 +183,15 @@ impl MetachainDebugRPCServer for MetachainDebugRPCModule {
 
         // Handle trace params
         let params = handle_trace_params(trace_params)?;
-        match params.1 {
-            TraceType::Raw { .. } => (),
-            TraceType::CallList => (),
-            not_supported => return Err(RPCError::TraceTypeError(not_supported).into()),
-        }
-        let raw_max_memory_usage =
-            usize::try_from(ain_cpp_imports::get_tracing_raw_max_memory_usage_bytes())
-                .map_err(|_| to_custom_err("failed to convert response size limit to usize"))?;
-
         let trace_block = self
             .handler
             .storage
             .get_block_by_hash(&hash)
             .map_err(to_custom_err)?
             .ok_or(RPCError::BlockNotFound)?;
+        let raw_max_memory_usage =
+            usize::try_from(ain_cpp_imports::get_tracing_raw_max_memory_usage_bytes())
+                .map_err(|_| to_custom_err("failed to convert response size limit to usize"))?;
 
         Ok(self
             .handler
