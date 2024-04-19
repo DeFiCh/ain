b5a3a639ba20e024afcb5872738300e808a9cf33
canonbrother <w.canonbrother@gmail.com>
2024-04-19 21:28:02 +0800

    rm extra api

--- lib/ain-ocean/src/api/poolpairs.rs
+++ lib/ain-ocean/src/api/poolpairs.rs
@@ -529,7 +529,6 @@ pub fn router(ctx: Arc<AppContext>) -> Router {
         //     "/paths/best/from/:fromTokenId/to/:toTokenId",
         //     get(get_best_path),
         // )
-        // .route("/paths/from/:fromTokenId/to/:toTokenId", get(get_all_paths))
         // .route("/dexprices", get(list_dex_prices))
         .layer(Extension(ctx))
 }
