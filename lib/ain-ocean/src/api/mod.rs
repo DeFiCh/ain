b881751c3d2d23849b4b6dbadab935452d328488
canonbrother <w.canonbrother@gmail.com>
2024-06-01 15:31:06 +0800

    rm unuse

--- lib/ain-ocean/src/api/mod.rs
+++ lib/ain-ocean/src/api/mod.rs
@@ -80,8 +80,6 @@ pub async fn ocean_router(
             .nest("/masternodes", masternode::router(Arc::clone(&context)))
             .nest("/oracles", oracle::router(Arc::clone(&context)))
             .nest("/poolpairs", pool_pair::router(Arc::clone(&context)))
-            // .nest("/prices", prices::router(Arc::clone(&context)))
-            // .nest("/poolpairs", poolpairs::router(Arc::clone(&context)))
             .nest("/prices", prices::router(Arc::clone(&context)))
             // .nest("/rawtx", rawtx::router(Arc::clone(&context)))
             .nest("/stats", stats::router(Arc::clone(&context)))
