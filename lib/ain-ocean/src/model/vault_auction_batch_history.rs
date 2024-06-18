64e1752d8abac5b13f0ceb090bc4df4c7094e198
canonbrother <w.canonbrother@gmail.com>
2024-06-19 00:38:02 +0800

    desc

--- lib/ain-ocean/src/model/vault_auction_batch_history.rs
+++ lib/ain-ocean/src/model/vault_auction_batch_history.rs
@@ -3,8 +3,8 @@ use serde::{Deserialize, Serialize};
 
 use super::BlockContext;
 
-pub type AuctionHistoryKey = (Txid, u32, Txid);
-pub type AuctionHistoryByHeightKey = (Txid, u32, u32, usize);
+pub type AuctionHistoryKey = (Txid, u32, Txid); // (vault_id, auction_batch_index, txid)
+pub type AuctionHistoryByHeightKey = (Txid, u32, u32, usize); // (vault_id, auction_batch_index, block_height, txid)
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
