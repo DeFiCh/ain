ad9e0aaff5a9c795b7bd806e30f53be7397b20b3
canonbrother <w.canonbrother@gmail.com>
2024-06-25 22:15:38 +0800

    fmt

--- lib/ain-ocean/src/indexer/helper.rs
+++ lib/ain-ocean/src/indexer/helper.rs
@@ -2,15 +2,15 @@ use bitcoin::{hashes::Hash, Txid};
 use defichain_rpc::json::blockchain::{Transaction, Vin};
 
 pub fn check_if_evm_tx(txn: &Transaction) -> bool {
-  txn.vin.len() == 2
-      && txn.vin.iter().all(|vin| match vin {
-          Vin::Coinbase(_) => true,
-          Vin::Standard(tx) => tx.txid == Txid::all_zeros(),
-      })
-      && txn.vout.len() == 1
-      && txn.vout[0]
-          .script_pub_key
-          .asm
-          .starts_with("OP_RETURN 4466547839")
-      && txn.vout[0].value == 0f64
+    txn.vin.len() == 2
+        && txn.vin.iter().all(|vin| match vin {
+            Vin::Coinbase(_) => true,
+            Vin::Standard(tx) => tx.txid == Txid::all_zeros(),
+        })
+        && txn.vout.len() == 1
+        && txn.vout[0]
+            .script_pub_key
+            .asm
+            .starts_with("OP_RETURN 4466547839")
+        && txn.vout[0].value == 0f64
 }
