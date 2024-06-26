e46cc6581b671df9b691be2495d53c4ea0b992a8
canonbrother <w.canonbrother@gmail.com>
2024-06-26 23:19:29 +0800

    address api: get_balance + get_aggregation

--- lib/ain-ocean/src/api/address.rs
+++ lib/ain-ocean/src/api/address.rs
@@ -4,18 +4,20 @@ use super::{
     common::address_to_hid,
     path::Path,
     query::{PaginationQuery, Query},
-    response::ApiPagedResponse,
+    response::{ApiPagedResponse, Response},
     AppContext,
 };
 use crate::{
     error::ApiError,
-    model::{ScriptActivity, ScriptUnspent},
+    model::{ScriptActivity, ScriptAggregation, ScriptUnspent},
     repository::{RepositoryOps, SecondaryIndex},
     storage::SortOrder,
     Result,
 };
 use ain_macros::ocean_endpoint;
 use axum::{routing::get, Extension, Router};
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -47,13 +49,50 @@ struct Address {
 //     format!("List account history for address {}", address)
 // }
 
-// async fn get_balance(Path(Address { address }): Path<Address>) -> String {
-//     format!("balance for address {address}")
-// }
+fn get_latest_aggregation(ctx: &Arc<AppContext>, hid: String) -> Result<Option<ScriptAggregation>> {
+    let latest = ctx
+        .services
+        .script_aggregation
+        .by_id
+        .list(Some((u32::MAX, hid.clone())), SortOrder::Descending)?
+        .take(1)
+        .take_while(|item| match item {
+            Ok(((_, v), _)) => v == &hid,
+            _ => true,
+        })
+        .map(|item| {
+            let (_, v) = item?;
+            Ok(v)
+        })
+        .collect::<Result<Vec<_>>>()?;
 
-// async fn get_aggregation(Path(Address { address }): Path<Address>) -> String {
-//     format!("Aggregation for address {}", address)
-// }
+    Ok(latest.first().cloned())
+
+}
+
+#[ocean_endpoint]
+async fn get_balance(
+    Path(Address { address }): Path<Address>,
+    Extension(ctx): Extension<Arc<AppContext>>,
+) -> Result<Response<Decimal>> {
+    let hid = address_to_hid(&address, ctx.network.into())?;
+    let aggregation = get_latest_aggregation(&ctx, hid)?;
+    if aggregation.is_none() {
+        return Ok(Response::new(dec!(0)))
+    }
+    let aggregation = aggregation.unwrap();
+    Ok(Response::new(aggregation.amount.unspent))
+}
+
+#[ocean_endpoint]
+async fn get_aggregation(
+    Path(Address { address }): Path<Address>,
+    Extension(ctx): Extension<Arc<AppContext>>,
+) -> Result<Response<Option<ScriptAggregation>>> {
+    let hid = address_to_hid(&address, ctx.network.into())?;
+    let aggregation = get_latest_aggregation(&ctx, hid)?;
+    Ok(Response::new(aggregation))
+}
 
 // async fn list_token(Path(Address { address }): Path<Address>) -> String {
 //     format!("List tokens for address {}", address)
@@ -118,8 +157,8 @@ pub fn router(ctx: Arc<AppContext>) -> Router {
     Router::new()
         // .route("/history/:height/:txno", get(get_account_history))
         // .route("/history", get(list_account_history))
-        // .route("/balance", get(get_balance))
-        // .route("/aggregation", get(get_aggregation))
+        .route("/:address/balance", get(get_balance))
+        .route("/:address/aggregation", get(get_aggregation))
         // .route("/tokens", get(list_token))
         // .route("/vaults", get(list_vault))
         .route("/:address/transactions", get(list_transaction))
