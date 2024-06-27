b84fa0ee301eaa47e77f6b85abf28f9996b37301
Prasanna Loganathar <pvl@prasannavl.com>
2024-06-28 00:16:32 +0800

    fmt

--- test/functional/test_framework/tokenamount.py
+++ test/functional/test_framework/tokenamount.py
@@ -3,10 +3,14 @@ from decimal import Decimal, InvalidOperation
 
 
 class TokenAmount:
-    def __init__(self, number_with_symbol: str = None, number: Decimal = None, symbol: str = None):
+    def __init__(
+        self, number_with_symbol: str = None, number: Decimal = None, symbol: str = None
+    ):
         if number_with_symbol:
             if not self._validate(number_with_symbol):
-                raise ValueError("String must be in the format NumberWith8DigitPrecision@SYMBOL")
+                raise ValueError(
+                    "String must be in the format NumberWith8DigitPrecision@SYMBOL"
+                )
             self.number_with_symbol = number_with_symbol
         elif number is not None and symbol:
             try:
@@ -14,13 +18,17 @@ class TokenAmount:
             except InvalidOperation:
                 raise ValueError("Number must be convertible to Decimal")
             if not self._validate_number(number):
-                raise ValueError("Number must have up to 8 digit precision after the decimal point")
+                raise ValueError(
+                    "Number must have up to 8 digit precision after the decimal point"
+                )
             self.number_with_symbol = f"{number:.8f}@{symbol}"
         else:
-            raise ValueError("Invalid arguments. Provide either a single string or number and symbol.")
+            raise ValueError(
+                "Invalid arguments. Provide either a single string or number and symbol."
+            )
 
     def _validate(self, number_with_symbol: str) -> bool:
-        pattern = r'^\d{1,32}(\.\d{1,8})?@\w+$'
+        pattern = r"^\d{1,32}(\.\d{1,8})?@\w+$"
         return bool(re.match(pattern, number_with_symbol))
 
     def _validate_number(self, number: Decimal) -> bool:
@@ -33,11 +41,11 @@ class TokenAmount:
         return f"TokenAmount({self.number_with_symbol!r})"
 
     def get_number(self) -> Decimal:
-        number_str = self.number_with_symbol.split('@')[0]
+        number_str = self.number_with_symbol.split("@")[0]
         return Decimal(number_str)
 
     def get_symbol(self) -> str:
-        return self.number_with_symbol.split('@')[1]
+        return self.number_with_symbol.split("@")[1]
 
     def _new_with_number(self, new_number: Decimal):
         return TokenAmount(f"{new_number:.8f}@{self.get_symbol()}")
@@ -62,7 +70,7 @@ class TokenAmount:
             new_number = self.get_number() - Decimal(other)
         else:
             return NotImplemented
-        return  self._new_with_number(new_number)
+        return self._new_with_number(new_number)
 
     def __mul__(self, other):
         if isinstance(other, TokenAmount):
