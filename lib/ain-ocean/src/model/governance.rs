b7879e717a42b2069ad0c7ea9230d2c5c3199a83
canonbrother <w.canonbrother@gmail.com>
2024-07-07 23:17:42 +0800

    fix ser model

--- lib/ain-ocean/src/model/governance.rs
+++ lib/ain-ocean/src/model/governance.rs
@@ -1,9 +1,8 @@
 use defichain_rpc::json::governance::{ProposalInfo, ProposalStatus, ProposalType};
-use serde::{Deserialize, Serialize, Serializer};
+use serde::Serialize;
 use serde_with::skip_serializing_none;
 
-#[skip_serializing_none]
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ApiProposalInfo {
     pub proposal_id: String,
@@ -12,20 +11,32 @@ pub struct ApiProposalInfo {
     pub context_hash: String,
     pub r#type: ProposalType,
     pub status: ProposalStatus,
-    #[serde(
-        serialize_with = "serialize_amount",
-        skip_serializing_if = "should_skip"
-    )]
-    pub amount: Option<String>,
     pub current_cycle: u64,
     pub total_cycles: u64,
     pub creation_height: u64,
     pub cycle_end_height: u64,
     pub proposal_end_height: u64,
-    pub payout_address: Option<String>,
     pub voting_period: u64,
     pub approval_threshold: String,
     pub quorum: String,
+    #[serde(flatten)]
+    pub confidence_vote: ApiProposalConfidenceVote,
+    #[serde(flatten)]
+    pub vote_info: ApiProposalVoteInfo,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ApiProposalConfidenceVote {
+    pub amount: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub payout_address: Option<String>,
+}
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ApiProposalVoteInfo {
     pub votes_possible: Option<u64>,
     pub votes_present: Option<u64>,
     pub votes_present_pct: Option<String>,
@@ -49,41 +60,32 @@ impl From<ProposalInfo> for ApiProposalInfo {
             context_hash: proposal.context_hash,
             r#type: proposal.r#type,
             status: proposal.status,
-            amount: proposal.amount.map(|a| format!("{:.8}", a)),
             current_cycle: proposal.current_cycle,
             total_cycles: proposal.total_cycles,
             creation_height: proposal.creation_height,
             cycle_end_height: proposal.cycle_end_height,
             proposal_end_height: proposal.proposal_end_height,
-            payout_address: proposal.payout_address,
             voting_period: proposal.voting_period,
             approval_threshold: proposal.approval_threshold,
             quorum: proposal.quorum,
-            votes_possible: proposal.votes_possible,
-            votes_present: proposal.votes_present,
-            votes_present_pct: proposal.votes_present_pct,
-            votes_yes: proposal.votes_yes,
-            votes_invalid: proposal.votes_invalid,
-            votes_neutral: proposal.votes_neutral,
-            votes_no: proposal.votes_no,
-            votes_yes_pct: proposal.votes_yes_pct,
-            fee: proposal.fee,
-            options: proposal.options,
-            fee_redistribution_per_vote: proposal.fee_redistribution_per_vote,
-            fee_redistribution_total: proposal.fee_redistribution_total,
+            confidence_vote: ApiProposalConfidenceVote {
+                amount: proposal.amount.map(|a| format!("{:.8}", a)),
+                payout_address: proposal.payout_address,
+            },
+            vote_info: ApiProposalVoteInfo {
+                votes_possible: proposal.votes_possible,
+                votes_present: proposal.votes_present,
+                votes_present_pct: proposal.votes_present_pct,
+                votes_yes: proposal.votes_yes,
+                votes_invalid: proposal.votes_invalid,
+                votes_neutral: proposal.votes_neutral,
+                votes_no: proposal.votes_no,
+                votes_yes_pct: proposal.votes_yes_pct,
+                fee: proposal.fee,
+                options: proposal.options,
+                fee_redistribution_per_vote: proposal.fee_redistribution_per_vote,
+                fee_redistribution_total: proposal.fee_redistribution_total,
+            },
         }
     }
-}
-fn serialize_amount<S>(amount: &Option<String>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    match amount {
-        Some(value) => serializer.serialize_some(value),
-        None => serializer.serialize_none(),
-    }
-}
-
-fn should_skip<T>(_option: &Option<T>) -> bool {
-    false
 }
