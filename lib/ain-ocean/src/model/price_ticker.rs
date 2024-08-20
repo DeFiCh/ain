17ffe898471075703c3d8d06441b7c8b63408fe0
canonbrother <w.canonbrother@gmail.com>
2024-08-20 16:19:23 +0800

    Ocean: CollateralToken.active_price + typing amount: decimal (#2990)
    * add active_price on CollateralToken
    
    * fix serde
    
    * fix type
    
    * fmt_rs

--- lib/ain-ocean/src/model/price_ticker.rs
+++ lib/ain-ocean/src/model/price_ticker.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use super::{oracle_price_aggregated::OraclePriceAggregated, OraclePriceAggregatedApi};
+use super::oracle_price_aggregated::OraclePriceAggregated;
 
 pub type PriceTickerId = (String, String); //token-currency
 pub type PriceTickerKey = (i32, u32, String, String); // total-height-token-currency
@@ -11,12 +11,4 @@ pub struct PriceTicker {
     pub id: PriceTickerId, //token-currency
     pub sort: String,      //count-height-token-currency
     pub price: OraclePriceAggregated,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct PriceTickerApi {
-    pub id: String,   //token-currency
-    pub sort: String, //count-height-token-currency
-    pub price: OraclePriceAggregatedApi,
 }
