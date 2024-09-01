7ead87fbe793c2883e7082885343c969adce03c3
Bushstar <bushsolo@gmail.com>
2024-09-01 10:34:10 +0100

    lint: format CPP

--- src/miner.cpp
+++ src/miner.cpp
@@ -447,22 +447,22 @@ ResVal<std::unique_ptr<CBlockTemplate>> BlockAssembler::CreateNewBlock(const CSc
         };
 
         attributes->ForEach(
-        [&](const CDataStructureV0 &attr, const CAttributeValue &) {
-            if (attr.type != AttributeTypes::Token) {
-                return false;
-            }
-            if (attr.key == TokenKeys::LoanCollateralEnabled) {
-                if (auto collateralToken = mnview.GetCollateralTokenFromAttributes({attr.typeId})) {
-                    return checkLivePrice(collateralToken->fixedIntervalPriceId);
+            [&](const CDataStructureV0 &attr, const CAttributeValue &) {
+                if (attr.type != AttributeTypes::Token) {
+                    return false;
                 }
-            } else if (attr.key == TokenKeys::LoanMintingEnabled) {
-                if (auto loanToken = mnview.GetLoanTokenFromAttributes({attr.typeId})) {
-                    return checkLivePrice(loanToken->fixedIntervalPriceId);
+                if (attr.key == TokenKeys::LoanCollateralEnabled) {
+                    if (auto collateralToken = mnview.GetCollateralTokenFromAttributes({attr.typeId})) {
+                        return checkLivePrice(collateralToken->fixedIntervalPriceId);
+                    }
+                } else if (attr.key == TokenKeys::LoanMintingEnabled) {
+                    if (auto loanToken = mnview.GetLoanTokenFromAttributes({attr.typeId})) {
+                        return checkLivePrice(loanToken->fixedIntervalPriceId);
+                    }
                 }
-            }
-            return true;
-        },
-        CDataStructureV0{AttributeTypes::Token});
+                return true;
+            },
+            CDataStructureV0{AttributeTypes::Token});
 
         if (lockedTokens.balances.empty() && lockRatio && tokenPricesValid) {
             SplitMap lockSplitMapEVM;
