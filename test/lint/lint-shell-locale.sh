a0c084445ab66036f4f0e9bfd695b61fa2d3f18c
Prasanna Loganathar <pvl@prasannavl.com>
2024-09-03 14:12:09 +0800

    cleanup

--- test/lint/lint-shell-locale.sh
+++ test/lint/lint-shell-locale.sh
@@ -12,26 +12,30 @@
 export LC_ALL=C
 
 EXIT_CODE=0
+LINES_TO_CHECK=5
 
 for SHELL_SCRIPT in $(git ls-files -- "*.sh" | grep -vE "src/(secp256k1|univalue)/"); do
     CURRENT_LINE_NO=0
     while IFS= read -r line; do
-        if [[ ${line} == "# This script is intentionally locale dependent by not setting \"export LC_ALL=C\"" ]]; then
-            continue 2
-        fi
-        if [[ ${line} =~ ^(#.*)?$ ]]; then
-            continue
-        fi
         if [[ ${line} == "export LC_ALL=C" ]] ||
             [[ ${line} == "export LC_ALL=C.UTF-8" ]] ||
             [[ ${line} == "export LC_ALL=en_US.UTF-8" ]]; then
             continue 2
         fi
+
+        if [[ ${line} == "# This script is intentionally locale dependent by not setting \"export LC_ALL=C\"" ]]; then
+            continue 2
+        fi
+
+        if [[ ${line} =~ ^(#.*)?$ ]]; then
+            continue
+        fi
+
+        if [[ ${CURRENT_LINE_NO} > ${LINES_TO_CHECK} ]]; then break; fi
         ((CURRENT_LINE_NO++))
-        if [[ ${CURRENT_LINE_NO} > 10 ]]; then break; fi
     done <"$SHELL_SCRIPT"
 
-    echo "Missing \"export LC_ALL=<C|C.UTF-8|en_US.UTF-8>\" (to avoid locale dependence) within first 10 non-empty lines in ${SHELL_SCRIPT}"
+    echo "Missing \"export LC_ALL=<C|C.UTF-8|en_US.UTF-8>\" (to avoid locale dependence) within first ${LINES_TO_CHECK} non-empty lines in ${SHELL_SCRIPT}"
     EXIT_CODE=1
 done
 exit ${EXIT_CODE}
