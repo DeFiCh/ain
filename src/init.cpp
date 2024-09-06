abab045354b16c8e8b5e5e6cf3639eae22d92903
Prasanna Loganathar <pvl@prasannavl.com>
2024-09-06 21:01:41 +0800

    Minor arg doc tweaks

--- src/init.cpp
+++ src/init.cpp
@@ -682,8 +682,8 @@ void SetupServerArgs()
 
 
 #if HAVE_DECL_FORK
-    gArgs.AddArg("-daemon", strprintf("Wait for initialization to be finished before exiting. This implies -daemonnowait (default: %d)", DEFAULT_DAEMON), ArgsManager::ALLOW_BOOL, OptionsCategory::OPTIONS);
-    gArgs.AddArg("-daemonnowait", strprintf("Run in the background as a daemon and accept commands (default: %d)", DEFAULT_DAEMONNOWAIT), ArgsManager::ALLOW_BOOL, OptionsCategory::OPTIONS);
+    gArgs.AddArg("-daemon", strprintf("Run in the background as a daemon and accept commands. Will wait until init is complete before returning. (default: %d)", DEFAULT_DAEMON), ArgsManager::ALLOW_BOOL, OptionsCategory::OPTIONS);
+    gArgs.AddArg("-daemonnowait", strprintf("Same as daemon but doesn't wait for init and returns immediately after forking (default: %d)", DEFAULT_DAEMONNOWAIT), ArgsManager::ALLOW_BOOL, OptionsCategory::OPTIONS);
 #else
     hidden_args.emplace_back("-daemon");
     hidden_args.emplace_back("-daemonnowait");
