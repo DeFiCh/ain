32a84845fcd131c25d692891d6277a5669257dd1
jouzo <15011228+Jouzo@users.noreply.github.com>
2024-10-15 16:30:32 +0100

    Remove dead code

--- lib/labs-grpc2/src/proto.rs
+++
@@ -1,7 +0,0 @@
-pub mod eth {
-    include!(concat!(env!("OUT_DIR"), "/eth.rs"));
-    // include!(concat!(env!("CARGO_MANIFEST_DIR"), "/gen", "/eth.rs"));
-}
-
-// macro_rules! blackhole { ($tt:x) => () }
-// blackhole!(1e0);
