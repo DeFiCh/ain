1ab3af78c052185d76c7bcb471591b462087ea19
Prasanna Loganathar <pvl@prasannavl.com>
2024-10-15 14:21:28 +0800

    break compile for todo

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -143,7 +143,7 @@ public:
         consensus.DF21GrandCentralEpilogueHeight = 2574000; // Jan 10th, 2023.
         consensus.DF22MetachainHeight = 3462000; // Nov 15th, 2023.
         consensus.DF23Height = 4006000; // May 23th, 2024. Tentative date, might change on stable release.
-        consensus.DF24Height = std::numeric_limits<int>::max(); // TODO: Target in 2 weeks
+        consensus.DF24Height = [[ TODO: INTENTIONALLY BREAK COMPILE ]]; // TODO: Target in 2 weeks
 
         consensus.pos.diffLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 //        consensus.pos.nTargetTimespan = 14 * 24 * 60 * 60; // two weeks
