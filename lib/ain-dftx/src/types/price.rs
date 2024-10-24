c03f5dada15129560f6b79a157bdabbb9da8c4c9
canonbrother <w.canonbrother@gmail.com>
2024-10-24 16:49:32 +0800

    struct dftx derive clone

--- lib/ain-dftx/src/types/price.rs
+++ lib/ain-dftx/src/types/price.rs
@@ -3,19 +3,19 @@ use bitcoin::io;
 
 use super::common::CompactVec;
 
-#[derive(ConsensusEncoding, Debug, PartialEq, Eq)]
+#[derive(ConsensusEncoding, Debug, PartialEq, Eq, Clone)]
 pub struct CurrencyPair {
     pub token: String,
     pub currency: String,
 }
 
-#[derive(ConsensusEncoding, Debug, PartialEq, Eq)]
+#[derive(ConsensusEncoding, Debug, PartialEq, Eq, Clone)]
 pub struct TokenAmount {
     pub currency: String,
     pub amount: i64,
 }
 
-#[derive(ConsensusEncoding, Debug, PartialEq, Eq)]
+#[derive(ConsensusEncoding, Debug, PartialEq, Eq, Clone)]
 pub struct TokenPrice {
     pub token: String,
     pub prices: CompactVec<TokenAmount>,
